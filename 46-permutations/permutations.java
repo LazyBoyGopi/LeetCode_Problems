b3ca78267b870a01fb1b683247c2ea4c85de3f61
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-03-13 11:52:26 +0530

    Time: 1 ms (95.49%) | Memory: 44.2 MB (79.80%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,21 +1,43 @@
 class Solution {
-    void backtrack(int[] nums, List<Integer> temp, List<List<Integer>> result) {
-        if (temp.size() == nums.length) {
-            result.add(new ArrayList<>(temp));
+    // void backtrack(int[] nums, List<Integer> temp, List<List<Integer>> result) {
+    //     if (temp.size() == nums.length) {
+    //         result.add(new ArrayList<>(temp));
+    //         return;
+    //     }
+    //     for (int i = 0; i < nums.length; i++) {
+    //         if (temp.contains(nums[i]))
+    //             continue;
+    //         temp.add(nums[i]);
+    //         backtrack(nums, temp, result);
+    //         temp.remove(temp.size() - 1);
+    //     }
+    // }
+    void recur(int[]nums,int fi,List<List<Integer>>result)
+    {
+        if(fi == nums.length)
+        {
+            List<Integer> list = new LinkedList<>();
+            for(int ele : nums) list.add(ele);
+            result.add(list);
             return;
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (temp.contains(nums[i]))
-                continue;
-            temp.add(nums[i]);
-            backtrack(nums, temp, result);
-            temp.remove(temp.size() - 1);
+        for(int i=fi;i<nums.length;i++)
+        {
+            swap(nums,i,fi);
+            recur(nums,fi+1,result);
+            swap(nums,i,fi);
         }
+    }
+    void swap(int[]nums,int i,int fi)
+    {
+        int temp = nums[i];
+        nums[i] = nums[fi];
+        nums[fi] = temp;
     }
 
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        backtrack(nums, new ArrayList<>(), result);
+        List<List<Integer>> result = new LinkedList<>();
+        recur(nums,0, result);
         return result;
     }
 }
