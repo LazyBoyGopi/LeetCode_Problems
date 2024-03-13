c0f74713ad8d5120ce28b020cc7b9ad475fad20d
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-03-13 12:02:18 +0530

    Time: 1 ms (77.54%) | Memory: 40.4 MB (78.99%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int pivotInteger(int n) {
+        if (n==1) return n;
+        int sum = 0;
+        for(int i=1;i<=n;i++)
+        {
+            sum += i;
+        }
+        int preSum = 0;
+        for(int i=1;i<=n;i++)
+        {
+            preSum += i;
+            if(preSum == sum-preSum+i) return i;
+        }
+        return -1;
+    }
+}
