10a18c956980cf050bdb0e4cf667780432f35fc6
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-03-16 12:25:42 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (79.93%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,38 +1,49 @@
 class Solution {
+    private String backSpace(String s)
+    {
+        StringBuilder sb = new StringBuilder();
+        for(char ch : s.toCharArray())
+        {
+            if(ch != '#') sb.append(ch);
+            else if(sb.length() != 0) sb.deleteCharAt(sb.length()-1);
+        }
+        return sb.toString();
+    }
     public boolean backspaceCompare(String s, String t) {
-        Stack<Character> s1 = new Stack<>();
-        Stack<Character> t1 = new Stack<>();
-        int i;
-        for(i=0;i<s.length();i++)
-        {
-            char ch = s.charAt(i);
-            if(ch == '#'){
-                if(!s1.isEmpty())
-                {
-                    s1.pop();
-                }
-            }
-            else{
-                s1.push(ch);
-            }
-        }
-        for(i=0;i<t.length();i++)
-        {
-            char ch = t.charAt(i);
-            if(ch == '#'){
-                if(!t1.isEmpty())
-                {
-                    t1.pop();
-                }
-            }
-            else{
-                t1.push(ch);
-            }
-        }
-        while(!s1.isEmpty() && !t1.isEmpty())
-        {
-            if(s1.pop() != t1.pop()) return false;
-        }
-        return s1.isEmpty() && t1.isEmpty();
+        return backSpace(s).equals(backSpace(t));
+        // Stack<Character> s1 = new Stack<>();
+        // Stack<Character> t1 = new Stack<>();
+        // int i;
+        // for(i=0;i<s.length();i++)
+        // {
+        //     char ch = s.charAt(i);
+        //     if(ch == '#'){
+        //         if(!s1.isEmpty())
+        //         {
+        //             s1.pop();
+        //         }
+        //     }
+        //     else{
+        //         s1.push(ch);
+        //     }
+        // }
+        // for(i=0;i<t.length();i++)
+        // {
+        //     char ch = t.charAt(i);
+        //     if(ch == '#'){
+        //         if(!t1.isEmpty())
+        //         {
+        //             t1.pop();
+        //         }
+        //     }
+        //     else{
+        //         t1.push(ch);
+        //     }
+        // }
+        // while(!s1.isEmpty() && !t1.isEmpty())
+        // {
+        //     if(s1.pop() != t1.pop()) return false;
+        // }
+        // return s1.isEmpty() && t1.isEmpty();
     }
 }
