be12906990aa84950e5e80d253d9122e4fa72120
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-03-16 16:45:54 +0530

    Time: 2 ms (73.93%) | Memory: 52.8 MB (88.14%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,28 +1,22 @@
-
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int len = nums.length;
-        int[] preSum = new int[len];
-        int[] sufSum = new int[len];
-        int[]ans = new int[len];
-        for (int i = 0; i < len; i++) {
-            if (i == 0)
-                preSum[i] = nums[i];
-            else
-                preSum[i] = preSum[i-1]*nums[i];
+        int[] ans = new int[len];
+        ans[0] = nums[0];
+        for(int i=1;i<len;i++)
+        {
+            ans[i] = ans[i-1]*nums[i];
         }
-        for (int i = len - 1; i >= 0; i--) {
-            if (i == len - 1)
-                sufSum[i] = nums[i];
-            else
-                sufSum[i] = sufSum[i+1]*nums[i];
+        for(int i=len-2;i>=0;i--)
+        {
+            nums[i] *= nums[i+1]; 
         }
-        ans[0] = sufSum[1]; 
-        ans[len-1] = preSum[len-2];
+        nums[0] = nums[1];
         for(int i=1;i<len-1;i++)
         {
-            ans[i] = preSum[i-1] * sufSum[i+1];
+            nums[i] = nums[i+1] * ans[i-1];
         }
-        return ans;
+        nums[len-1] = ans[len-2];
+        return nums;
     }
 }
