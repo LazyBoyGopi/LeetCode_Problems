bdc84345a61608ef86fba160e4ab5146965e695f
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-03-16 14:16:22 +0530

    Time: 1 ms (99.88%) | Memory: 42 MB (66.42%) - LeetSync

--- 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.java
+++ 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.java
@@ -1,6 +1,34 @@
 class Solution {
+    void swap(int[]nums,int i,int j)
+    {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    int getPivot(int[]nums,int low , int high)
+    {
+        int pivot = nums[low];
+        int i = low , j = high;
+        while(i<j)
+        {
+            while(i <= high && nums[i] <= pivot) i++;
+            while(j >= low && nums[j] > pivot) j--;
+            if(i<j) swap(nums,i,j);
+        }
+        swap(nums,low,j);
+        return j;
+    }
+    void sort(int[]nums,int low,int high){
+       
+       if(low <= high)
+       {
+        int pivot = getPivot(nums,low,high);
+       sort(nums,low,pivot-1);
+       sort(nums,pivot+1,high);
+       }
+    }
     public int minimumBoxes(int[] apple, int[] capacity) {
-        Arrays.sort(capacity);
+       sort(capacity,0,capacity.length-1);
         int sum = 0 , count = 0;
         for(int apples : apple) sum += apples;
         for(int i=capacity.length-1;i>=0;i--)
