3ce14e4ced542c7ea17ff484752383b0fcfd9ca6
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-03-16 17:21:30 +0530

    Time: 1 ms (99.88%) | Memory: 41.8 MB (89.78%) - LeetSync

--- 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.java
+++ 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.java
@@ -28,14 +28,14 @@ class Solution {
        }
     }
     public int minimumBoxes(int[] apple, int[] capacity) {
-       sort(capacity,0,capacity.length-1);
-        int sum = 0 , count = 0;
+        int len = capacity.length;
+       sort(capacity,0,len-1);
+        int sum = 0;
         for(int apples : apple) sum += apples;
-        for(int i=capacity.length-1;i>=0;i--)
+        for(int i=len-1;i>=0;i--)
         {
             sum -= capacity[i];
-            count++;
-            if(sum <= 0) return count;
+            if(sum <= 0) return len-i;
         }
         return -1;
     }
