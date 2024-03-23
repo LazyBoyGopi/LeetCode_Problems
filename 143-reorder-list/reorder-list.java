81af290247f3615ea76b0985968c540379af15cb
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-03-23 15:27:31 +0530

    Time: 1 ms (99.98%) | Memory: 49.2 MB (6.11%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -9,29 +9,45 @@
  * }
  */
 class Solution {
+    ListNode reOrderList(ListNode head,ListNode cur)
+    {
+        ListNode temp ;
+        if(cur.next != null) temp = reOrderList(head,cur.next);
+        else temp = head;
+        if(temp == null) return null;
+        if(temp == cur || temp.next == cur) {
+            cur.next = null;
+            return null;
+        }
+        cur.next = temp.next;
+        temp.next = cur;
+        return cur.next;
+    }
     public void reorderList(ListNode head) {
-        Stack<ListNode> stack = new Stack<>();
-        ListNode fast = head, slow = head;
-        while(fast != null && fast.next != null)
-        {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        while(slow != null){
-            stack.push(slow);
-            slow = slow.next;
-        }
-        ListNode ans = new ListNode();
-        ListNode org = ans;
-        while(!stack.isEmpty())
-        {
-            ans.next = head;
-            ans = ans.next;
-            head = head.next;
-            ans.next = stack.pop();
-            ans = ans.next;
-        }
-        ans.next = null;
-        head = org.next;
+        if(head == null || head.next == null) return ;
+        reOrderList(head,head.next);
+        // Stack<ListNode> stack = new Stack<>();
+        // ListNode fast = head, slow = head;
+        // while(fast != null && fast.next != null)
+        // {
+        //     slow = slow.next;
+        //     fast = fast.next.next;
+        // }
+        // while(slow != null){
+        //     stack.push(slow);
+        //     slow = slow.next;
+        // }
+        // ListNode ans = new ListNode();
+        // ListNode org = ans;
+        // while(!stack.isEmpty())
+        // {
+        //     ans.next = head;
+        //     ans = ans.next;
+        //     head = head.next;
+        //     ans.next = stack.pop();
+        //     ans = ans.next;
+        // }
+        // ans.next = null;
+        // head = org.next;
     }
 }
