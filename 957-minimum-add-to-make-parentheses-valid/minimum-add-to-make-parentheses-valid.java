e91369ae2651f6a8db4c297107a09349286d9f25
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-03-23 16:46:16 +0530

    Time: 48 ms (27.20%) | Memory: 45.2 MB (8.04%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(stack.isEmpty()){
+                stack.push(ch);
+            }
+            else if(ch == ')' && stack.peek() == '(')
+            {
+                stack.pop();
+            }
+            else stack.push(ch);
+            System.out.println(stack);
+        }
+        return stack.size();
+    }
+}
