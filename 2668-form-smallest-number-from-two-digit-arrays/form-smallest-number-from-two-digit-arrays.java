4d9a1632b9e949cfbdce04a1ccf6bc9b507e2a38
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-03-25 21:36:18 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (82.60%) - LeetSync

--- 2668-form-smallest-number-from-two-digit-arrays/form-smallest-number-from-two-digit-arrays.java
+++ 2668-form-smallest-number-from-two-digit-arrays/form-smallest-number-from-two-digit-arrays.java
@@ -1,7 +1,34 @@
 class Solution {
+    void swap(int[]nums,int i,int j)
+    {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    int getPivot(int[]nums,int st,int end)
+    {
+        int pivot = nums[st];
+        int low = st , high = end;
+        while(low <= high)
+        {
+            while(low <= high && nums[low] <= pivot) low++;
+            while(high >= st && nums[high] > pivot) high--;
+            if(low <= high) swap(nums,low,high);
+        }
+        swap(nums,st,high);
+        return high;
+    }
+    void quickSort(int[]nums,int st,int end)
+    {
+        if(st <= end){
+            int pivot = getPivot(nums,st,end);
+            quickSort(nums,st,pivot-1);
+            quickSort(nums,pivot+1,end);
+        }
+    }
     public int minNumber(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
+        quickSort(nums1,0,nums1.length-1);
+        quickSort(nums2,0,nums2.length-1);
         int first = 0 , second = 0;
         while(first < nums1.length && second < nums2.length)
         {
