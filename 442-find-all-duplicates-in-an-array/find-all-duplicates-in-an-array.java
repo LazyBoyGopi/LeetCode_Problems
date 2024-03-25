af7545dcadbb09c871048606e690b84451e7a44d
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-03-25 20:11:33 +0530

    Time: 6 ms (74.33%) | Memory: 55.2 MB (15.16%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -1,11 +1,21 @@
 class Solution {
+    void swap(int i,int j,int[] nums)
+    {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
     public List<Integer> findDuplicates(int[] nums) {
-        Set<Integer> set = new TreeSet<>();
+        int st= 1;
         List<Integer> list = new LinkedList<>();
-        for(int i : nums)
+        while(st < nums.length)
         {
-            if(set.contains(i)) list.add(i);
-            else set.add(i);
+            if(nums[st] != st+1 && nums[nums[st]-1] != nums[st]) swap(st,nums[st]-1,nums);
+            else st++; 
+        }
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i] != i+1) list.add(nums[i]);
         }
         return list;
     }
