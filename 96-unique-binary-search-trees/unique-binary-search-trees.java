4b607499a276583480e825160bcff89e78b7d40f
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-03-30 01:02:22 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (60.63%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,11 +1,16 @@
 class Solution {
-    public int numTrees(int n) {
+    private int getCombinations(int n , int[]dp)
+    {
         if(n <= 1) return 1;
-        int res = 0;
+        if(dp[n] != 0) return dp[n];
         for(int i=1;i<=n;i++)
         {
-            res += numTrees(i-1)*numTrees(n-i);
+            dp[n] += getCombinations(i-1,dp)*getCombinations(n-i,dp);
         }
-        return res;
+        return dp[n];
+    }
+    public int numTrees(int n) {
+        int[]dp = new int[n+1];
+        return getCombinations(n,dp);
     }
 }
