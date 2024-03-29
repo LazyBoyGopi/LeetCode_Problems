2aa7fbcc41fbc11601b60187173a88a3bd3348e7
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-03-29 17:56:52 +0530

    Time: 2 ms (98.03%) | Memory: 41.8 MB (39.70%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        boolean freq[] = new boolean[2001];
+        Arrays.sort(arr);
+        int count = 1;
+        int last = arr[0];
+        for(int i=1;i<arr.length;i++)
+        {
+            if(arr[i] == last ) count++;
+            else {
+                if(freq[count]) return false;
+                else freq[count] = true;
+                last = arr[i];
+                count = 1;
+            }
+        }
+        return !freq[count];
+    }
+}
