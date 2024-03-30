3f84a50f15cf0e9d8484cc397a7c6a18ba7db18e
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-03-31 01:32:14 +0530

    Time: 11 ms (47.43%) | Memory: 44.9 MB (77.46%) - LeetSync

---
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countCharacters(String[] words, String chars) {
+        int len = 0;
+        int[] freq = new int[26];
+        for(char ch : chars.toCharArray())
+        {
+            freq[ch-97]++;
+        }
+        for(String word : words)
+        {
+            int[] ar = Arrays.copyOf(freq,freq.length);
+            boolean isPossible = true;
+            for(char ch : word.toCharArray())
+            {
+                if(ar[ch-97] <= 0) {
+                    isPossible = false;
+                    continue;
+                }
+                else ar[ch - 97]--;
+            }
+            if(isPossible) len += word.length();
+        }
+        return len;
+    }
+}
