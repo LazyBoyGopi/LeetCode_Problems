f7d25964605204c5d9c21a54bc479d78ddb29bae
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-03-31 00:50:56 +0530

    Time: 2 ms (38.64%) | Memory: 46.8 MB (26.47%) - LeetSync

--- 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.java
+++ 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.java
@@ -1,35 +1,27 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
         int idx = 0;
         ListNode org = list1;
-        while(list1 != null)
-        {
-            
-            if(idx == a-1)
-            {
-                System.out.println("HII "+idx);
+        while (list1 != null) {
+            if (idx == a - 1) {
                 ListNode temp = list1.next;
                 list1.next = list2;
                 list1 = temp;
             }
-            if(idx == b)
-            {
-                System.out.println("hello "+idx +" "+list1.val);
-                while(list2.next != null)
-                {
+            if (idx == b) {
+                while (list2.next != null) {
                     list2 = list2.next;
                 }
-
                 list2.next = list1;
                 break;
             }
