6ff79bdb970f611411508610c7afedadd94d7c5e
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-04-01 19:39:34 +0530

    Time: 1 ms (100.00%) | Memory: 45.9 MB (7.90%) - LeetSync

--- 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.java
+++ 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.java
@@ -16,6 +16,20 @@ class Solution {
         return count;
     }
     public int[] findIntersectionValues(int[] nums1, int[] nums2) {
-        return new int[]{getCount(nums1,nums2),getCount(nums2,nums1)};
+        // return new int[]{getCount(nums1,nums2),getCount(nums2,nums1)};
+        boolean[] b1 = new boolean[101];
+        boolean[] b2 = new boolean[101];
+        for(final int ele : nums1) b1[ele] = true;
+        for(final int ele : nums2) b2[ele] = true;
+        int c1 = 0 , c2 =0;
+        for(final int ele : nums1)
+        {
+            if(b2[ele]) c1++;
+        }
+        for(final int ele : nums2)
+        {
+            if(b1[ele]) c2++;
+        }
+        return new int[]{c1,c2};
     }
 }
