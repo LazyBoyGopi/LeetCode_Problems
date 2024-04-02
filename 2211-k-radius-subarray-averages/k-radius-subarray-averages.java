bca95d8abc203010a4dffda62c58df89ac443cac
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-04-02 19:15:47 +0530

    Time: 1018 ms (5.02%) | Memory: 63.8 MB (39.83%) - LeetSync

---
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] getAverages(int[] nums, int k) {
+        int len = nums.length;
+        int range = 2*k+1;
+        int[] ans = new int[len];
+        if(range > len)
+        {
+            Arrays.fill(ans,-1);
+            return ans;
+        }
+        for(int i=0;i<k;i++)
+        {
+            ans[i] = -1;
+            ans[len-i-1] = -1;
+        }
+        long sum =0;
+        for(int i=0;i<range;i++)
+        {
+            sum += nums[i];
+        }
+        for(int i=k;i<len-k;i++)
+        {
+            System.out.println(sum);
+            ans[i] =(int) (sum/range);
+            if(i < len-k-1)
+            {
+                sum += nums[i+k+1];
+                sum -= nums[i-k];
+            }
+        }
+        return ans;
+    }
+}
