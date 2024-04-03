bd611f80eb656a8105b3fc4ef8eceaf2acb6bdb1
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-04-03 17:25:05 +0530

    Time: 148 ms (54.92%) | Memory: 41.3 MB (84.84%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,44 +1,41 @@
 class Solution {
-    private boolean checkNext(char[][] board, String word, int r, int c, int curIdx,boolean isFirst) {
-        if(word.length() == curIdx) return true;
-        for (int i = r; i < board.length; i++) {
-            for (int j = c; j < board[i].length; j++) {
+    private boolean check(char[][]board,String word,int r,int c ,int curIdx,boolean isFirst)
+    {
+        if(curIdx == word.length()) return true;
+        for(int i=r;i<board.length;i++)
+        {
+            for(int j=c;j<board[i].length;j++)
+            {
                 if(board[i][j] == ' ') return false;
-                if (board[i][j] == word.charAt(curIdx)) {
+                if(board[i][j] == word.charAt(curIdx))
+                {
                     if(word.length() == curIdx+1) return true;
                     char ch = board[i][j];
                     board[i][j] = ' ';
-                    if (i < board.length-1 ) {
-                        if(checkNext(board,word,i+1,j,curIdx+1,false)) 
-                        {
-                            return true;
-                        }
+                    if(i > 0)
+                    {
+                        if(check(board,word,i-1,j,curIdx+1,false)) return true;
                     }
-                    if (i > 0) {
-                        if(checkNext(board,word,i-1,j,curIdx+1,false)) {
-                            return true;
-                        }
+                    if(i < board.length-1)
+                    {
+                        if(check(board,word,i+1,j,curIdx+1,false)) return true;
                     }
-                    if (j < board[i].length-1 ) {
-                        if(checkNext(board,word,i,j+1,curIdx+1,false)) {
-                            return true;
-                        }
+                    if(j < board[i].length-1)
+                    {
+                        if(check(board,word,i,j+1,curIdx+1,false)) return true;
                     }
-                    if (j > 0) {
-                        if(checkNext(board,word,i,j-1,curIdx+1,false)) {
-                            return true;
-                        }
+                    if(j > 0)
+                    {
+                        if(check(board,word,i,j-1,curIdx+1,false)) return true;
                     }
                     board[i][j] = ch;
                 }
                 if(!isFirst) return false;
             }
-            if(!isFirst) return false;
         }
         return false;
     }
-
     public boolean exist(char[][] board, String word) {
-        return checkNext(board,word,0,0,0,true);
+        return check(board,word,0,0,0,true);
     }
 }
