38c52d75da9423261ada73a35f5c6c368481587b
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-04-04 13:12:28 +0530

    Time: 2 ms (84.20%) | Memory: 44.3 MB (87.53%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,25 +1,24 @@
 class Solution {
-    void backTrack(int[]candidates,int target,List<List<Integer>>result , List<Integer>temp,int sum,int idx)
+    void backTrack(int[]candidates,int target,List<List<Integer>>result , List<Integer>temp,int idx)
     {
-        if(sum == target)
+        if(target == 0)
         {
             result.add(new LinkedList<>(temp));
             return;
         }
-        if(sum > target)
-        {
-            return;
-        }
         for(int i=idx;i<candidates.length;i++)
         {
-            temp.add(candidates[i]);
-            backTrack(candidates,target,result,temp,sum+candidates[i],i);
-            temp.remove(temp.size()-1);
+            if(target >= candidates[i])
+            {
+                temp.add(candidates[i]);
+                backTrack(candidates,target-candidates[i],result,temp,i);
+                temp.remove(temp.size()-1);
+            }
         }
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new LinkedList<>();
-        backTrack(candidates,target,result,new LinkedList<>(),0,0);
+        backTrack(candidates,target,result,new LinkedList<>(),0);
         return result;
     }
 }
