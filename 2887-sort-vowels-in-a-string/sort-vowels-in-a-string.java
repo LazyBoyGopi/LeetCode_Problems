5b11306410b150a10c32115f3d83760680de0046
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-04-04 16:09:16 +0530

    Time: 43 ms (68.47%) | Memory: 45.5 MB (91.68%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
@@ -1,62 +1,32 @@
 class Solution {
     public String sortVowels(String s) {
-        int[]ar = new int[10];
+        int[] ar = new int[128];
+        String vowels = "AEIOUaeiou";
         for(char ch : s.toCharArray())
         {
-            switch(ch)
+            if(vowels.indexOf(ch) != -1)
             {
-                case 'A' -> ar[0]++;
-                case 'E' -> ar[1]++;
-                case 'I' -> ar[2]++;
-                case 'O' -> ar[3]++;
-                case 'U' -> ar[4]++;
-                case 'a' -> ar[5]++;
-                case 'e' -> ar[6]++;
-                case 'i' -> ar[7]++;
-                case 'o' -> ar[8]++;
-                case 'u' -> ar[9]++;
+                ar[ch]++;
             }
         }
-        int idx = 0;
         StringBuilder sb = new StringBuilder();
         for(char ch : s.toCharArray())
         {
-            switch(ch)
+            if(vowels.indexOf(ch) == -1)
             {
-                case 'A':
-                case 'E':
-                case 'I':
-                case 'O':
-                case 'U':
-                case 'a':
-                case 'e':
-                case 'i':
-                case 'o':
-                case 'u': char curChar = ' ';
-                for(int i = 0;i<ar.length;i++)
+                sb.append(ch);
+            }
+            else{
+                for(int i=0;i<128;i++)
                 {
-                    if(ar[i] > 0)
+                    int ele = ar[i];
+                    if(ele > 0)
                     {
-                        switch(i)
-                        {
-                            case 0 -> curChar = 'A';
-                            case 1 -> curChar = 'E';
-                            case 2 -> curChar = 'I';
-                            case 3 -> curChar = 'O';
-                            case 4 -> curChar = 'U';
-                            case 5 -> curChar = 'a';
-                            case 6 -> curChar = 'e';
-                            case 7 -> curChar = 'i';
-                            case 8 -> curChar = 'o';
-                            case 9 -> curChar = 'u';
-                        }
+                        sb.append((char)i);
                         ar[i]--;
                         break;
                     }
                 }
-                sb.append(curChar);
-                break;
-                default : sb.append(ch);
             }
         }
         return sb.toString();
