cf338bee4fb77576121dc93271adf7870f3433f6
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-04-05 20:01:58 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (49.22%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -3,41 +3,49 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         for(int i=0;i<nums.length;i++)
         {
-            sb.append('0');
+            char curr = nums[i].charAt(i);
+            sb.append(curr == '0'?'1':'0');
         }
-        String s = sb.toString();
-        int idx = 0;
-        int times = 0;
-        while(true)
-        {
-            boolean flag = true;
-            for(String str : nums)
-            {
-                if(str.equals(s)){
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag) break;
-            else{
-                char[]ar = s.toCharArray();
-                if(ar[idx] == '1')
-                {
-                    ar[idx] = '0';
-                    times++;
-                }
-                else{
-                    ar[idx] ='1';
-                    times++;
-                }
-                if(times == 2)
-                {
-                    idx++;
-                    times=0;
-                }
-                s = new StringBuilder().append(ar).toString();
-            }
-        }
-        return s;
+        return sb.toString();
+
+        // StringBuilder sb = new StringBuilder();
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     sb.append('0');
+        // }
+        // String s = sb.toString();
+        // int idx = 0;
+        // int times = 0;
+        // while(true)
+        // {
+        //     boolean flag = true;
+        //     for(String str : nums)
+        //     {
+        //         if(str.equals(s)){
+        //             flag = false;
+        //             break;
+        //         }
+        //     }
+        //     if(flag) break;
+        //     else{
+        //         char[]ar = s.toCharArray();
+        //         if(ar[idx] == '1')
+        //         {
+        //             ar[idx] = '0';
+        //             times++;
+        //         }
+        //         else{
+        //             ar[idx] ='1';
+        //             times++;
+        //         }
+        //         if(times == 2)
+        //         {
+        //             idx++;
+        //             times=0;
+        //         }
+        //         s = new StringBuilder().append(ar).toString();
+        //     }
+        // }
+        // return s;
     }
 }
