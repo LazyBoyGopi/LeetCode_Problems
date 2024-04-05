ee5cd4740afdd093bd3c331be421da615bbbde79
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-04-05 19:59:10 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (86.73%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -3,19 +3,17 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         for(int i=0;i<nums.length;i++)
         {
-            sb.append("1");
+            sb.append('0');
         }
         String s = sb.toString();
         int idx = 0;
-        int ones = 0;
+        int times = 0;
         while(true)
         {
-            
             boolean flag = true;
-            for(int i=0;i<nums.length;i++)
+            for(String str : nums)
             {
-                if(nums[i].equals(s))
-                {
+                if(str.equals(s)){
                     flag = false;
                     break;
                 }
@@ -23,21 +21,21 @@ class Solution {
             if(flag) break;
             else{
                 char[]ar = s.toCharArray();
-            if(ar[idx] == '1')
-            {
-                ar[idx] = '0';
-                ones++;
-            }
-            else{
-                ar[idx] = '1';
-                ones++;
-            }
-            if(ones == 2)
-            {
-                ones =0;
-                idx++;
-            }
-            s = new StringBuilder().append(ar).toString();
+                if(ar[idx] == '1')
+                {
+                    ar[idx] = '0';
+                    times++;
+                }
+                else{
+                    ar[idx] ='1';
+                    times++;
+                }
+                if(times == 2)
+                {
+                    idx++;
+                    times=0;
+                }
+                s = new StringBuilder().append(ar).toString();
             }
         }
         return s;
