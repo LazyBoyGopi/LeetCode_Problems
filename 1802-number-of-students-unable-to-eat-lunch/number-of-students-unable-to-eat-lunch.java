395aed39dc5c717601982053af1f6b1bad2349b0
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-04-08 11:33:37 +0530

    Time: 2 ms (50.92%) | Memory: 41.4 MB (50.61%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.java
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.java
@@ -2,16 +2,13 @@ class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
         Queue<Integer> stu = new ArrayDeque<>();
         for(int ele : students) stu.offer(ele);
-        System.out.println(stu);
         int idx = 0;
         while(idx < sandwiches.length && stu.size() > 0)
         {
             if(sandwiches[idx] == stu.peek())
             {
-                System.out.println(stu.remove());
                 idx++;
-                    System.out.println("if"+stu);
-
+                stu.remove();
             }
             else{
                 int rotationCount = 0;
@@ -19,11 +16,9 @@ class Solution {
                 {
                     rotationCount++;
                     stu.add(stu.poll());
-                    System.out.println("else"+stu);
                 }
                 if(rotationCount > stu.size()) break;
             }
-            System.out.println(stu);
         }
         return stu.size();
     }
