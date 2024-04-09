4eacb6cdf79aadf1da8ba0189492766bfe52c238
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-04-09 10:25:33 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (38.65%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -1,18 +1,11 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
-        List<Integer> q = new LinkedList<>();
-        for(int ele : tickets) q.add(ele);
-        int time = 0;
-        while(q.size() > 0 && q.get(k) > 0)
+        int target = tickets[k] , time = 0;
+        for(int i=0;i<tickets.length;i++)
         {
-            int ele = q.remove(0)-1;
-            time++;
-            if(k == 0 && ele == 0) return time;
-            if(ele > 0)
-            q.add(ele);
-            if(k > 0)
-            k--;
-            else k = q.size()-1;
+            int ele = tickets[i];
+            time += ele-target < 0 ? ele : target;
+            if(i > k && ele >= target) time--;
         }
         return time;
     }
