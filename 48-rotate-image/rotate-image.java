436f1881848e78907131b1900a951b7a512517f3
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-04-10 20:31:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (53.53%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,33 +1,23 @@
 class Solution {
-    private void transpose(int[][]ar)
-    {
-        for(int i=0;i<ar.length;i++)
-        {
-            for(int j=i;j<ar.length;j++)
-            {
-                int temp = ar[i][j];
-                ar[i][j] = ar[j][i];
-                ar[j][i] = temp;
-            }
-        }
-    }
-    private void swap(int[][]ar)
-    {
-        for(int i=0;i<ar.length;i++)
-        {
-            int st = 0 , end = ar.length-1;
-            while(st <= end)
-            {
-                int temp = ar[i][st];
-                ar[i][st] = ar[i][end];
-                ar[i][end] = temp;
-                st++;
-                end--;
-            }
-        }
-    }
     public void rotate(int[][] matrix) {
-        transpose(matrix);
-        swap(matrix);
+        int[][]ans = new int[matrix.length][matrix.length];
+        int col = matrix.length-1;
+        for(int i=0;i<matrix.length;i++)
+        {
+            int row = 0;
+            for(int j=0;j<matrix[i].length;j++)
+            {
+                ans[row][col] = matrix[i][j];
+                row++;
+            }
+            col--;
+        }
+        for(int i=0;i<matrix.length;i++)
+        {
+            for(int j=0;j<matrix.length;j++)
+            {
+                matrix[i][j] = ans[i][j];
+            }
+        }
     }
 }
