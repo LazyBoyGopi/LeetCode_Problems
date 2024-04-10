c6302a5ba6f1256be456a403d10458e4cabc60df
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-04-10 19:47:22 +0530

    Time: 41 ms (5.22%) | Memory: 45.7 MB (24.58%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,48 @@
+class Solution {
+    private void setZeroesAtCol(int[][]matrix,int col)
+    {
+        for(int i=0;i<matrix.length;i++)
+        {
+            matrix[i][col] = 0;
+        }
+    }
+    private void setZeroesAtRow(int[][]matrix,int row)
+    {
+        for(int i=0;i<matrix[0].length;i++)
+        {
+            System.out.println(row);
+            matrix[row][i] = 0;
+        }
+    }
+    public void setZeroes(int[][] matrix) {
+        int[]row = new int[matrix.length];
+        int[]col = new int[matrix[0].length];
+        for(int i=0;i<matrix.length;i++)
+        {
+            for(int j=0;j<matrix[i].length;j++)
+            {
+                if(matrix[i][j] == 0)
+                {
+                    col[j] = 1;
+                    row[i] = 1;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(col));
+        System.out.println(Arrays.toString(row));
+        for(int i=0;i<col.length;i++)
+        {
+            if(col[i] == 1)
+            {
+                setZeroesAtCol(matrix,i);
+            }
+        }
+        for(int i=0;i<row.length;i++)
+        {
+            if(row[i] == 1)
+            {
+                setZeroesAtRow(matrix,i);
+            }
+        }
+    }
+}
