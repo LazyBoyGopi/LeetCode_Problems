a8d3be477ea8faf988f503366a039ae55ef63434
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-04-10 20:21:11 +0530

    Time: 1 ms (73.58%) | Memory: 45.5 MB (40.23%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,45 +1,32 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int col0 = -1;
-        for(int i=0;i<matrix.length;i++)
-        {
-            for(int j=0;j<matrix[i].length;j++)
-            {
-                if(matrix[i][j] == 0)
-                {
-                    if(j == 0)
-                    {
+        int col0 = 1;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 0) {
+                    if (j == 0) {
                         col0 = 0;
-                    }
-                    else
-                    {
-                        matrix[0][j] = 0;
+                    } else {
                         matrix[i][0] = 0;
+                        matrix[0][j] = 0;
                     }
                 }
             }
         }
-        for(int i=matrix.length-1;i>0;i--)
-        {
-            for(int j=matrix[i].length-1;j>0;j--)
-            {
-                if(matrix[i][0] == 0 || matrix[0][j] == 0)
-                {
+        for (int i = matrix.length - 1; i > 0; i--) {
+            for (int j = matrix[i].length - 1; j > 0; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
         }
-        for(int i=matrix[0].length-1;i>=0;i--)
-        {
-            if(matrix[0][i] == 0 || matrix[0][0] == 0)
-            {
+        for (int i = matrix[0].length - 1; i >= 0; i--) {
+            if (matrix[0][i] == 0 || matrix[0][0] == 0) {
                 matrix[0][i] = 0;
             }
         }
-        for(int i=matrix.length-1;i>=0;i--)
-        {
-            if(col0 == 0)
-            {
+        for (int i = matrix.length - 1; i >= 0; i--) {
+            if (matrix[i][0] == 0 || col0 == 0) {
                 matrix[i][0] = 0;
             }
         }
