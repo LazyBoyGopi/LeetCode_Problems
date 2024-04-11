e89494bef8749d684e2dc89804ca0e98226522ee
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-04-11 12:18:47 +0530

    Time: 2 ms (99.20%) | Memory: 47.9 MB (52.17%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,14 +1,36 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int three = (nums.length/3)+1;
-        List<Integer>list = new LinkedList<>();
-        HashMap<Integer,Integer> map = new HashMap<>();
+        int c1 = 0 , c2 = 0;
+        int e1 =Integer.MIN_VALUE, e2= Integer.MIN_VALUE;
         for(int ele : nums)
         {
-            map.put(ele,map.getOrDefault(ele,0)+1);
-            if(map.get(ele) == three) list.add(ele);
-            if(list.size() == 2) break;
+            if(c1 == 0 && ele != e2)
+            {
+                c1 = 1;
+                e1 = ele;
+            }
+            else if(c2 == 0 && ele != e1)
+            {
+                c2 = 1;
+                e2 = ele;
+            }
+            else if(ele == e1) c1++;
+            else if(ele == e2) c2++;
+            else{
+                c1--;
+                c2--;
+            }
         }
+        c1 = 0;
+        c2 = 0;
+        for(int ele : nums)
+        {
+            if(ele == e1) c1++;
+            else if(ele == e2) c2++;
+        }
+        List<Integer> list = new LinkedList<>();
+        if(c1 >= (nums.length/3)+1) list.add(e1);
+        if(c2 >= (nums.length/3)+1) list.add(e2);
         return list;
     }
 }
