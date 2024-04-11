d73d07d9a68445c0acdc71948f375a4ab9177270
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-04-11 12:04:01 +0530

    Time: 15 ms (13.82%) | Memory: 47.8 MB (57.31%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,13 +1,13 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int three = nums.length/3;
-        Set<Integer>set = new HashSet<>();
+        int three = (nums.length/3)+1;
+        List<Integer>list = new LinkedList<>();
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int ele : nums)
         {
             map.put(ele,map.getOrDefault(ele,0)+1);
-            if(map.get(ele) > three) set.add(ele);
+            if(map.get(ele) == three) list.add(ele);
         }
-        return new LinkedList<>(set);
+        return list;
     }
 }
