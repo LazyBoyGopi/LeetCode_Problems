0059f419cdb402ce5f8b5cbbf45583a153997042
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-04-13 16:23:00 +0530

    Time: 0 ms (100.00%) | Memory: 39.7 MB (99.58%) - LeetSync

---
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        int totalMatches = 0;
+        while(n != 1)
+        {
+            if(n == 2) return ++totalMatches;
+            if(n % 2 == 0){
+                totalMatches += n/2;
+                n /=2;
+            }
+            else{
+                totalMatches += n/2;
+                n = ( n/2 )+1;
+            }
+        }
+        return totalMatches;
+    }
+}
