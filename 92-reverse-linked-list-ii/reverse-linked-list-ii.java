e01f8496099bf23e944e941e0d38e8c78a7659ff
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-04-14 19:00:20 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (76.21%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -1,49 +1,56 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        if(left == right) return head;
-        if(head == null || head.next == null) return head;
-        Stack<ListNode> stack = new Stack<>();
+        if (head == null || head.next == null || left == right)
+            return head;
         int idx = 1;
-        ListNode start = null , end = null , temp = head;
-        while(temp != null)
-        {
-            if(idx == left-1) start = temp;
-            if(idx >= left && idx <= right)
-            {
-                stack.push(temp);
+        ListNode temp = head, last = null;
+        while (temp != null) {
+            if (idx == left) {
+                int curIdx = idx;
+                ListNode rev = null, front = null, temp1 = temp ;
+                while (curIdx <= right) {
+                    front = temp1.next;
+                    temp1.next = rev;
+                    rev = temp1;
+                    temp1 = front;
+                    curIdx++;
+                }
+                if(last == null && front == null) return rev;
+                else if (last == null && front != null) {
+                    ListNode main = rev;
+                    temp1 = rev;
+                    while (temp1.next != null) {
+                        temp1 = temp1.next;
+                    }
+                    temp1.next = front;
+                    return main;
+                }
+                else if(last != null)
+                {
+                    last.next = rev;
+                    temp1 = last;
+                    while (temp1.next != null) {
+                        temp1 = temp1.next;
+                    }
+                    temp1.next = front;
+                    break;
+                }
+
             }
-            if(idx == right+1){
-                end = temp;
-                 break;
-            }
+            last = temp;
             temp = temp.next;
             idx++;
         }
-        boolean isStartFirst = start == null ? true : false;
-        if(start == null)
-        {
-            start = stack.pop();
-        }
-        ListNode ans = start;
-
-        while(!stack.isEmpty())
-        {
-            start.next = stack.pop();
-            start = start.next;
-        }
-
-        start.next = end;
-        if(isStartFirst) return ans;
         return head;
     }
 }
