8f261536c260314eda006bb0c590c30d2825fe7e
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-04-14 22:54:18 +0530

    Time: 1 ms (86.46%) | Memory: 40.6 MB (64.81%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,10 +1,14 @@
 class Solution {
     public int mySqrt(int x) {
         if(x <= 1) return x;
-        for(long i=1;i<x;i++)
+        int st = 1 , end = x;
+        while(st <= end)
         {
-            if(i*i <= x && (i+1)*(i+1) > x) return (int)i;
+            long mid = (long)st+(end-st)/2;
+            if(mid*mid <= x && (mid+1)*(mid+1) > x) return (int)mid;
+            if(mid*mid > x) end = (int)mid-2;
+            else st = (int)mid+1;
         }
-        return 0;
+        return st;
     }
 }
