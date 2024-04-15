12d9cfa666c4254133cf70d2873909fbbbb3d125
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-04-15 12:04:28 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (76.93%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
@@ -1,34 +1,29 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
-    private int getSum(TreeNode root,int curVal)
-    {
-        if(root == null) return 0;
-        if(root.left == null && root.right == null)
-        {
-            curVal = curVal*10 + root.val;
+    private int getSum(TreeNode root, int curVal) {
+        if (root == null)
+            return 0;
+        curVal = curVal * 10 + root.val;
+        if (root.left == null && root.right == null)
             return curVal;
-        }
-        int sum = 0;
-        curVal = curVal*10+root.val;
-        sum += getSum(root.left,curVal);
-        sum += getSum(root.right,curVal);
-        return sum;
+        return getSum(root.left, curVal) + getSum(root.right, curVal);
     }
+
     public int sumNumbers(TreeNode root) {
-        return getSum(root,0);
+        return getSum(root, 0);
     }
 }
