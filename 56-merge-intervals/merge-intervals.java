9c58804d1d233ed4a63c51c7af2cec95b7d88b29
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-04-19 17:51:46 +0530

    Time: 86 ms (5.22%) | Memory: 49.9 MB (5.30%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,14 +1,15 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+        Arrays.sort(intervals,(a,b) -> Integer.compare(a[0],b[0]));
         List<List<Integer>> list = new LinkedList<>();
         for(int i=0;i<intervals.length;i++)
         {
             if(list.isEmpty() || list.get(list.size()-1).get(1) < intervals[i][1])
             {
-                List<Integer> temp = new LinkedList<>(Arrays.asList(intervals[i][0],intervals[i][1]));
+                List<Integer>temp = new LinkedList<>(Arrays.asList(intervals[i][0],intervals[i][1]));
                 list.add(temp);
             }
+            else continue;
             for(int j=i;j<intervals.length;j++)
             {
                 if(list.get(list.size()-1).get(1) >= intervals[j][0])
@@ -16,17 +17,15 @@ class Solution {
                     if(list.get(list.size()-1).get(1) < intervals[j][1])
                     list.get(list.size()-1).set(1,intervals[j][1]);
                 }
-                else {
-                    break;
-                }
+                else break;
             }
         }
-        int[][]ans = new int[list.size()][2] ;
+        int[][]ans = new int[list.size()][2];
         for(int i=0;i<list.size();i++)
         {
             ans[i][0] = list.get(i).get(0);
             ans[i][1] = list.get(i).get(1);
-        }  
-        return ans;   
+        }
+        return ans;
     }
 }
