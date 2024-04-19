8e97f4c5ae3785999072f4b3beb63b44784a9751
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-04-19 18:14:34 +0530

    Time: 9 ms (57.33%) | Memory: 46.4 MB (48.62%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,37 +1,21 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals,(a,b) -> Integer.compare(a[0],b[0]));
-        List<List<Integer>> list = new LinkedList<>();
-        for(int i=0;i<intervals.length;i++)
+        List<int[]> merged = new LinkedList<>();
+        int[] curInterval = intervals[0];
+        for(int curIdx = 0 ;curIdx < intervals.length;curIdx++)
         {
-            if(list.isEmpty() || list.get(list.size()-1).get(1) < intervals[i][1])
+            if(curInterval[1] >= intervals[curIdx][0])
             {
-                List<Integer>temp = new LinkedList<>(Arrays.asList(intervals[i][0],intervals[i][1]));
-                list.add(temp);
+                curInterval[1] = Math.max(curInterval[1],intervals[curIdx][1]);
             }
-            int ele = list.get(list.size()-1).get(1);
-            for(int j=i;j<intervals.length;j++)
-            {
-                if(ele >= intervals[j][0])
-                {
-                    if(ele < intervals[j][1])
-                    {
-                    list.get(list.size()-1).set(1,intervals[j][1]);
-                    ele = intervals[j][1];
-                    }
-                }
-                else {
-                    i=j-1;
-                    break;
-                }
+            else{
+                merged.add(curInterval);
+                curInterval = intervals[curIdx];
             }
+
         }
-        int[][]ans = new int[list.size()][2];
-        for(int i=0;i<list.size();i++)
-        {
-            ans[i][0] = list.get(i).get(0);
-            ans[i][1] = list.get(i).get(1);
-        }
-        return ans;
+        merged.add(curInterval);
+        return merged.toArray(new int[merged.size()][]);
     }
 }
