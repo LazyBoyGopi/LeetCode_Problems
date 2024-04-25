55d1fd14abb0c07779574f26f54b46c066a08788
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-04-25 11:12:17 +0530

    Time: 1 ms (100.00%) | Memory: 44.2 MB (64.01%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -10,49 +10,54 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode ans = new ListNode();
-        ListNode org = ans;
         int rem = 0;
+        ListNode org = l1;
+        ListNode pre = l1;
         while (l1 != null && l2 != null) {
             int sum = l1.val + l2.val + rem;
             if (sum > 9) {
-                ans.next = new ListNode(sum % 10);
+                l1.val = sum % 10;
                 rem = sum / 10;
             } else {
                 rem = 0;
-                ans.next = new ListNode(sum);
+                l1.val = sum;
             }
+            pre = l1;
             l1 = l1.next;
             l2 = l2.next;
-            ans = ans.next;
         }
         while (l1 != null) {
             int sum = l1.val + rem;
             if (sum > 9) {
-                ans.next = new ListNode(sum % 10);
+                l1.val = sum % 10;
                 rem = sum / 10;
             } else {
                 rem = 0;
-                ans.next = new ListNode(sum);
+                l1.val = sum;
             }
+            pre = l1;
             l1 = l1.next;
-            ans = ans.next;
         }
+        l1 = pre;
         while (l2 != null) {
             int sum = l2.val + rem;
             if (sum > 9) {
-                ans.next = new ListNode(sum % 10);
+                l1.next = new ListNode(sum % 10);
                 rem = sum / 10;
             } else {
                 rem = 0;
-                ans.next = new ListNode(sum);
+                l1.next = new ListNode(sum);
             }
+            pre = l1;
             l2 = l2.next;
-            ans = ans.next;
+            l1 = l1.next;
         }
+
         if (rem != 0) {
-            ans.next = new ListNode(rem);
+            if(l1 == null)
+                l1 = pre;
+            l1.next = new ListNode(rem);
         }
-        return org.next;
+        return org;
     }
 }
