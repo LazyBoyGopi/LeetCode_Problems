58a65f00e63cf826b628d7a55bc935cdf7e1a775
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-04-25 13:05:52 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (5.53%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,34 +1,29 @@
 class Solution {
-    private int getPivot(int[]ar,int target)
-    {
-        int st = 0 , end = ar.length-1;
-        while(st <= end)
-        {
-            int mid = st+(end-st)/2;
-            if(ar[mid] == target) return mid;
-            if(mid < ar.length-1 && ar[mid] > ar[mid+1]) return mid+1;
-            if(mid > 0 && ar[mid] < ar[mid-1]) return mid;
-            if(ar[mid] > ar[st]) st = mid+1;
-            else end = mid-1;
-        }
-        return st;
-    }
-    private int binarySearch(int[]nums,int target,int st , int end )
+    private int getIdx(int[]nums,int target,int st, int end)
     {
         while(st <= end)
         {
             int mid = st+(end-st)/2;
             if(nums[mid] == target) return mid;
-            if(nums[mid] > target) end = mid-1;
-            else st = mid+1;
+            if(nums[st] <= nums[mid]  )
+            {
+                if(nums[st] <= target && target <= nums[mid])
+                    end = mid-1;
+                else st = mid+1;
+            }
+            else {
+                if(nums[mid] < nums[end] && nums[mid] <= target && target <= nums[end])
+                {
+                    st = mid+1;
+                }
+                else end = mid-1;
+            }
         }
         return -1;
     }
     public int search(int[] nums, int target) {
-        int idx = getPivot(nums,target);
-        if(idx == nums.length || idx == 0) return binarySearch(nums,target,0,nums.length-1);
-        if(target == nums[idx]) return idx;
-        if(target >= nums[0] && target <= nums[idx-1]) return binarySearch(nums,target,0,idx-1);
-        return binarySearch(nums,target,idx+1,nums.length-1);
+        int idx = getIdx(nums,target,0,nums.length-1);
+        if(idx >= nums.length) return -1;
+        return idx;
     }
 }
