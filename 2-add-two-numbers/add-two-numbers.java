2713633c82238d3c214c2be05024372f002111bb
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-04-25 11:03:29 +0530

    Time: 2 ms (15.98%) | Memory: 44.5 MB (38.01%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -1,39 +1,24 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-    private ListNode reverse(ListNode head)
-    {
-        ListNode pre = null , cur = head , next = null;
-        while(cur != null)
-        {
-            next = cur.next;
-            cur.next = pre;
-            pre = cur;
-            cur = next;
-        }
-        return pre;
-    }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode ans = new ListNode();
         ListNode org = ans;
-        int rem = 0; 
-        while(l1 != null && l2 != null)
-        {
-            int sum = l1.val+l2.val+rem;
-            if(sum > 9)
-            {
-                ans.next = new ListNode(sum%10);
-                rem = sum/10;
-            }
-            else{
+        int rem = 0;
+        while (l1 != null && l2 != null) {
+            int sum = l1.val + l2.val + rem;
+            if (sum > 9) {
+                ans.next = new ListNode(sum % 10);
+                rem = sum / 10;
+            } else {
                 rem = 0;
                 ans.next = new ListNode(sum);
             }
@@ -41,38 +26,31 @@ class Solution {
             l2 = l2.next;
             ans = ans.next;
         }
-        while(l1 != null)
-        {
-            int sum = l1.val+rem;
-            if(sum > 9)
-            {
-                ans.next = new ListNode(sum%10);
-                rem = sum/10;
-            }
-            else{
+        while (l1 != null) {
+            int sum = l1.val + rem;
+            if (sum > 9) {
+                ans.next = new ListNode(sum % 10);
+                rem = sum / 10;
+            } else {
                 rem = 0;
                 ans.next = new ListNode(sum);
             }
             l1 = l1.next;
             ans = ans.next;
         }
-        while(l2 != null)
-        {
-            int sum = l2.val+rem;
-            if(sum > 9)
-            {
-                ans.next = new ListNode(sum%10);
-                rem = sum/10;
-            }
-            else{
+        while (l2 != null) {
+            int sum = l2.val + rem;
+            if (sum > 9) {
+                ans.next = new ListNode(sum % 10);
+                rem = sum / 10;
+            } else {
                 rem = 0;
                 ans.next = new ListNode(sum);
             }
             l2 = l2.next;
             ans = ans.next;
         }
-        if(rem != 0)
-        {
+        if (rem != 0) {
             ans.next = new ListNode(rem);
         }
         return org.next;
