b502d4a30a4d668a9fe3dd8fd63500dee7e39605
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-04-30 15:26:13 +0530

    Time: 1 ms (98.61%) | Memory: 41.5 MB (43.65%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char ch : s.toCharArray())
+        {
+            if(ch == ')' || ch == '}' || ch == ']')
+            {
+                if(stack.isEmpty()) return false;
+                if(ch == ')' && stack.peek() == '(') stack.pop();
+                else if(ch == '}' && stack.peek() == '{') stack.pop();
+                else if(ch == ']' && stack.peek() == '[') stack.pop();
+                else return false; 
+            }
+            else{
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
