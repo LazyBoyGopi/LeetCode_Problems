78b4e601836cbeb17fa41c1b5094f822806a6701
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-05-01 11:30:25 +0530

    Time: 9 ms (97.16%) | Memory: 47.5 MB (17.11%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,39 @@
+class Solution {
+    private int getSum(int []weights)
+    {
+        int sum = 0;
+        for(int ele : weights) sum += ele;
+        return sum;
+    }
+    private boolean isPossible(int[]weights,int capacity,int days)
+    {
+        
+        int weight = 0;
+        for(int ele : weights)
+        {
+            if(capacity < ele) return false;
+            if(weight+ele <= capacity)
+            {
+                weight += ele;
+            }
+            else{
+                days--;
+                weight = ele;
+            }
+        }
+        return days > 0;
+    }
+    public int shipWithinDays(int[] weights, int days) {
+        int low = 1 , high = getSum(weights);
+        while(low <= high)
+        {
+            int mid = low+(high-low)/2;
+            if(isPossible(weights,mid,days))
+            {
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
+        return low;
+    }
+}
