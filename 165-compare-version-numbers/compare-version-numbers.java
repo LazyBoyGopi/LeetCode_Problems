91b3df1d69ec1fb609b47411176462d87c85ff59
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-05-03 21:01:06 +0530

    Time: 1 ms (78.87%) | Memory: 41.2 MB (83.58%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.java
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -1,58 +1,42 @@
 class Solution {
-    private String removeLeadingZeores(String s)
+    private String removeLeadingZeroes(String str)
     {
-        for(int i=0;i<s.length();i++)
+        for(int i=0;i<str.length();i++)
         {
-            if(s.charAt(i) != '0') {
-                return s.substring(i,s.length());
-            }
+            if(str.charAt(i) != '0') return str.substring(i);
         }
         return "";
     }
+    private boolean checkOneVersion(String[]ar,int idx)
+    {
+        for(int i=idx;i<ar.length;i++)
+        {
+            String s = ar[idx].startsWith("0") ? removeLeadingZeroes(ar[i]) : ar[i];
+            int num = s.length() != 0 ? Integer.parseInt(s) : 0;
+            if(num > 0) return true;
+        }
+        return false;
+    }
     public int compareVersion(String version1, String version2) {
-        int l1 = 0 , l2 = 0 , last1 =0 , last2=0;
-        while(l1 < version1.length() && l2 < version2.length())
+        String ar1 [] = version1.split("\\.");
+        String ar2 [] = version2.split("\\.");
+        int l1 = 0 , l2 = 0;
+        while(l1 < ar1.length && l2 < ar2.length)
         {
-            while(l1 < version1.length() && version1.charAt(l1) != '.') l1++;
-            while(l2 < version2.length() && version2.charAt(l2) != '.') l2++;
-            String s1 = removeLeadingZeores(version1.substring(last1,l1));
-            String s2 = removeLeadingZeores(version2.substring(last2,l2));
-            int n = (s1.length() != 0 ? Integer.parseInt(s1) : 0) - (s2.length() != 0 ? Integer.parseInt(s2) : 0);
-            if(n < 0)
-                return -1;
-            if(n > 0) 
-                return 1;
-            else{
-                last1 = l1+1;
-                last2 = l2+1;
-                l1++;
-                l2++;
-            }
+            String s1 = ar1[l1].startsWith("0") ? removeLeadingZeroes(ar1[l1]) : ar1[l1];
+            String s2 = ar2[l2].startsWith("0") ? removeLeadingZeroes(ar2[l2]) : ar2[l2];
+            int num1 = s1.length() != 0 ? Integer.parseInt(s1) : 0;
+            int num2 = s2.length() != 0 ? Integer.parseInt(s2) : 0;
+            if(num1 > num2) return 1;
+            if(num1 < num2) return -1;
+            l1++;
+            l2++;
         }
-        while(l1 < version1.length())
-        {
-            while(l1 < version1.length() && version1.charAt(l1) != '.') l1++;
-            String s = removeLeadingZeores(version1.substring(last1,l1));
-            int n = s.length() != 0 ? Integer.parseInt(s) : 0;
-            if(n == 0)
-            {
-                last1 = l1+1;
-                l1++;
-            }
-            else if(n > 0) return 1;
+        if(l1 < ar1.length) {
+            if(checkOneVersion(ar1,l1)) return 1;
         }
-        while(l2 < version2.length())
-        {
-            while(l2 < version2.length() && version2.charAt(l2) != '.') l2++;
-            String s = removeLeadingZeores(version2.substring(last2,l2));
-            int n = s.length() != 0 ? Integer.parseInt(s) : 0;
-            if(n == 0)
-            {
-                last2 = l2+1;
-                l2++;
-            }
-            else if(n > 0) return -1;
-        }
+        if(l2 < ar2.length)
+             if(checkOneVersion(ar2,l2)) return -1 ;
         return 0;
     }
 }
