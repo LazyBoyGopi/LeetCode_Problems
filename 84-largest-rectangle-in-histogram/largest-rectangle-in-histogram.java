25c44417c1cacab78db3cebef869c492b4839f84
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-05-03 11:42:03 +0530

    Time: 64 ms (65.10%) | Memory: 57.5 MB (36.58%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,25 +1,17 @@
 class Solution {
-    private int getArea(int[]heights,int len)
-    {
-        Stack<Integer> stack = new Stack<>();
-        int max = 0;
-        int idx = 0;
-        while(idx < len){
-            if(stack.isEmpty() || heights[stack.peek()] <= heights[idx])
-            {
-                stack.push(idx++);
-            }
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer>stack = new Stack<>();
+        int idx = 0 , max = 0;
+        while(idx < heights.length)
+        {
+            if(stack.isEmpty() || heights[stack.peek()] <= heights[idx]) stack.push(idx++);
             else{
-                int area = heights[stack.pop()] * (stack.isEmpty() ? idx : idx-stack.peek()-1);
-                max = Math.max(max,area);
+                max = Math.max(max , (heights[stack.pop()] * (stack.isEmpty() ? idx : idx-stack.peek()-1)));
             }
         }
         while(!stack.isEmpty()){
-            max = Math.max(max,heights[stack.pop()] * (stack.isEmpty() ? idx : idx-stack.peek()-1));
+            max = Math.max(max,(heights[stack.pop()] * (stack.isEmpty() ? idx : idx-stack.peek()-1)));
         }
         return max;
-    }
-    public int largestRectangleArea(int[] heights) {
-        return getArea(heights,heights.length);
     }
 }
