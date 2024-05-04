e972d0b5e31fd83132f51bc492a1de123d33a102
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-05-04 20:52:33 +0530

    Time: 1 ms (99.72%) | Memory: 44.1 MB (5.48%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,19 +1,19 @@
 class Solution {
-    private void getAllSubsets(int[]nums,int idx,List<List<Integer>> list ,List<Integer>temp)
+    private void getAllSubsets(int[]nums,List<List<Integer>>list,int idx,List<Integer>temp)
     {
         list.add(new LinkedList<>(temp));
         for(int i=idx;i<nums.length;i++)
         {
             if(i > idx && nums[i] == nums[i-1]) continue;
             temp.add(nums[i]);
-            getAllSubsets(nums,i+1,list,temp);
+            getAllSubsets(nums,list,i+1,temp);
             temp.remove(temp.size()-1);
         }
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> list = new LinkedList<>();
-        getAllSubsets(nums,0,list,new LinkedList<>());
+        getAllSubsets(nums,list,0,new LinkedList<>());
         return list;
     }
 }
