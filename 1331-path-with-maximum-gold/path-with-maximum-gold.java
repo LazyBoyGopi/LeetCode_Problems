da960a9574353887e1610befc650ace3aef0bf8a
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-05-14 14:20:05 +0530

    Time: 60 ms (87.54%) | Memory: 41.1 MB (56.84%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.java
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.java
@@ -1,38 +1,29 @@
 class Solution {
-    private static void addAllPaths(int[][]ar,int row,int col,List<Integer>list,int sum){
-		if(ar[row][col] == 0 || (row < 0 || col < 0 || row >= ar.length || col >= ar[0].length)) {
-			list.add(sum);
+    int max = 0;
+    private  void addAllPaths(int[][]ar,int row,int col,int sum){
+		if((row < 0 || col < 0 || row >= ar.length || col >= ar[0].length) || ar[row][col] == 0) {
+			max = Math.max(sum,max);
 			return;
 		}
 		int ele = ar[row][col];
 		ar[row][col] = 0;
-		if(row > 0 ){
-			addAllPaths(ar,row-1,col,list,sum+ele);
-		}
-		if(row < ar.length-1){
-			addAllPaths(ar,row+1,col,list,sum+ele);
-		}
-		if(col < ar[0].length-1){
-			addAllPaths(ar,row,col+1,list,sum+ele);
-		}
-		if(col > 0){
-			addAllPaths(ar,row,col-1,list,sum+ele);
-		}
+		addAllPaths(ar,row-1,col,sum+ele);
+		addAllPaths(ar,row+1,col,sum+ele);
+		addAllPaths(ar,row,col+1,sum+ele);
+		addAllPaths(ar,row,col-1,sum+ele);
 		ar[row][col] = ele;
 	}
-	private static int getPathWithMaxGold(int[][]ar,List<Integer>list){
+	private  int getPathWithMaxGold(int[][]ar){
 		for(int i=0;i<ar.length;i++){
 			for(int j=0;j<ar[i].length;j++){
 				if(ar[i][j] != 0){
-					addAllPaths(ar,i,j,list,0);
+					 addAllPaths(ar,i,j,0);
 				}
 			}
 		}
-		int max = 0;
-		for(int ele : list) if(max < ele ) max = ele;
-		return max;
+        return max;
 	}
     public int getMaximumGold(int[][] grid) {
-        return getPathWithMaxGold(grid,new LinkedList<>());
+        return getPathWithMaxGold(grid);
     }
 }
