363ec3b6a3054ba3df06724c9d0f8552624ae63b
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-05-20 13:01:55 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (70.47%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -1,28 +1,16 @@
 class Solution {
-    private void getSubsets(int[]nums,List<List<Integer>>list,List<Integer>temp,int idx){
+    int sum = 0;
+    private void getSubsets(int[]nums,int xor,int idx){
         if(idx == nums.length){
-            list.add(new ArrayList(temp));
+            sum += xor;
             return;
         }
-        temp.add(nums[idx]);
-        getSubsets(nums,list,temp,idx+1);
-        temp.remove(temp.size()-1);
-        getSubsets(nums,list,temp,idx+1);
-    }
-    private int ans(int[]nums){
-        List<List<Integer>> list = new LinkedList<>();
-        getSubsets(nums,list,new ArrayList(),0);
-        int sum = 0;
-        for(List<Integer> l : list){
-            int xor = 0;
-            for(int i : l){
-                xor ^= i;
-            }
-            sum += xor;
-        }
-        return sum;
+        getSubsets(nums,xor,idx+1);
+        xor ^= nums[idx];
+        getSubsets(nums,xor,idx+1);
     }
     public int subsetXORSum(int[] nums) {
-        return ans(nums);
+        getSubsets(nums,0,0);
+        return sum;
     }
 }
