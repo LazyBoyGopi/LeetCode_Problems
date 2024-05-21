c45ad7df4a682abce7edbee15e87efedf44054e7
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-05-21 13:35:38 +0530

    Time: 1 ms (57.66%) | Memory: 42.8 MB (16.33%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,13 +1,11 @@
 class Solution {
     private void getSubsets(int[]nums,List<List<Integer>> list,List<Integer>temp,int idx){
-        if(idx == nums.length){
-            list.add(new ArrayList(temp));
-            return;
+        list.add(new LinkedList(temp));
+        for(int i=idx;i<nums.length;i++){
+            temp.add(nums[i]);
+            getSubsets(nums,list,temp,i+1);
+            temp.remove(temp.size()-1);
         }
-        temp.add(nums[idx]);
-        getSubsets(nums,list,temp,idx+1);
-        temp.remove(temp.size()-1);
-        getSubsets(nums,list,temp,idx+1);
     }
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> list = new LinkedList<>();
