259575cf737a43dcc48bb83487b0b522ab961053
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-05-22 13:14:26 +0530

    Time: 10 ms (25.47%) | Memory: 64.6 MB (5.01%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,29 +1,25 @@
 class Solution {
-    private boolean isPalindrome(String s){
-        if(s.length() == 0) return false;
-        int st = 0 , end = s.length()-1;
+    private boolean isPalindrome(String s,int st,int end){
         while(st <= end){
-            if(s.charAt(st) != s.charAt(end)) return false;
-            st++;
-            end--;
+            if(s.charAt(st++) != s.charAt(end--)) return false;
         }
         return true;
     }
-    private void getAns(String s, List<List<String>>list,List<String>temp,int idx){
+    private void getAns(String s, List<List<String>> list, List<String>temp,int idx){
         if(idx == s.length()){
-            list.add(new ArrayList(temp));
+            list.add(new LinkedList(temp));
             return;
         }
-        for(int i=idx+1;i<=s.length();i++){
-            if(isPalindrome(s.substring(idx,i))){
-                temp.add(s.substring(idx,i));
-                getAns(s,list,temp,i);
+        for(int i=idx;i<s.length();i++){
+            if(isPalindrome(s,idx,i)){
+                temp.add(s.substring(idx,i+1));
+                getAns(s,list,temp,i+1);
                 temp.remove(temp.size()-1);
             }
         }
     }
     public List<List<String>> partition(String s) {
-        List<List<String>> list = new LinkedList();
+        List<List<String>> list = new LinkedList<>();
         getAns(s,list,new LinkedList(),0);
         return list;
     }
