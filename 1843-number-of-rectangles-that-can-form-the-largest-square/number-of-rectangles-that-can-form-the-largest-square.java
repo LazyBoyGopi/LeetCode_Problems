b423f94521163b60dcf10a6d9c2e726b9dcb6b57
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-05-24 23:49:30 +0530

    Time: 2 ms (98.79%) | Memory: 44.9 MB (49.70%) - LeetSync

--- 1843-number-of-rectangles-that-can-form-the-largest-square/number-of-rectangles-that-can-form-the-largest-square.java
+++ 1843-number-of-rectangles-that-can-form-the-largest-square/number-of-rectangles-that-can-form-the-largest-square.java
@@ -1,13 +1,18 @@
 import java.util.*;
 class Solution {
     public int countGoodRectangles(int[][] rectangles) {
-        Map<Integer,Integer> map = new HashMap<>();
         int max = 0;
+        int count = 0;
         for(int[]ar : rectangles){
             int m = Math.min(ar[0],ar[1]);
-            map.put(m,map.getOrDefault(m,0)+1);
-            max = Math.max(max,m);
+           if(max < m){
+            max = m;
+            count = 1;
+           }
+           else if(max == m){
+            count++;
+           }
         }
-        return map.get(max);
+        return count;
     }
 }
