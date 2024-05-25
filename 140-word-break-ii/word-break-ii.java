f6d8c287efeee7d7b5fce7430d161d097b0b22ba
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-05-25 22:18:06 +0530

    Time: 1 ms (94.29%) | Memory: 41.3 MB (93.53%) - LeetSync

--- 140-word-break-ii/word-break-ii.java
+++ 140-word-break-ii/word-break-ii.java
@@ -1,26 +1,24 @@
 class Solution {
-    private void backTrack(String s,  List<String> wordDict,int idx,List<String>list,StringBuilder temp){
+    private void backTrack(String s,Set<String>wordDict,int idx,List<String>list,StringBuilder sb){
         if(idx >= s.length()){
-            list.add(temp.toString());
+            list.add(sb.toString());
             return;
         }
         for(int i=idx;i<s.length();i++){
-           
-                String str = s.substring(idx,i+1);
-                if(wordDict.contains(str)){
-                    int lenBeforeAppend = temp.length();
-                    if(temp.length() != 0)
-                        temp.append(" ");
-                    temp.append(str);
-                    backTrack(s,wordDict,i+1,list,temp);
-                    temp.delete(lenBeforeAppend,temp.length());
-                }
-           
+            String temp = s.substring(idx,i+1);
+            if(wordDict.contains(temp)){
+                int len = sb.length();
+                if(len != 0) sb.append(" ");
+                sb.append(temp);
+                backTrack(s,wordDict,i+1,list,sb);
+                sb.delete(len,sb.length());
+            }
         }
     }
     public List<String> wordBreak(String s, List<String> wordDict) {
-        List<String> list = new LinkedList<>();
-        backTrack(s,wordDict,0,list,new StringBuilder());
+        List<String>list = new LinkedList<>();
+        Set<String>set = new HashSet<>(wordDict);
+        backTrack(s,set,0,list,new StringBuilder());
         return list;
     }
 }
