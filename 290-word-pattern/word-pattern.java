a3647a35a5160ae586b561f48deb9ed288c9ac5b
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-05-27 20:27:52 +0530

    Time: 1 ms (75.77%) | Memory: 41.3 MB (38.04%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -1,25 +1,20 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         Map<Character,String> map = new HashMap<>();
-        String []ar = s.split(" ");
+        Map<String,Character> reverseMap = new HashMap<>();
+        String[]ar = s.split(" ");
         if(ar.length != pattern.length()) return false;
         for(int i=0;i<pattern.length();i++){
             char ch = pattern.charAt(i);
-            if(map.containsKey(ch) && !map.get(ch).equals(ar[i]))
-               { 
-                return false;}
-                else
-            map.put(ch,ar[i]);
-            
-        }
-        Map<String,Character> map1 = new HashMap<>();
-        for(int i=0;i<pattern.length();i++){
-            char ch = pattern.charAt(i);
-            if(map1.containsKey(ar[i]) && map1.get(ar[i]) != ch) {
-                return false;
+            String str = ar[i];
+            if(map.containsKey(ch)){
+                if(!map.get(ch).equals(str)) return false;
             }
-            else
-            map1.put(ar[i],ch);
+            else{
+                if(reverseMap.containsKey(str)) return false;
+                map.put(ch,str);
+                reverseMap.put(str,ch);
+            }
         }
         return true;
     }
