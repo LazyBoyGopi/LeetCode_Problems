fa30c11cc141e46c39947435a26b9dd1f7312dca
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-05-27 12:07:45 +0530

    Time: 1 ms (78.32%) | Memory: 41 MB (68.83%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
@@ -2,14 +2,20 @@ class Solution {
     public int specialArray(int[] nums) {
         int largest = Integer.MIN_VALUE;
         for(int ele : nums) if(ele > largest) largest = ele;
-        int i;
-        for(i=largest;i>=0;i--){
-            int count = 0;
+        int st = 0 , end = largest;
+        int count = 0 , mid = 0;
+        while(st<=end){
+            mid = st+(end-st)/2;
+            count = 0;
             for(int ele : nums){
-                if(ele >= i) count++;
+                if(ele >= mid) count++;
             }
-            if(i != 0 && count == i) return i;
+            if(mid != 0 && count == mid) return mid;
+            if(count > mid){
+                st = mid+1;
+            }
+            else end = mid-1;
         }
-        return i;
+        return mid == count ? end : -1;
     }
 }
