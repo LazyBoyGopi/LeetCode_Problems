8f7a7f887003fa47d5a49315d2e44995dea5db32
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-05-28 14:32:25 +0530

    Time: 17 ms (97.13%) | Memory: 59.9 MB (70.76%) - LeetSync

---
+++ 1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    public int minSetSize(int[] arr) {
+        int[]freq = new int[100001];
+        for(int ele : arr){
+            freq[ele]++;
+        }
+        Arrays.sort(freq);
+        int len = arr.length/2 , count =0 ,i;
+        for(i=freq.length-1;i>=0 && freq[i] != 0 ; i--){
+            if(count >= len) return freq.length-1-i; 
+            count += freq[i];
+        }
+        return freq.length-1-i; 
+    //     int len = arr.length;
+    //    Map<Integer,Integer> freqMap = new HashMap<>();
+    //    for(int ele : arr){
+    //     freqMap.put(ele,freqMap.getOrDefault(ele,0)+1);
+    //    } 
+    //    Collection<Integer> v = freqMap.values();
+    //     List<Integer> values = new LinkedList<>(v);
+    //     int count = 0 ,i;
+    //    Collections.sort(values);
+    //    for(i=values.size()-1;i>=0;i--){
+    //     if(count >= len/2) return values.size()-i-1;
+    //     count += values.get(i);
+    //    }
+    //    return values.size()-i-1;
+    }
+}
