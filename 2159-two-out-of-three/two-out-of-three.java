b94bb46360bb9c2442af007f936f6b81ab607a74
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-05-30 16:37:53 +0530

    Time: 4 ms (69.55%) | Memory: 44.5 MB (73.25%) - LeetSync

---
+++ 2159-two-out-of-three/two-out-of-three.java
@@ -0,0 +1,16 @@
+class Solution {
+    private void getNumber(int[]nums,int[]freq){
+        Set<Integer> set = new HashSet<>();
+        for(int ele : nums) set.add(ele);
+        for(int ele : set) freq[ele]++;
+    }
+    public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
+        int[]freq = new int[101];
+        List<Integer> list = new LinkedList<>();
+        getNumber(nums1,freq);
+        getNumber(nums2,freq);
+        getNumber(nums3,freq);
+        for(int i=0;i<101;i++) if(freq[i] >= 2) list.add(i);
+        return list;
+    }
+}
