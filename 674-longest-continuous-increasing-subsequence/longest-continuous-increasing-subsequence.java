0cbcc73b61684fff5cba351f5217b972307d51f4
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-05-31 23:47:38 +0530

    Time: 1 ms (99.69%) | Memory: 44 MB (92.79%) - LeetSync

--- 674-longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.java
+++ 674-longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.java
@@ -3,9 +3,8 @@ class Solution {
         if(nums.length <= 1) return 1;
         int left = 0 , right = 0 ,max = 0 , len = nums.length;
         while(right < len-1){
-            max = Math.max(max,right-left+1);
-
             if(!(nums[right] < nums[right+1])) {
+                max = Math.max(max,right-left+1);
                 left = right+1;
             }
             right++;
