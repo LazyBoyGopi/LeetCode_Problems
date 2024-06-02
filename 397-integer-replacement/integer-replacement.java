a549e3ff43553016bdc4a41819e1cbecc7b6ad82
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-02 17:42:43 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (37.23%) - LeetSync

--- 397-integer-replacement/integer-replacement.java
+++ 397-integer-replacement/integer-replacement.java
@@ -1,10 +1,14 @@
 class Solution {
-    private int integerReplacement1(long n){
+    private long integerReplacement1(long n,Map<Long,Long>dp){
         if(n <= 1) return 0;
-        if(n%2 == 0) return integerReplacement1(n/2)+1;
-        return Math.min(integerReplacement1(n+1),integerReplacement1(n-1))+1;
+        if(dp.containsKey(n)) return dp.get(n);
+        if(n%2 == 0) return integerReplacement1(n/2,dp)+1;
+        long min =  Math.min(integerReplacement1(n+1,dp),integerReplacement1(n-1,dp))+1;
+        dp.put(n,min);
+        return dp.get(n);
     }
     public int integerReplacement(int n) {
-        return integerReplacement1(n);
+        Map<Long,Long> dp = new HashMap();
+        return (int)integerReplacement1(n,dp);
     }
 }
