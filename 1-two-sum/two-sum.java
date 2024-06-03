217dcee32b1b95429b78d93209db9735dcfb2720
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-03 20:44:57 +0530

    Time: 45 ms (31.07%) | Memory: 44.5 MB (89.00%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,17 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] ans = new int[2];
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
+        ganesh : for(int i=0;i<nums.length;i++){
+            gopi : for(int j=i+1;j<nums.length;j++){
                 int tempTarget = nums[i] + nums[j];
                 if(tempTarget == target){
                     ans[0] = i;
                     ans[1] = j;
-                    break;
+                    break ganesh;
                 }
-            }
-            if(ans[0] != 0 || ans[1] != 0) {
-                break;
             }
         }
         return ans;
