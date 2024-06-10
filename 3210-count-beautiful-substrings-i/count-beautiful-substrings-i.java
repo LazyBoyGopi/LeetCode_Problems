f11d72d3e88b5258aa178501dc3416143a11cf68
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-10 19:33:35 +0530

    Time: 51 ms (74.29%) | Memory: 41.5 MB (98.10%) - LeetSync

--- 3210-count-beautiful-substrings-i/count-beautiful-substrings-i.java
+++ 3210-count-beautiful-substrings-i/count-beautiful-substrings-i.java
@@ -1,9 +1,9 @@
 class Solution {
     public int beautifulSubstrings(String s, int k) {
-        int count = 0;
-        for (int i = 0; i < s.length(); i++) {
+        int count = 0 , len = s.length();
+        for (int i = 0; i < len; i++) {
             int vowelCount = 0, constCount = 0;
-            for (int j = i; j < s.length(); j++) {
+            for (int j = i; j < len; j++) {
                 char ch = s.charAt(j);
                 if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') vowelCount++;
                 else constCount++;
