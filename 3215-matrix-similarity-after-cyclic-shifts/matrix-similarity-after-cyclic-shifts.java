5802833a8cc4cbf663550367bf33f819b36ede13
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-10 19:14:22 +0530

    Time: 11 ms (6.02%) | Memory: 44.4 MB (91.73%) - LeetSync

---
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean areSimilar(int[][] mat, int k) {
+        if(k % mat[0].length == 0) return true;
+        k %= mat[0].length;
+        for(int i=0;i<mat.length;i++){
+            int[]dup = new int[mat[0].length];
+            int j ,idx = 0;
+            for(j=k;j<mat[0].length;j++){
+                dup[idx++] = mat[i][j];
+            }
+            for(j=0;j<k;j++){
+                dup[idx++] = mat[i][j];
+            }
+            System.out.println(Arrays.toString(dup));
+            for(j=0;j<mat[0].length;j++){
+                if(mat[i][j] != dup[j]) return false;
+            }
+        }
+        return true;
+    }
+}
