f32787f1f6aa770770fef7cd92601b2eca5d928d
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-12 01:31:59 +0530

    Time: 2 ms (94.25%) | Memory: 43.1 MB (23.50%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -1,13 +1,11 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        int idx = 0;
        Set<Integer> set = new HashSet();
        for(int ele : arr){
         if(set.contains(ele*2) || (ele %2 == 0 && set.contains(ele/2))) {
             return true;
         }
         set.add(ele);
-        idx++;
        }
        return false;
     }
