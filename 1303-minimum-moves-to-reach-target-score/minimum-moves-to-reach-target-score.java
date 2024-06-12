b09f54d1f07e4162f2e4cd955d5af32de19bfe47
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-12 18:59:54 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (38.91%) - LeetSync

--- 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.java
+++ 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.java
@@ -1,15 +1,13 @@
 class Solution {
     public int minMoves(int target, int maxDoubles) {
-        int steps = 0;
+        int steps = 0 ;
         while(target != 1){
             if(maxDoubles > 0 && target %2 == 0){
-                target /=2;
                 maxDoubles--;
+                target /= 2;
             }
-            else{
-                if(maxDoubles == 0){
-                    return steps +(target-1); 
-                }
+            else {
+                if(maxDoubles == 0) return steps+(target-1);
                 target--;
             }
             steps++;
