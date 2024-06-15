3b1052dc959a4feb81beab52bbab431439ed4aa3
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-15 12:55:57 +0530

    Time: 100 ms (16.03%) | Memory: 62.8 MB (24.84%) - LeetSync

--- 502-ipo/ipo.java
+++ 502-ipo/ipo.java
@@ -1,43 +1,19 @@
-/*class Solution {
+class Solution {
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
-        List<List<Integer>>list = new LinkedList<>();
-        int totalProfit = w;
+        PriorityQueue<int[]> minCapitalHeap = new PriorityQueue<>(Comparator.comparingInt(a->a[0]));
+        PriorityQueue<Integer>maxProfitHeap = new PriorityQueue<Integer>(Collections.reverseOrder());
+        
         for(int i=0;i<profits.length;i++){
-            list.add(Arrays.asList(profits[i],capital[i]));
+            minCapitalHeap.add(new int[]{capital[i],profits[i]});
         }
-        Collections.sort(list,(a,b) -> Integer.compare(b.get(0),a.get(0)));
-       
-        while(k > 0){
-            boolean isPossible = false;
-            for(int i=0;i<list.size();i++){
-                if(list.get(i).get(1) <= w){
-                    w += list.get(i).get(0);
-                    totalProfit += list.get(i).get(0);
-                    list.remove(i);
-                    k--;
-                    isPossible = true;
-                    break;
-                }
-            }
-            if(!isPossible) break;
-        }
-        return totalProfit;
-    }
-} */
-class Solution{
-     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital)    {
-        PriorityQueue<int[]>minHeapCapital = new PriorityQueue<>(Comparator.comparingInt(a->a[0]));
-        PriorityQueue<Integer>maxHeapProfit = new PriorityQueue<>(Comparator.reverseOrder());
-        for(int i=0;i<profits.length;i++){
-            minHeapCapital.add(new int[]{capital[i],profits[i]});
-        }
+
         for(int i=0;i<k;i++){
-            while(!minHeapCapital.isEmpty() && minHeapCapital.peek()[0] <= w){
-                maxHeapProfit.add(minHeapCapital.poll()[1]);
+            while(!minCapitalHeap.isEmpty() && minCapitalHeap.peek()[0] <= w){
+                maxProfitHeap.add(minCapitalHeap.poll()[1]);
             }
-            if(maxHeapProfit.isEmpty()) break;
-            w += maxHeapProfit.poll();
+            if(maxProfitHeap.isEmpty()) break;
+            w += maxProfitHeap.poll();
         }
         return w;
-     }
+    }
 }
