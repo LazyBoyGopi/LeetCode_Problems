fe930c7eb1b3b8999ae977f0a44bdeed08e30b21
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-16 15:16:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (65.91%) - LeetSync

---
+++ 112-path-sum/path-sum.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private boolean recu(TreeNode root,int targetSum,int sum){
+        if(root == null) return false;
+        sum += root.val;
+        if(root.left == null && root.right == null){
+            return sum == targetSum;
+        }
+        if(recu(root.left,targetSum,sum)) return true;
+        return recu(root.right,targetSum,sum);
+    }
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null) return false;
+        return recu(root,targetSum,0);
+    }
+}
