c075471fdfc1aa4e36f428fb55bfac9310d560a8
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-16 11:45:58 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (95.47%) - LeetSync

--- 330-patching-array/patching-array.java
+++ 330-patching-array/patching-array.java
@@ -1,14 +1,14 @@
 class Solution {
     public int minPatches(int[] nums, int n) {
-        long sum = 0 ;
-        int idx = 0 , count = 0;
-        while(sum < n ){
-            if(idx < nums.length && nums[idx] <= sum +1){
+        long sum = 0;
+        int count = 0, idx = 0 , len = nums.length;
+        while(sum < n){
+            if(idx < len && nums[idx] <= sum+1){
                 sum += nums[idx++];
             }
             else{
+                sum +=(sum+1);
                 count++;
-                sum += (sum+1);
             }
         }
         return count;
