fdcc0f2fc31e43d5a8f854e59ef0aa8835b72a0e
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-18 11:49:48 +0530

    Time: 7 ms (39.10%) | Memory: 45.1 MB (18.29%) - LeetSync

--- 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.java
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.java
@@ -5,7 +5,6 @@ class Solution {
         for(int i=1;i<nums.length;i++){
             nums[i] += nums[i-1];
         }
-        System.out.println(Arrays.toString(nums));
         for(int i=0;i<queries.length;i++){
             int st = 0 , end = n-1;
             while(st <= end){
