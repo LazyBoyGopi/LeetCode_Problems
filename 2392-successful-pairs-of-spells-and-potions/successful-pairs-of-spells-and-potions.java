0a0875886bb1ee665d7bc9feb86cdb842a421553
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-18 11:06:44 +0530

    Time: 38 ms (93.74%) | Memory: 64.4 MB (57.08%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.java
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.java
@@ -1,27 +1,16 @@
 class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
         Arrays.sort(potions);
-        Map<Integer,List<Integer>>map = new HashMap<>();
-        for(int i=0;i<spells.length;i++){
-           if(map.containsKey(spells[i])){
-                map.get(spells[i]).add(i);
+        int n = spells.length , m = potions.length;
+        for(int i=0;i<n;i++){
+            int st = 0 , end = m-1;
+            while(st <= end){
+                int mid = st+(end-st)/2;
+                if((long)spells[i]*potions[mid] >= success) end = mid-1;
+                else st = mid+1;
             }
-            else
-            map.put(spells[i],new LinkedList<>(Arrays.asList(i)));
+            spells[i] = m-st;
         }
-        Arrays.sort(spells);
-        int[]pairs = new int[spells.length];
-        int lastIdx = -1 , len = potions.length;
-        for(int i=0;i<spells.length;i++){
-            for(int j=lastIdx == -1 ? potions.length-1 : lastIdx;j>=0;j--){
-                if((long)potions[j]*spells[i] >= success) {
-                    lastIdx = j;
-                }
-                else break;
-            }
-            pairs[map.get(spells[i]).get(0)] = lastIdx == -1 ? 0 : len - lastIdx;
-            map.get(spells[i]).remove(0);
-        }
-        return pairs;
+        return spells;
     }
 }
