b9e118db75a188fa3de8c10344973faca08d0358
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-18 10:25:06 +0530

    Time: 580 ms (9.82%) | Memory: 45.6 MB (58.84%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.java
+++ 853-most-profit-assigning-work/most-profit-assigning-work.java
@@ -1,18 +1,23 @@
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        PriorityQueue<int[]>diff = new PriorityQueue<>(Comparator.comparingInt(a->a[0]));
-        PriorityQueue<Integer>maxProfit = new PriorityQueue<>(Comparator.reverseOrder());
+        int[][]ar = new int[difficulty.length][2];
         for(int i=0;i<difficulty.length;i++){
-            diff.add(new int[]{difficulty[i],profit[i]});
+            ar[i][0] = difficulty[i];
+            ar[i][1] = profit[i];
         }
-        int total = 0;
+        Arrays.sort(ar,(a,b) -> a[0]-b[0]);
         Arrays.sort(worker);
+        int total = 0;
+        int bestSoFar = 0;
+        int lastIdx = 0;
         for(int i=0;i<worker.length;i++){
-            while(!diff.isEmpty() && diff.peek()[0] <= worker[i]){
-                maxProfit.add(diff.poll()[1]);
+            for(int j=lastIdx;j<ar.length;j++){
+                if(worker[i] >= ar[j][0]){
+                    lastIdx = j;
+                    bestSoFar = Math.max(bestSoFar,ar[j][1]);
+                }
             }
-            if(!maxProfit.isEmpty())
-            total += maxProfit.peek();
+            total += bestSoFar;
         }
         return total;
     }
