1fd0d33bed1b8a49c51c70c2329d7c1dcce267b6
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-19 08:49:47 +0530

    Time: 26 ms (13.00%) | Memory: 57.7 MB (63.78%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,49 +1,30 @@
 class Solution {
-    private int findMax(int[]bloomDay)
-    {
-        int max = Integer.MIN_VALUE;
-        for(int ele : bloomDay)
-        {
-            max = Math.max(ele,max);
-        }
-        return max;
-    }
-    private boolean isPossible(int[]bloomDay,int days,int bouquets,int numberOfFlowers)
-    {
+    private boolean isPossible(int[]bloomDay,int m,int k,int mid){
         int count = 0;
-        for(int ele : bloomDay)
-        {
-            if(count >= numberOfFlowers)
-            {
-                bouquets--;
-                count = 0;
-            }
-            if(ele - days <= 0)
-            {
+        for(int i=0;i<bloomDay.length;i++){
+            if(bloomDay[i] - mid <= 0){ 
                 count++;
             }
             else{
                 count = 0;
             }
-            if(bouquets <= 0) return true;
+            if(count == k) {
+                count = 0;
+                m--;
+                if(m == 0) return true;
+            }
         }
-        if(count >= numberOfFlowers) bouquets--;
-        return bouquets <= 0;
+        return m <= 0;
     }
     public int minDays(int[] bloomDay, int m, int k) {
-        if(bloomDay.length < (m*k)) return -1;
-        int low = 0 , high = findMax(bloomDay);
-        int ans = -1;
-        while(low <= high)
-        {
-            int mid = low+(high-low)/2;
-            if(isPossible(bloomDay,mid,m,k))
-            {
-                ans = mid;
-                high = mid-1;
-            }
-            else low = mid+1;
+        int st = 0 , end = 0;
+        for(int ele : bloomDay) end = Math.max(end,ele);
+        int max = end;
+        while(st <= end){
+            int mid = st+(end-st)/2;
+            if(isPossible(bloomDay,m,k,mid)) end = mid-1;
+            else st = mid+1;
         }
-        return ans;
+        return end ==max ? -1 : st;
     }
 }
