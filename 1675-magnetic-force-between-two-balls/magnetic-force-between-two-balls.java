ca035361ef3485bb6ed627e4361dce09b9c7d32f
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-20 20:21:28 +0530

    Time: 43 ms (58.58%) | Memory: 55.9 MB (37.12%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.java
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.java
@@ -1,25 +1,24 @@
 class Solution {
-    private boolean isPossible(int[]position,int m,int dis){
-        int last = position[0];
+    private boolean isPossible(int[]position,int m,int day){
         m--;
+        int last = position[0];
         for(int i=1;i<position.length;i++){
-            if(position[i]-last>= dis) {
-                m--;
-                if(m == 0) return true;
+            if(position[i]-last >= day) {
                 last = position[i];
+                m--;
+                if(m <= 0) return true;
             }
         }
-        return m <= 0;
+        return false;
     }
     public int maxDistance(int[] position, int m) {
         Arrays.sort(position);
-        int st = 1 , end = position[position.length-1]-position[0];
+        int len = position.length, st = 1 , end = position[len-1]-position[0];
         while(st <= end){
-            int dis = st+(end-st)/2;
-            if(isPossible(position,m,dis)){
-                st = dis+1;
-            }
-            else end = dis-1;
+            int day = st+(end-st)/2;
+            if(isPossible(position,m,day)) st = day+1;
+            else
+                end = day-1;
         }
         return end;
     }
