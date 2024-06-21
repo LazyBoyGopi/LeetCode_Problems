df417ba78aa06b02f8cd7f9635b7c57972ff3fb0
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-21 12:17:01 +0530

    Time: 7 ms (14.93%) | Memory: 47.5 MB (33.68%) - LeetSync

---
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.java
@@ -0,0 +1,49 @@
+class Solution {
+    private int[]getIdxForRange(int[]customers,int[]grumpy,int minutes){
+        int[]idx = {-1,-1};
+        int left = 0 , noOfGrumpy = 0 , max = 0 , sum = 0 , maxGrumpy = 0;
+        for(int i=0;i<minutes;i++){
+            if(grumpy[i] == 1) {
+                noOfGrumpy++;
+                sum += customers[i];
+            }
+        }
+        if(noOfGrumpy > 0){
+            maxGrumpy = noOfGrumpy;
+            max = sum;
+            idx[0] = 0;
+            idx[1] = minutes-1;
+        }
+        for(int i=minutes;i<customers.length;i++){
+            if(grumpy[i-minutes] == 1) {
+                noOfGrumpy--;
+                sum -= customers[i-minutes];
+            }
+            if(grumpy[i] == 1) {
+                noOfGrumpy++;
+                sum += customers[i];
+            }
+            if(grumpy[i] == 1 && (sum > max)){
+                maxGrumpy = noOfGrumpy;
+                max = sum;
+                idx[0] = i-minutes+1;
+                idx[1] = i; 
+            }
+        }
+        return idx;
+    }
+    private int getAns(int[]customers,int[]grumpy,int[]idx){
+        int max = 0 , st = idx[0] , end = idx[1];
+        for(int i=0;i<customers.length;i++){
+            if(grumpy[i] == 0 ||(st==-1 || (i>=st && i<= end))){
+                max += customers[i];
+            }
+        }
+        return max;
+    }
+    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+        int[]idx = getIdxForRange(customers,grumpy,minutes);
+        System.out.println(Arrays.toString(idx));
+        return getAns(customers,grumpy,idx);
+    }
+}
