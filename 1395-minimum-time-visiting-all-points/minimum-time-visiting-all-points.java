d778509ff50a29219b5860a345c1e435725cae5f
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-21 19:22:31 +0530

    Time: 1 ms (92.06%) | Memory: 42.9 MB (37.55%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.java
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.java
@@ -4,8 +4,7 @@ class Solution {
         for(int i=0;i<points.length-1;i++){
             int verDis = Math.abs(points[i+1][0] - points[i][0]);
             int horDis = Math.abs(points[i+1][1] - points[i][1]);
-            totalDis += Math.min(verDis,horDis);
-            totalDis += (verDis == horDis) ? 0 : Math.abs(verDis-horDis);
+            totalDis += Math.min(verDis,horDis) + Math.abs(verDis-horDis);
         }   
         return totalDis;
     }
