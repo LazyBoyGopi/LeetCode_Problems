612a9d4814c684ae6084dcad7c91d30bffdb93c1
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-21 18:41:03 +0530

    Time: 4 ms (100.00%) | Memory: 45.9 MB (28.73%) - LeetSync

---
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxCount(int[] banned, int n, int maxSum) {
+
+        boolean[]freq = new boolean[(int)1e4+1];
+        for(int i=0;i<banned.length;i++){
+            freq[banned[i]] = true;
+        }
+
+        int len = freq.length , sum = 0 , count = 0;
+        for(int i=1;i<=n && i<len ;i++){
+            if(!freq[i]) {
+                if((sum+i) <= maxSum){
+                    sum += i;
+                    count++;
+                }
+                else break;
+            }
+        }
+        return count;
+    }
+
+}
