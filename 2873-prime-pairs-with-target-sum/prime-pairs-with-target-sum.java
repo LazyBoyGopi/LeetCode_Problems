ac47bad28ecb255d0761529831b6c6bc598dcfc1
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-21 20:06:05 +0530

    Time: 1123 ms (6.67%) | Memory: 57.8 MB (5.19%) - LeetSync

--- 2873-prime-pairs-with-target-sum/prime-pairs-with-target-sum.java
+++ 2873-prime-pairs-with-target-sum/prime-pairs-with-target-sum.java
@@ -1,30 +1,35 @@
 class Solution {
     int dp[] = new int[(int)1e6+1];
-    private boolean isPrime(int num){
-        if(dp[num] == 1) return true;
-        if(dp[num] == -1) return false;
-        if(num == 2) return true;
-        if(num%2 == 0) {
-            dp[num] = -1;
-            return false;
-        }
-        int sqrt = (int)Math.sqrt(num)+1;
+
+    private boolean isPrime(int n){
+        if(dp[n] == -1) return false;
+        if(dp[n] == 1) return true;
+        if(n == 2) return true;
+        if(n%2 == 0) return false;
+        int sqrt = (int)Math.sqrt(n)+1;
         for(int i=3;i<=sqrt;i+=2){
-            if(num%i == 0) {
-                dp[num] = -1;
-                return false;
-            }
+            if(n%i == 0) return false;
         }
-        dp[num] = 1;
         return true;
     }
+    private void checkPrime(int n){
+        int sqrt = (int)Math.sqrt(n);
+        for(int i=2;i<=sqrt;i++){
+            if(dp[i] == 0){
+                if(isPrime(i)){
+                    dp[i] = 1;
+                }
+                else dp[i] = -1;
+            }
+        }
+    }
     public List<List<Integer>> findPrimePairs(int n) {
-        List<List<Integer>> list = new LinkedList<>();
+        checkPrime(n);
+        List<List<Integer>>list = new LinkedList<>();
         for(int i=2;i<=n/2;i++){
             if(isPrime(i)){
-                int comp = n-i;
-                if(isPrime(comp)){
-                    list.add(new ArrayList<>(Arrays.asList(i,comp)));
+                if(isPrime(n-i)){
+                    list.add(Arrays.asList(i,n-i));
                 }
             }
         }
