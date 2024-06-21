1732dd1788d4f6c2a4812f278951340137183e31
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-21 12:26:45 +0530

    Time: 3 ms (91.49%) | Memory: 47.8 MB (12.15%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.java
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.java
@@ -1,48 +1,29 @@
 class Solution {
-    private int[]getIdxForRange(int[]customers,int[]grumpy,int minutes){
-        int[]idx = {-1,-1};
-        int left = 0 , noOfGrumpy = 0 , max = 0 , sum = 0 , maxGrumpy = 0;
+    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+        int ans = 0 , maxGrumpySum = 0 , curSum = 0 ,lastSum = 0;
         for(int i=0;i<minutes;i++){
-            if(grumpy[i] == 1) {
-                noOfGrumpy++;
-                sum += customers[i];
+            if(grumpy[i] == 1){
+                curSum += customers[i];
             }
+            else ans += customers[i];
         }
-        if(noOfGrumpy > 0){
-            maxGrumpy = noOfGrumpy;
-            max = sum;
-            idx[0] = 0;
-            idx[1] = minutes-1;
+        if(maxGrumpySum < curSum){
+            maxGrumpySum = curSum;
+            ans += curSum;
+            lastSum = curSum;
         }
         for(int i=minutes;i<customers.length;i++){
-            if(grumpy[i-minutes] == 1) {
-                noOfGrumpy--;
-                sum -= customers[i-minutes];
+            if(grumpy[i-minutes] == 1) curSum -= customers[i-minutes];
+            if(grumpy[i] == 1) curSum += customers[i];
+            else ans += customers[i];
+            if(maxGrumpySum < curSum){
+                ans -= lastSum;
+                lastSum = curSum;
+                ans += curSum;
+                maxGrumpySum = curSum;
             }
-            if(grumpy[i] == 1) {
-                noOfGrumpy++;
-                sum += customers[i];
-            }
-            if(grumpy[i] == 1 && (sum > max)){
-                maxGrumpy = noOfGrumpy;
-                max = sum;
-                idx[0] = i-minutes+1;
-                idx[1] = i; 
-            }
+            
         }
-        return idx;
-    }
-    private int getAns(int[]customers,int[]grumpy,int[]idx){
-        int max = 0 , st = idx[0] , end = idx[1];
-        for(int i=0;i<customers.length;i++){
-            if(grumpy[i] == 0 ||(st==-1 || (i>=st && i<= end))){
-                max += customers[i];
-            }
-        }
-        return max;
-    }
-    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
-        int[]idx = getIdxForRange(customers,grumpy,minutes);
-        return getAns(customers,grumpy,idx);
+        return ans;
     }
 }
