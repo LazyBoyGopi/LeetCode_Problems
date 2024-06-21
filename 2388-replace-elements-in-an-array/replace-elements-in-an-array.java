c35b9d8d23ab42b2841c3cb9eaada071c19a1170
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-21 19:04:57 +0530

    Time: 14 ms (96.00%) | Memory: 82.1 MB (97.78%) - LeetSync

--- 2388-replace-elements-in-an-array/replace-elements-in-an-array.java
+++ 2388-replace-elements-in-an-array/replace-elements-in-an-array.java
@@ -1,14 +1,13 @@
 class Solution {
     public int[] arrayChange(int[] nums, int[][] operations) {
-        Map<Integer,Integer>map = new HashMap();
+        int[]idx = new int[(int)1e6+1];
         for(int i=0;i<nums.length;i++){
-            map.put(nums[i],i);
+            idx[nums[i]] = i;
         }
-        for(int i=0;i<operations.length;i++){
-            int idx = map.get(operations[i][0]);
-            int ele = operations[i][1];
-            nums[idx] = ele;
-            map.put(ele,idx);
+        for(int []ar : operations){
+            int index = idx[ar[0]];
+            nums[index] = ar[1];
+            idx[ar[1]] = index;
         }
         return nums;
     }
