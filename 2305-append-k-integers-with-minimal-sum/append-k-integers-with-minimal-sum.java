47f517755ea027af9a217dc731b0baa0cc95e43e
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-21 18:31:06 +0530

    Time: 20 ms (92.26%) | Memory: 61.5 MB (34.52%) - LeetSync

--- 2305-append-k-integers-with-minimal-sum/append-k-integers-with-minimal-sum.java
+++ 2305-append-k-integers-with-minimal-sum/append-k-integers-with-minimal-sum.java
@@ -1,21 +1,50 @@
 class Solution {
     public long minimalKSum(int[] nums, int k) {
-        long sum = 0;
+        int idx = 0;
+        long ans = 0;
         Arrays.sort(nums);
-        for(int i=1;i<nums[0] && k>0;i++){
-            sum += i;
-            k--;
-        }
-        for (int i = 0; i < nums.length - 1 && k > 0; i++) {
-            for (int j = nums[i] + 1; j < nums[i + 1] && k > 0; j++) {
-                sum += j;
-                k--;
+        if (idx == 0 && nums[idx] != 1) {
+            int first = 1, last = nums[idx] - 1;
+            int diff = last - first + 1;
+            if (diff >= k) {
+                last = first + k-1;
+                long sum = ((long) k * (first + last)) / 2;
+                ans += sum;
+                return ans;
+            } else {
+                long sum = ((long) diff * (first + last)) / 2;
+                ans += sum;
+                k -= diff;
             }
         }
-        for(int i=nums[nums.length-1]+1;k>0;i++) {
-            k--;
-            sum += i;
+        while (k > 0) {
+            if (idx < nums.length - 1) {
+                int diff = nums[idx + 1] - nums[idx] - 1;
+                long sum;
+                if (diff > 0) {
+                    if (diff >= k) {
+                        int first = nums[idx] + 1;
+                        int last = nums[idx] + k;
+                        sum = ((long) k * (first + last)) / 2;
+                        ans += sum;
+                        return ans;
+                    } else {
+                        int first = nums[idx] + 1;
+                        int last = nums[idx + 1] - 1;
+                        sum = ((long) diff * (first + last)) / 2;
+                        k -= diff;
+                        ans += sum;
+                    }
+                }
+                idx++;
+            } else {
+                int firstNum = nums[idx] + 1;
+                int lastNum = nums[idx] + k;
+                long sum = ((long) k * (lastNum + firstNum)) / 2;
+                ans += sum;
+                return ans;
+            }
         }
-        return sum;
+        return ans;
     }
 }
