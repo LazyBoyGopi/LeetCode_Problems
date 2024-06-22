77d87a1afe4a4237236ba21b8f46230ebfbc8cda
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-22 17:37:05 +0530

    Time: 27 ms (17.68%) | Memory: 52.8 MB (6.59%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,13 +1,14 @@
 class Solution {
-    public int numSubarraysWithSum(int[] nums, int goal) {
-        int[]ar = new int[3*10000+1];
-        ar[0] = 1;
+    public int numSubarraysWithSum(int[] nums, int k) {
+        Map<Integer,Integer>map = new HashMap<>();
+        map.put(0,1);
         int sum = 0 , subarrayCount = 0;
         for(int i=0;i<nums.length;i++){
             sum += nums[i];
-            if(sum-goal >= 0) subarrayCount += ar[sum-goal];
-            ar[sum]++;
-
+            if(map.containsKey(sum-k)) {
+                subarrayCount += map.get(sum-k);
+            }
+            map.put(sum,map.getOrDefault(sum,0)+1);
         }
         return subarrayCount;
     }
