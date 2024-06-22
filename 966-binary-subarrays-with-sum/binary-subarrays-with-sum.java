e4701dbb4fd5d37875b0ee284481932e473ff551
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-22 17:32:54 +0530

    Time: 4 ms (45.76%) | Memory: 46.8 MB (99.02%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,15 +1,14 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        int sum = 0 , count = 0;
-        for(int i=0;i<nums.length;i++)
-        {
-            sum = 0;
-            for(int j=i;j<nums.length;j++)
-            {
-                sum += nums[j];
-                if(sum == goal) count++;
-            }
+        int[]ar = new int[3*10000+1];
+        ar[0] = 1;
+        int sum = 0 , subarrayCount = 0;
+        for(int i=0;i<nums.length;i++){
+            sum += nums[i];
+            if(sum-goal >= 0) subarrayCount += ar[sum-goal];
+            ar[sum]++;
+
         }
-        return count;
+        return subarrayCount;
     }
 }
