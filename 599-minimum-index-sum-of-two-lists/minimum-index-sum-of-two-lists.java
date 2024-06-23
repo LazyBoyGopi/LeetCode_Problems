6a0a16a8960044624e05fc7a502ce1f754414fb2
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-23 18:34:12 +0530

    Time: 112 ms (12.81%) | Memory: 45.3 MB (56.37%) - LeetSync

---
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        Map<Integer,List<String>>map = new HashMap<>();
+        for(int i=0;i<list1.length;i++){
+            String str = list1[i];
+            for(int j=0;j<list2.length;j++){
+                if(str.equals(list2[j])){
+                    if(map.containsKey(i+j)){
+                        List<String>l = map.get(i+j);
+                        l.add(str);
+                        map.put(i+j,l);
+                    }else{
+                        map.put(i+j,new LinkedList(Arrays.asList(str)));
+                    }
+                }
+            }
+        }
+        int min = Integer.MAX_VALUE;
+        for(Integer key : map.keySet()){
+            min = Math.min(min,key);
+        }
+        List<String>list = map.get(min);
+        String[]ans = new String[list.size()];
+        for(int i=0;i<ans.length;i++){
+            ans[i] = list.get(i);
+        }
+        return ans;
+    }
+}
