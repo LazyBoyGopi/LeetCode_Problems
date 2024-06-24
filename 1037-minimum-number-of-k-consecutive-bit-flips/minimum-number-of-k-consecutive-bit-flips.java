f255db6ab18dd0b17523c0036826ee796ab5e772
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-24 11:14:45 +0530

    Time: 5 ms (88.39%) | Memory: 60.1 MB (10.97%) - LeetSync

--- 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.java
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.java
@@ -1,19 +1,16 @@
 class Solution {
     public int minKBitFlips(int[] nums, int k) {
-        int flipCount = 0 , len = nums.length , validFlipsFromLastWindow = 0;
-        Deque<Boolean>flipped = new LinkedList<>();
+        int len = nums.length , flipCount = 0 , validFilpsInLastWindow = 0;
+        boolean[] flipped = new boolean[len];
         for(int i=0;i<len;i++){
             if(i >= k){
-                if(flipped.poll()) validFlipsFromLastWindow--;
+                if(flipped[i-k]) validFilpsInLastWindow--;
             }
-            if(validFlipsFromLastWindow %2 == nums[i]){
+            if(validFilpsInLastWindow % 2 == nums[i]){
                 if(i+k > len) return -1;
-                validFlipsFromLastWindow++;
-                flipped.addLast(true);
+                validFilpsInLastWindow++;
                 flipCount++;
-            }
-            else{
-                flipped.offer(false);
+                flipped[i] = true;
             }
         }
         return flipCount;
