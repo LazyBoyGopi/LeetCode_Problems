80d47eba9f083d4bd7160f0c18b60839bc04a9ca
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-24 12:08:21 +0530

    Time: 1 ms (100.00%) | Memory: 46.5 MB (34.68%) - LeetSync

--- 1254-deepest-leaves-sum/deepest-leaves-sum.java
+++ 1254-deepest-leaves-sum/deepest-leaves-sum.java
@@ -14,31 +14,25 @@
  * }
  */
 class Solution {
-    private void getSum(TreeNode root,Map<Integer,List<Integer>>map,int level){
-        if(root == null) return ;
+    int sum = 0;
+    int maxLevel = -1;
+    private void getSum(TreeNode root,int level){
+        if(root == null) return;
         if(root.left == null && root.right == null) {
-            if(map.containsKey(level)){
-                map.get(level).add(root.val);
-            }else{
-                map.put(level,new ArrayList<>(Arrays.asList(root.val)));
+            if(level > maxLevel){
+                sum = root.val;
+                maxLevel = level;
             }
+            else if(level == maxLevel) 
+                sum += root.val;
             return;
         }
-        getSum(root.left,map,level+1);
-        getSum(root.right,map,level+1);
+        getSum(root.left,level+1);
+        getSum(root.right,level+1);
     }
     public int deepestLeavesSum(TreeNode root) {
-        Map<Integer,List<Integer>>map = new HashMap<>();
-        getSum(root,map,0);
-        int max = -1 , maxSum = 0;
-        for(Map.Entry<Integer,List<Integer>> entry : map.entrySet()){
-            if(entry.getKey() > max){
-                maxSum = 0;
-                for(int ele : entry.getValue()){
-                    maxSum += ele;
-                }
-            }
-        }
-        return maxSum;
+        sum = 0;
+        getSum(root,0);
+        return sum;
     }
 }
