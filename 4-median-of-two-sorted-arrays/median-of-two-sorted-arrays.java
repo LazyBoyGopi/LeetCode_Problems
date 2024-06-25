f2d2d07bf26819afd12533865bf5aef333756caa
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-25 23:48:51 +0530

    Time: 1 ms (100.00%) | Memory: 46.2 MB (15.10%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,39 +1,27 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int len1 = nums1.length , len2 = nums2.length , e1 = -1 , e2 = -1 , i = 0 , j = 0 , m2 = (len1+len2)/2 , m1 = m2-1 , c1 = 0 ;
-        while(i < len1 && j < len2){
-            if(nums1[i] < nums2[j]){
-                if(c1 == m1) e1 = nums1[i];
-                if(c1 == m2) e2 = nums1[i];
-                i++;
+        int n1 = nums1.length , n2 = nums2.length ;
+        if(n1 > n2) return findMedianSortedArrays(nums2,nums1);
+        int st = 0 , end = n1;
+        int n = n1+n2;
+        int half = (n1+n2+1) >> 1;
+        while(st <= end){
+            int mid1 = (st+end) >> 1;
+            int mid2 = half - mid1;
+            
+            int l1 = mid1 > 0 ?  nums1[mid1-1] : Integer.MIN_VALUE ;
+            int l2 = mid2 > 0 ?  nums2[mid2-1] : Integer.MIN_VALUE ;
+            int r1 = mid1 < n1 ?  nums1[mid1] : Integer.MAX_VALUE ;
+            int r2 = mid2 < n2 ?  nums2[mid2] : Integer.MAX_VALUE ;
+
+            if(l1 <= r2 && l2 <= r1) {
+                if(n % 2 == 1) return Math.max(l1,l2);
+                return ((double)Math.max(l1,l2)+(double)Math.min(r1,r2))/2.0;
             }
-            else{
-                if(c1 == m1) e1 = nums2[j];
-                if(c1 == m2) e2 = nums2[j];
-                j++;
-            }
-            c1++;
-            if(e1 != -1 && e2 != -1) break;
+            else if(l1 > r2) end = mid1-1;
+            else st = mid1+1;
+
         }
-        while(i < len1){
-            if(c1 == m1) e1 = nums1[i];
-            if(c1 == m2) e2 = nums1[i];
-            i++;
-            c1++;
-            if(e1 != -1 && e2 != -1) break;
-        }
-        while(j < len2){
-            if(c1 == m1) e1 = nums2[j];
-            if(c1 == m2) e2 = nums2[j];
-            j++;
-            c1++;
-            if(e1 != -1 && e2 != -1) break;
-        }
-        if((len1+len2) %2 == 0){
-            if(e1 == -1) return e2;
-            if(e2 == -1) return e1;
-            return ((double)e1+e2)/2;
-        }
-        return e2;
+        return 0.0;
     }
 }
