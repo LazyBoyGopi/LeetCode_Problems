1bd021ca3b9d1f1753033a34cf92cb3b63c8321d
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-25 13:17:48 +0530

    Time: 33 ms (73.79%) | Memory: 45.3 MB (13.59%) - LeetSync

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.java
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.java
@@ -1,34 +1,18 @@
 class Solution {
-    private int getAns(String str,Set<String>set ,int idx){
-        if(idx == str.length()){
-            return set.size();
-        }
+    private int getAns(String s,Set<String>set,int idx){
+        if(idx == s.length()) return set.size();
         int max = 0;
-        for(int right = idx+1;right <= str.length() ;right++){
-            String curStr = str.substring(idx,right);
-            if(!set.contains(curStr)){
-                set.add(curStr);
-                max = Math.max(max,getAns(str,set,right));
-                set.remove(curStr);
+        for(int right = idx+1;right <= s.length();right++){
+            String cur = s.substring(idx,right);
+            if(!set.contains(cur)){
+                set.add(cur);
+                max = Math.max(getAns(s,set,right),max);
+                set.remove(cur);
             }
         }
         return max;
     }
     public int maxUniqueSplit(String s) {
-       /* Set<String>str = new HashSet<>();
-        Set<String>str2 = new HashSet<>();
-        int left = 0 , right = 1 , len = s.length();
-        while(right < len){
-            String curStr = s.substring(left,right);
-            if(!str.contains(curStr)){
-                str.add(curStr);
-                left = right;
-            }
-            right++;
-        }
-        String curStr = s.substring(left,right);
-        if(!str.contains(curStr)) str.add(curStr);
-        return str.size(); */
         return getAns(s,new HashSet<>(),0);
     }
 }
