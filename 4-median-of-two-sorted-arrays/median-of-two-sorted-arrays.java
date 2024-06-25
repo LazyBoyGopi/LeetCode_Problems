a372b69ce0a2339eb770408ee159bc9efe4d2e91
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-25 19:13:22 +0530

    Time: 1 ms (100.00%) | Memory: 46.4 MB (7.05%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,25 +1,39 @@
 class Solution {
-    private int[]merge(int[]nums1,int[]nums2){
-        int idx1 = 0 , idx2 = 0 , idx = 0 , len1 = nums1.length , len2 = nums2.length;
-        int[]nums = new int[len1+len2];
-        while(idx1 < len1 && idx2 < len2){
-            if(nums1[idx1] > nums2[idx2]){
-                nums[idx++] = nums2[idx2++];
-            }else{
-                nums[idx++] = nums1[idx1++];
-            }
-        }
-        while(idx1 < len1) nums[idx++] = nums1[idx1++];
-        while(idx2 < len2) nums[idx++] = nums2[idx2++];
-        return nums;
-    }
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int []nums = merge(nums1,nums2);
-        if(nums.length % 2 == 0){
-            int mid = nums.length/2;
-            double sum = (double)nums[mid]+nums[mid-1];
-            return sum/2;
+        int len1 = nums1.length , len2 = nums2.length , e1 = -1 , e2 = -1 , i = 0 , j = 0 , m2 = (len1+len2)/2 , m1 = m2-1 , c1 = 0 ;
+        while(i < len1 && j < len2){
+            if(nums1[i] < nums2[j]){
+                if(c1 == m1) e1 = nums1[i];
+                if(c1 == m2) e2 = nums1[i];
+                i++;
+            }
+            else{
+                if(c1 == m1) e1 = nums2[j];
+                if(c1 == m2) e2 = nums2[j];
+                j++;
+            }
+            c1++;
+            if(e1 != -1 && e2 != -1) break;
         }
-        return nums[nums.length/2];
+        while(i < len1){
+            if(c1 == m1) e1 = nums1[i];
+            if(c1 == m2) e2 = nums1[i];
+            i++;
+            c1++;
+            if(e1 != -1 && e2 != -1) break;
+        }
+        while(j < len2){
+            if(c1 == m1) e1 = nums2[j];
+            if(c1 == m2) e2 = nums2[j];
+            j++;
+            c1++;
+            if(e1 != -1 && e2 != -1) break;
+        }
+        if((len1+len2) %2 == 0){
+            if(e1 == -1) return e2;
+            if(e2 == -1) return e1;
+            return ((double)e1+e2)/2;
+        }
+        return e2;
     }
 }
