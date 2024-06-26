092f0e53acca3098cba179ed0d33be32bc341849
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-06-26 15:49:47 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (81.53%) - LeetSync

--- 1651-shuffle-string/shuffle-string.java
+++ 1651-shuffle-string/shuffle-string.java
@@ -1,13 +1,9 @@
 class Solution {
     public String restoreString(String s, int[] indices) {
-       char[] ar = new char[101];
+       char[] ar = new char[s.length()];
        for(int i=0; i<indices.length;i++){
         ar[indices[i]] = s.charAt(i);
        }
-       StringBuilder sb = new StringBuilder();
-       for(char ch : ar){
-        if(ch >= 'a' && ch <= 'z') sb.append(ch);
-       }
-       return sb.toString();
+       return new String(ar);
     }
 }
