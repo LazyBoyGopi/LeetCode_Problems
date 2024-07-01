cce375f0929b742afe7f4d088fad1502bf65cf95
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-07-01 08:41:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (93.34%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.java
+++ 1293-three-consecutive-odds/three-consecutive-odds.java
@@ -2,15 +2,12 @@ class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
         if(arr.length < 3) return false;
         int count = 0;
-        for(int i=0;i<3;i++){
-            if(arr[i] % 2 == 1) count++;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] % 2 == 1) {
+                count++;
+                if(count == 3) return true;
+            }
             else count = 0;
-        }
-        if(count == 3) return true;
-        for(int i=3;i<arr.length;i++){
-            if(arr[i] % 2 == 1) count++;
-            else count = 0;
-            if(count == 3) return true;
         }
         return false;
     }
