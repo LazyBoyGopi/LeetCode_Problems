1635575c4444846dfa74d1dd72b1905b65e8ef1b
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-07-08 13:00:33 +0530

    Time: 49 ms (6.99%) | Memory: 44.4 MB (10.99%) - LeetSync

---
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findTheWinner(int n, int k) {
+        List<Integer>list = new LinkedList<>();
+        for(int i=1;i<=n;i++) list.add(i);
+        int stIdx = 0;
+        while(list.size() != 1){
+            int removeIdx  = k+stIdx-1;
+            if(removeIdx >= list.size()){
+                int count = k;
+                removeIdx = stIdx;
+                while(count > 1){
+                    removeIdx = (removeIdx+1 >= list.size()) ? 0 : removeIdx+1;
+                    count--;
+                }
+            } 
+            stIdx = (removeIdx == list.size()-1) ? 0 : removeIdx;
+            System.out.println(list.remove(removeIdx)+" "+stIdx);
+        }
+        return list.get(0);
+    }
+}
