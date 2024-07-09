812347731e766c539efd1babe0dfb5b326f0e5a3
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-07-09 19:19:53 +0530

    Time: 4 ms (42.86%) | Memory: 73.1 MB (18.78%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.java
+++ 1803-average-waiting-time/average-waiting-time.java
@@ -1,16 +1,16 @@
 class Solution {
     public double averageWaitingTime(int[][] customers) {
-        double total = customers[0][1];
-        int busyTill = customers[0][0]+customers[0][1];
-        for(int i=1;i<customers.length;i++){
-            if(customers[i][0] < busyTill){
-                total += busyTill-customers[i][0];
-                busyTill += customers[i][1];
+        double total = 0;
+        int busyTill = -1;
+        for(int ar[] : customers){
+            if(ar[0] < busyTill){
+                total += busyTill-ar[0];
+                busyTill += ar[1];
             }else{
-                busyTill = customers[i][0]+customers[i][1];
+                busyTill = ar[0]+ar[1];
             }
-            total += customers[i][1];
+            total += ar[1];
         }
-        return (double)total/(double)customers.length;
+        return total/customers.length;
     }
 }
