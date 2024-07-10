ffaab1f792965f3ba55fd7d48f5c73638fb8ef7d
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-07-10 15:19:03 +0530

    Time: 1 ms (99.42%) | Memory: 42.4 MB (13.66%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.java
+++ 1720-crawler-log-folder/crawler-log-folder.java
@@ -2,15 +2,10 @@ class Solution {
     public int minOperations(String[] logs) {
         int len = 0;
         for(String str : logs){
-            switch(str){
-                case "../" : {
-                    if(len > 0) len--;
-                }
-                break;
-                case "./" : 
-                break;
-                default : len++; 
+            if(str.charAt(1) == '.') {
+                if(len > 0) len--;
             }
+            else if(str.charAt(0) != '.') len++;
         }
         return len;
     }
