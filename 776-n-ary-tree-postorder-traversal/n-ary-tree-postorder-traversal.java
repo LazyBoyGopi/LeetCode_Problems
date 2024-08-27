ca45935cb8c588f2ddcbb2c435967bd48dc62e92
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-08-27 08:04:57 +0530

    Time: 1 ms (33.70%) | Memory: 44.8 MB (35.59%) - LeetSync

---
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.java
@@ -0,0 +1,35 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    private static void getList(Node root,List<Integer>list){
+        if(root == null) return;
+        List<Node>childrens = root.children;
+        for(Node children : childrens){
+            getList(children,list);
+            list.add(children.val);
+        }
+    }
+    public List<Integer> postorder(Node root) {
+        List<Integer>list = new LinkedList<>();
+        getList(root,list);
+        if(root != null) list.add(root.val);
+        return list;
+    }
+}
