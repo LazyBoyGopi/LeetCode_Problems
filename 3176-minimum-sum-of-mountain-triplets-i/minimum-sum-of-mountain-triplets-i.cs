1162fa41ea911cd98ab5449e905d5ed4e0e321b2
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-08-31 17:09:42 +0530

    Time: 65 ms (58.33%) | Memory: 42 MB (98.33%) - LeetSync

---
+++ 3176-minimum-sum-of-mountain-triplets-i/minimum-sum-of-mountain-triplets-i.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    private int Check(int[]nums,int idx){
+        int val1 = 51;
+        for(int i=0;i<idx;i++){
+            if(nums[i] < nums[idx]) val1 = Math.Min(val1,nums[i]);
+        }
+        int val2 = 51;
+        for(int i=idx+1;i<nums.Length;i++){
+            if(nums[i] < nums[idx]) val2 = Math.Min(val2,nums[i]);
+        }
+        return (val1 != 51 && val2 != 51) ? val1+val2 : 0;
+    }
+    public int MinimumSum(int[] nums) {
+        int min = 151;
+        for(int i=0;i<nums.Length;i++){
+            int curMin=Check(nums,i);
+            if(curMin != 0){
+                min = Math.Min(min,curMin+nums[i]);
+                Console.WriteLine(i);
+            }
+        }
+        return min != 151 ? min : -1;
+    }
+}
