6e9c3ae43e123f73f379317bbe4f7d7ff219c3a1
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-09-13 11:54:27 +0530

    Time: 13 ms (27.16%) | Memory: 55 MB (99.27%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
@@ -1,17 +1,26 @@
 class Solution {
-    private int getXOROf(int st,int end,int[]arr){
-        int xor =0;
-        for(int i=st;i<=end;i++){
-            xor ^= arr[i];
-        }
-        return xor;
-    }
     public int[] xorQueries(int[] arr, int[][] queries) {
         int[]answer = new int[queries.length];
-        int idx = 0;
-        for(int []query : queries){
-            answer[idx++] = getXOROf(query[0],query[1],arr);
+        int[]xor = new int[arr.length];
+        xor[0] = arr[0];
+
+        for(int i=1;i<arr.length;i++){
+            xor[i] = xor[i-1] ^ arr[i];
         }
+
+        System.out.println(Arrays.toString(xor));
+
+        for(int i=0;i<queries.length;i++){
+            int st = queries[i][0];
+            int end = queries[i][1];
+
+            if(st == end) answer[i] = arr[st];
+            else if(st == 0) answer[i] = xor[end];
+            else{
+                answer[i] = xor[end] ^ xor[st-1];
+            }
+        }
+
         return answer;
     }
 }
