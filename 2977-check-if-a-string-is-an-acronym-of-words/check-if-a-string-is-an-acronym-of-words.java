6e58cc12fa03bb7bbc5125b3ab907ec84ff56e31
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-09-19 10:42:38 +0530

    Time: 1 ms (100.00%) | Memory: 43.9 MB (99.30%) - LeetSync

--- 2977-check-if-a-string-is-an-acronym-of-words/check-if-a-string-is-an-acronym-of-words.java
+++ 2977-check-if-a-string-is-an-acronym-of-words/check-if-a-string-is-an-acronym-of-words.java
@@ -1,7 +1,8 @@
 class Solution {
     public boolean isAcronym(List<String> words, String s) {
         if(words.size() != s.length()) return false;
-        for(int i=0;i<s.length();i++){
+        int len = s.length();
+        for(int i=0;i<len;i++){
             if(s.charAt(i) != words.get(i).charAt(0)) return false;
         }
         return true;
