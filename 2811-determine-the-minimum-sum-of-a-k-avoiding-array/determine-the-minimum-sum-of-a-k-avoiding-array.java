a4aefd218b68fbfe880f67e7415324afc8e12baf
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-09-19 11:02:35 +0530

    Time: 4 ms (72.97%) | Memory: 43.8 MB (60.81%) - LeetSync

--- 2811-determine-the-minimum-sum-of-a-k-avoiding-array/determine-the-minimum-sum-of-a-k-avoiding-array.java
+++ 2811-determine-the-minimum-sum-of-a-k-avoiding-array/determine-the-minimum-sum-of-a-k-avoiding-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public int minimumSum(int n, int k) {
-        int sumTillN = (n*(n+1)/2);
-        if(sumTillN < k) return sumTillN;
+        int sumTillN = (n+n-1);
+        if(sumTillN < k) return (n*(n+1)/2);
 
         Set<Integer>set = new HashSet<>();
         int sum = 0, count = 0;
