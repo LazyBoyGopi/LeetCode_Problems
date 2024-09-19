265b21ab8f64cc8811798f1419cdfdabf14d3afb
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-09-19 11:20:28 +0530

    Time: 1 ms (100.00%) | Memory: 41.2 MB (97.97%) - LeetSync

--- 2811-determine-the-minimum-sum-of-a-k-avoiding-array/determine-the-minimum-sum-of-a-k-avoiding-array.java
+++ 2811-determine-the-minimum-sum-of-a-k-avoiding-array/determine-the-minimum-sum-of-a-k-avoiding-array.java
@@ -1,10 +1,10 @@
 class Solution {
     public int minimumSum(int n, int k) {
-       if(n+n-1 < k || k == 1 || k == 2) return (n*(n+1))/2;
-       int half = k/2;
-       int firstHalf = (half*(half+1))/2;
-       int i = k, j = k+(n-half-1);
-       int secondHalf = ((i+j) *(j-i+1))/2;
-       return firstHalf + secondHalf;
+        if(n+n-1 < k || k ==1 || k==2) return (n*(n+1))/2;
+        int half = k/2;
+        int firstHalf = (half*(half+1))/2;
+        int i = k , j = k + (n-half-1);
+        int secondHalf = ((i+j) * (j-i+1))/2;
+        return firstHalf + secondHalf;
     }
 }
