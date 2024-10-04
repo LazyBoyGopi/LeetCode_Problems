1ecad5a2d40badba4a62ec47e4afe07ec1cacd97
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-10-04 15:49:57 +0530

    Time: 2 ms (68.36%) | Memory: 40.3 MB (65.53%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.java
@@ -0,0 +1,53 @@
+class Solution {
+    int numberOfWays = 0;
+
+    private void canWePlaceNQueens(int n, int[][] mat, int curRow) {
+        if (curRow == n) {
+            numberOfWays++;
+            return;
+        }
+        outer : for (int j = 0; j < n; j++) {
+
+            int curCol = j;
+
+            for (int i = 0; i < n; i++) {
+                if (mat[curRow][i] == 1)
+                    continue outer;
+            }
+
+            for (int i = 0; i < n; i++) {
+                if (mat[i][curCol] == 1)
+                     continue outer;
+            }
+
+            for(int r=curRow,c=curCol; r>=0 && c>=0; r--,c--){
+                if(mat[r][c] == 1) continue outer;
+            }
+
+            for(int r=curRow,c=curCol; r<n && c<n; r++,c++){
+                if(mat[r][c] == 1) continue outer;
+            }
+
+            for(int r=curRow,c=curCol; r<n && c>=0; r++,c--){
+                if(mat[r][c] == 1) continue outer;
+            }
+
+            for(int r=curRow,c=curCol; r>=0 && c<n; r--,c++){
+                if(mat[r][c] == 1) continue outer;
+            }
+
+
+            mat[curRow][curCol] = 1;
+            canWePlaceNQueens(n, mat, curRow + 1);
+            mat[curRow][curCol] = 0;
+
+        }
+    }
+
+    public int totalNQueens(int n) {
+        numberOfWays = 0;
+        int[][] mat = new int[n][n];
+        canWePlaceNQueens(n, mat, 0);
+        return numberOfWays;
+    }
+}
