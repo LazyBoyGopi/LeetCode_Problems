ba8f83d5fe139c3c9418e3147a9e174a5808fcf6
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-10-05 16:42:59 +0530

    Time: 5 ms (53.16%) | Memory: 43.2 MB (68.22%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
-        int[]ar = new int[26];
-        for(int i=0;i<s.length();i++){
-            ar[s.charAt(i)-'a']++;
+        int sl = s.length(), tl = t.length();
+        if(sl != tl) return false;
+        int []freq = new int[26];
+        for(int i=0;i<sl;i++){
+            freq[s.charAt(i)-'a']++;
+            freq[t.charAt(i)-'a']--;
         }
-        for(int i=0;i<t.length();i++){
-            ar[t.charAt(i)-'a']--;
-        }
+
         for(int i=0;i<26;i++){
-            if(ar[i] != 0) return false;
+            if(freq[i] != 0) return false;
         }
         return true;
     }
