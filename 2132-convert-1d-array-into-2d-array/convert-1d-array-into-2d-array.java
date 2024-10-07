61ca51266c87f01a5e668484979ca4b182e31b0a
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-10-07 13:47:35 +0530

    Time: 4 ms (63.09%) | Memory: 56 MB (6.62%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
@@ -3,7 +3,8 @@ class Solution {
         if(m*n != original.length) return new int[0][0];
         int[][]ar = new int[m][n];
         int col = 0, row=0;
-        for(int ele : original){
+        for(int i=0;i<original.length;i++){
+            int ele = original[i];
             if(col == n) {
                 col = 0;
                 row++;
