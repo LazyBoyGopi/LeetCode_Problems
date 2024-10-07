e54f9a936db06292e2b2f55de19e38cab1910f58
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-10-07 11:02:28 +0530

    Time: 1 ms (96.41%) | Memory: 41.8 MB (23.90%) - LeetSync

--- 1923-sentence-similarity-iii/sentence-similarity-iii.java
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.java
@@ -15,44 +15,18 @@ class Solution {
         }
         if(idx1 == s1.length || idx2 == s2.length) return true;
 
-        idx1 = s1.length-1;
-        idx2 = s2.length-1;
-
-        while(idx1 >=0 && idx2 >=0 ){
-           if(s1[idx1].equals(s2[idx2])) {
-                idx1--;
-                idx2--;
-            }else break;
-        }
-        if(idx1 == -1 || idx2 == -1) return true;
-
-        idx1 = 0;
-        idx2 = 0;
-
         int lastIdx1 = s1.length-1;
         int lastIdx2 = s2.length-1;
-        while(idx1 <= lastIdx1 && idx2 <= lastIdx2){
-            if(s1[idx1].equals(s2[idx2])){
-                idx1++;
-                idx2++;
-            }else break;
-        }
 
-        while(idx1 <= lastIdx1 && idx2 <= lastIdx2){
-            if(s1[lastIdx1].equals(s2[lastIdx2])){
+        while(lastIdx1 >=0 && lastIdx2 >=0 ){
+           if(s1[lastIdx1].equals(s2[lastIdx2])) {
                 lastIdx1--;
                 lastIdx2--;
             }else break;
         }
+        if(lastIdx1 == -1 || lastIdx2 == -1) return true;
 
         if(idx1 > lastIdx1 || idx2 > lastIdx2) return true;
         return false;
-
-       /* int lastIdx = s1.length-1;
-        boolean checkingStringS1 = true;
-
-        while(idx1 <s1.length && idx2 < s2.length && (checkingStringS1 ? (idx1 <= lastIdx ? true : false):(idx2 <= lastIdx)? true : false)){
-
-        } */
     } 
 }
