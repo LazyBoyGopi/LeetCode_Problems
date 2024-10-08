4064c90755f5694efe44370355bb2878da174dcf
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-10-08 13:05:44 +0530

    Time: 68 ms (63.46%) | Memory: 74 MB (5.77%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cs
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cs
@@ -1,13 +1,11 @@
 public class Solution {
     public int MinSwaps(string s) {
-        Stack<char>stack = new Stack<char>();
+        int brackets = 0;
         foreach(char ch in s.ToCharArray()){
-            if(ch == ']' && stack.Count()>0 && stack.Peek() == '['){
-                stack.Pop();
-            }else{
-                stack.Push(ch);
-            }
+            if(ch == ']' && brackets > 0){
+                brackets--;
+            }else brackets++;
         }
-        return (stack.Count()/2+1)/2;
+        return (brackets+1)/2;
     }
 }
