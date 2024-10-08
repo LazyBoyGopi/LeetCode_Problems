6667c18eea845a5539e1215b6c6be949d32a5e3c
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-10-08 13:22:10 +0530

    Time: 50 ms (41.77%) | Memory: 39.2 MB (6.33%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int MinAddToMakeValid(string s) {
+        int opening = 0, closing = 0;
+        foreach(char ch in s.ToCharArray()){
+            if(ch == ')'){
+                if(opening > 0)
+                    opening--;
+                else closing++;
+            }else{
+                opening++;
+            }
+        }
+        Console.WriteLine($"{opening},{closing}");
+        return opening+closing;
+    }
+}
