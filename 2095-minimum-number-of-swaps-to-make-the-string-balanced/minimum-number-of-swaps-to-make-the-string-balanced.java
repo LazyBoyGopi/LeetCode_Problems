2e7e6aba902df584f34f67fc3b60103db6a17d71
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-10-08 13:03:12 +0530

    Time: 14 ms (76.82%) | Memory: 54 MB (76.92%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -1,7 +1,8 @@
 class Solution {
     public int minSwaps(String s) {
-        int opening = 0, closing = 0;
-        for(char ch : s.toCharArray()){
+        int opening = 0, closing = 0, len = s.length();
+        for(int i=0;i<len;i++){
+            char ch = s.charAt(i);
             if(ch == ']'){
                 if(opening > 0)
                     opening--;
