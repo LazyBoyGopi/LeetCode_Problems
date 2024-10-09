5c3dacd4ceb5e744c1aacb13329d3c8420374ced
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-10-09 09:48:04 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (8.99%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -1,14 +1,11 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        int opening = 0, closing = 0;
-        for(char ch : s.toCharArray()){
-            if(ch == ')'){
-                if(opening > 0)
-                    opening--;
+        int opening = 0, closing = 0, len = s.length();
+        for(int i=0;i<len;i++){
+            if(s.charAt(i) ==')')
+                if(opening > 0) opening--;
                 else closing++;
-            }else{
-                opening++;
-            }
+            else opening++;
         }
         return opening+closing;
     }
