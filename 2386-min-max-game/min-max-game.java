600f25b8b51830f77e37accc5f6bfd10f0b66926
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-10-09 16:33:49 +0530

    Time: 50 ms (6.21%) | Memory: 44.9 MB (6.65%) - LeetSync

--- 2386-min-max-game/min-max-game.java
+++ 2386-min-max-game/min-max-game.java
@@ -1,14 +1,14 @@
 class Solution {
     public int minMaxGame(int[] nums) {
-        while(nums.length != 1){
-            int[]newNums = new int[nums.length/2];
-            for(int i=0;i<newNums.length;i++){
-                if(i%2 == 0)
-                newNums[i] = Math.min(nums[2*i],nums[2*i+1]);
-                else
-                newNums[i] = Math.max(nums[2*i],nums[2*i+1]);
+        int len = nums.length;
+        while(len != 1){
+            len /= 2;
+            for(int i=0;i<len;i++){
+                System.out.println((i ^ 2));
+                if(i%2  == 0) nums[i] = Math.min(nums[2*i],nums[2*i+1]);
+                else nums[i] = Math.max(nums[2*i],nums[2*i+1]);
             }
-            nums = newNums;
+            System.out.println(Arrays.toString(nums));
         }
         return nums[0];
     }
