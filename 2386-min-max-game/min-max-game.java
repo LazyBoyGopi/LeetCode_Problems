1407023bf96dfaf40a066c6583b14038031ce220
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-10-09 16:34:02 +0530

    Time: 1 ms (63.87%) | Memory: 43.1 MB (69.80%) - LeetSync

--- 2386-min-max-game/min-max-game.java
+++ 2386-min-max-game/min-max-game.java
@@ -4,11 +4,9 @@ class Solution {
         while(len != 1){
             len /= 2;
             for(int i=0;i<len;i++){
-                System.out.println((i ^ 2));
                 if(i%2  == 0) nums[i] = Math.min(nums[2*i],nums[2*i+1]);
                 else nums[i] = Math.max(nums[2*i],nums[2*i+1]);
             }
-            System.out.println(Arrays.toString(nums));
         }
         return nums[0];
     }
