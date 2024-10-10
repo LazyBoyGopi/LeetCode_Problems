041cf2659069e3997e720ce373b8b334001eafdd
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-10-10 10:46:10 +0530

    Time: 57 ms (7.96%) | Memory: 54.8 MB (40.55%) - LeetSync

---
+++ 1002-maximum-width-ramp/maximum-width-ramp.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxWidthRamp(int[] nums) {
+        int maxRamp = 0, len = nums.length;
+        List<Integer>stack = new LinkedList();
+        stack.add(0);
+        for(int i=0;i<len;i++){
+            int eleAtI = nums[i];
+            if(nums[stack.get(stack.size()-1)]>eleAtI){
+                stack.add(i);
+            }else{
+                int idx = stack.size()-1;
+                while(idx >= 0 && nums[stack.get(idx)] <= eleAtI){
+                    maxRamp = Math.max(maxRamp,i-stack.get(idx--));
+                }
+            }
+        }
+        System.out.println(stack);
+        return maxRamp;
+    }
+}
