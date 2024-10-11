f79318e4227c98cad98484782159acf957ac55e0
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-10-11 15:44:24 +0530

    Time: 48 ms (63.16%) | Memory: 48.3 MB (84.80%) - LeetSync

--- 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.java
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.java
@@ -2,19 +2,23 @@ class Solution {
     public int smallestChair(int[][] times, int targetFriend) {
         Queue<int[]> leavingTime = new PriorityQueue<>((a,b)->Integer.compare(a[0],b[0]));
         Queue<Integer>availableChairs = new PriorityQueue<>();
-        for(int i=0;i<times.length;i++){
+        int len = times.length;
+        for(int i=0;i<len;i++){
             availableChairs.add(i);
         }
         int targetFrndArriveTime = times[targetFriend][0];
         Arrays.sort(times,(a,b)->Integer.compare(a[0],b[0]));
 
-        for(int time[] : times){
-            while(!leavingTime.isEmpty() && leavingTime.peek()[0] <= time[0]) {
+        for(int i=0;i<len;i++){
+            int time[] = times[i];
+            int arriveTime = time[0];
+            while(!leavingTime.isEmpty() && leavingTime.peek()[0] <= arriveTime) {
                 availableChairs.add(leavingTime.poll()[1]);
             }
+
             int curChair = availableChairs.poll();
             leavingTime.add(new int[]{time[1],curChair});
-            if(targetFrndArriveTime == time[0]) return curChair;
+            if(targetFrndArriveTime == arriveTime) return curChair;
         }
         return -1;
     }
