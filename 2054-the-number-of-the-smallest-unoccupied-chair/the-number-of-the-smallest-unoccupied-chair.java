77bc25498e7f5dbdf5809ef414eeb6f5b394d3b9
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-10-11 15:42:17 +0530

    Time: 48 ms (63.16%) | Memory: 48.8 MB (67.84%) - LeetSync

--- 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.java
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.java
@@ -1,34 +1,20 @@
 class Solution {
-    class Pair {
-        public int time;
-        public int chairNumber;
-
-        public Pair(int leavingTime_, int chairNumber_) {
-            time = leavingTime_;
-            chairNumber = chairNumber_;
-        }
-    }
-
     public int smallestChair(int[][] times, int targetFriend) {
-
-        Queue<Pair> leavingTimes = new PriorityQueue<>((a, b) -> {
-            return a.time - b.time;
-        });
-        Queue<Integer> availableChairs = new PriorityQueue<>();
-       for(int i=0;i<times.length;i++) availableChairs.add(i);
+        Queue<int[]> leavingTime = new PriorityQueue<>((a,b)->Integer.compare(a[0],b[0]));
+        Queue<Integer>availableChairs = new PriorityQueue<>();
+        for(int i=0;i<times.length;i++){
+            availableChairs.add(i);
+        }
         int targetFrndArriveTime = times[targetFriend][0];
-        Arrays.sort(times, (a, b) -> a[0] - b[0]);
+        Arrays.sort(times,(a,b)->Integer.compare(a[0],b[0]));
 
-        int chairNum = 0;
-        for (int[] time : times) {
-            while (!leavingTimes.isEmpty() && leavingTimes.peek().time <= time[0]) {
-                availableChairs.add(leavingTimes.poll().chairNumber);
+        for(int time[] : times){
+            while(!leavingTime.isEmpty() && leavingTime.peek()[0] <= time[0]) {
+                availableChairs.add(leavingTime.poll()[1]);
             }
-            int chairAval = availableChairs.poll();
-            leavingTimes.add(new Pair(time[1],chairAval));
-            if (targetFrndArriveTime == time[0]) {
-                return chairAval;
-            }
+            int curChair = availableChairs.poll();
+            leavingTime.add(new int[]{time[1],curChair});
+            if(targetFrndArriveTime == time[0]) return curChair;
         }
         return -1;
     }
