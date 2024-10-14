38e31bd4955a6a0cd2970fa297ab2067d35aeb55
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-10-14 09:59:52 +0530

    Time: 152 ms (58.77%) | Memory: 63.4 MB (15.64%) - LeetSync

---
+++ 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long maxKelements(int[] nums, int k) {
+        PriorityQueue<Integer>queue = new PriorityQueue(Comparator.reverseOrder());
+        for(int ele : nums) queue.offer(ele);
+        long score = 0;
+        while(k > 0){
+            int curScore = queue.poll();
+            queue.offer((int)Math.ceil((curScore*1.0)/3));
+            score += curScore;
+            k--;
+        }
+        return score;
+    }
+}
