23cfb17e8837a147168105b4dfd954f25bcee0d6
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-10-21 20:40:29 +0530

    Time: 35 ms (51.63%) | Memory: 44.9 MB (72.55%) - LeetSync

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.java
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.java
@@ -1,18 +1,22 @@
 class Solution {
-    private int getAns(String s,Set<String>set,int idx){
-        if(idx == s.length()) return set.size();
-        int max = 0;
-        for(int right = idx+1;right <= s.length();right++){
-            String cur = s.substring(idx,right);
-            if(!set.contains(cur)){
-                set.add(cur);
-                max = Math.max(getAns(s,set,right),max);
-                set.remove(cur);
-            }
+    int maxCount = 0;
+    private void checkAllCombinations(String s, Set<String>set,int idx){
+        if(idx >= s.length()){
+            maxCount = Math.max(maxCount,set.size());
+            return;
         }
-        return max;
+        for(int i=idx+1;i<=s.length();i++){
+            String curStr = s.substring(idx,i);
+           if(!set.contains(curStr)){
+                 set.add(curStr);
+            checkAllCombinations(s,set,i);
+            set.remove(curStr);
+           }
+        }
     }
     public int maxUniqueSplit(String s) {
-        return getAns(s,new HashSet<>(),0);
+        maxCount = 0;
+        checkAllCombinations(s,new HashSet(),0);
+        return maxCount;
     }
 }
