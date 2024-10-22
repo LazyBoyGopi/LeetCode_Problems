a42851c0aec405ee66908078e9ff0762bd29ff9e
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-10-22 13:47:39 +0530

    Time: 41 ms (22.18%) | Memory: 77.4 MB (8.19%) - LeetSync

--- 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.java
+++ 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.java
@@ -14,21 +14,18 @@
  * }
  */
 class Solution {
-    private void getAllSums(TreeNode root, Map<Integer,Long>sums, int curLevel){
+    private void getAllSums(TreeNode root, List<Long>sums, int curLevel){
         if(root == null) return;
-        sums.put(curLevel,sums.getOrDefault(curLevel,0l)+root.val);
+        if(sums.size() == curLevel) sums.add(0L);
+        sums.set(curLevel,sums.get(curLevel)+root.val);
         getAllSums(root.left,sums,curLevel+1);
         getAllSums(root.right,sums,curLevel+1);
     }
     public long kthLargestLevelSum(TreeNode root, int k) {
-        Map<Integer,Long>sums = new HashMap();
+        List<Long>sums = new ArrayList<>();
         getAllSums(root,sums,0);
-        List<Long> list = new LinkedList();
-        for(Long sum : sums.values()){
-            list.add(sum);
-        }
-        Collections.sort(list);
-        if(list.size() < k) return -1;
-        return list.get(list.size()-k);
+        if(sums.size() < k) return -1;
+        Collections.sort(sums,Collections.reverseOrder());
+        return sums.get(k-1);
     }
 }
