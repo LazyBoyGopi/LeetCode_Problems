9d7ad275d7d455792dfc77bbf31e96d4a11d0035
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-10-25 10:37:06 +0530

    Time: 36 ms (95.10%) | Memory: 54.5 MB (88.57%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.java
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.java
@@ -1,16 +1,20 @@
 class Solution {
-    private boolean doesContains(List<String>list,String str){
-        for(String curStr : list){
-            if(str.startsWith(curStr) && str.charAt(curStr.length()) == '/') return true;
-        }
-        return false;
-    }
     public List<String> removeSubfolders(String[] folder) {
+
         Arrays.sort(folder);
         List<String>list = new ArrayList();
-        for(String str : folder){
-            if(!doesContains(list,str)) list.add(str);
+        int len = folder.length;
+
+        String lastStr = folder[0];
+        for(int i=1;i<len;i++){
+            String curStr = folder[i];
+            if(curStr.startsWith(lastStr) &&  curStr.charAt(lastStr.length()) == '/') {
+                continue;
+            }
+            list.add(lastStr);
+            lastStr = curStr;
         }
+        if(list.size() == 0 || !list.get(list.size()-1).equals(lastStr)) list.add(lastStr);
         return list;
     }
 }
