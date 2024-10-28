e949e45bc9028ba719e9a2f3b941ddc14a925312
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-10-28 11:41:27 +0530

    Time: 39 ms (84.38%) | Memory: 58.8 MB (69.44%) - LeetSync

---
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    private int getLongestForCurrentEle(Set<Integer>set,long curEle){
+        int longest = 1;
+        curEle *= curEle;
+        while(curEle <= Math.pow(10,5) && set.contains((int)curEle)){
+            curEle *= curEle;
+            longest++;
+        }
+        return longest == 1 ? 0 : longest;
+    }
+    public int longestSquareStreak(int[] nums) {
+        int longestStreak = 0, half = (nums.length+1)/2;
+        HashSet<Integer> set = new HashSet();
+        for(int ele : nums){
+            set.add(ele);
+        }
+        for(int ele : nums){
+            longestStreak = Math.max(longestStreak,getLongestForCurrentEle(set,(long)ele));
+            if(longestStreak >= half) return longestStreak;
+        }
+        return longestStreak == 0 ? -1 : longestStreak;
+    }
+}
