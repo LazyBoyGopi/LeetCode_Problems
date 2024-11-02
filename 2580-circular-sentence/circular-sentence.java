7a9210d25144410f44ff34d0fa3b4ecb7a9d19b5
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-11-02 10:02:38 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (54.87%) - LeetSync

--- 2580-circular-sentence/circular-sentence.java
+++ 2580-circular-sentence/circular-sentence.java
@@ -1,18 +1,18 @@
 class Solution {
-    public boolean isCircularSentence(String sentence) {
-        int last = 0, cur = 1, len = sentence.length();
-        if(sentence.charAt(0) != sentence.charAt(len-1)) return false;
-        while(cur < len){
-            if(sentence.charAt(cur) != ' ') {
-                last++;
-                cur++;
-            }else{
-                cur++;
-                if(cur >= len) return true;
-                if(sentence.charAt(last) != sentence.charAt(cur)) return false;
-                last = cur;
-                cur++;
+    public boolean isCircularSentence(String s) {
+        if (s.charAt(0) != s.charAt(s.length() - 1))
+            return false;
+
+        int k = s.indexOf(" ");
+        if (k == -1)
+            return true;
+
+        while (k != -1) {
+            if (s.charAt(k - 1) != s.charAt(k + 1)) {
+                return false;
             }
+
+            k = s.indexOf(" ", k+1);
         }
         return true;
     }
