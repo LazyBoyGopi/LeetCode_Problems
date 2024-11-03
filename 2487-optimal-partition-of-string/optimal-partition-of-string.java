6738af95b6fe69bbb7ce840cb039afeaff5e3fd5
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-11-03 10:16:17 +0530

    Time: 32 ms (52.40%) | Memory: 45.2 MB (58.46%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.java
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.java
@@ -1,8 +1,9 @@
 class Solution {
     public int partitionString(String s) {
         Set<Character>set = new HashSet();
-        int count = 0;
-        for(char ch : s.toCharArray()){
+        int count = 0, len = s.length();
+        for(int i=0;i<len;i++){
+            char ch = s.charAt(i);
             if(set.contains(ch)){
                 count++;
                 set.clear();
