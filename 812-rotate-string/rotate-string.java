90398ee92d455eec97cc2759bf5eef5eeae60ecb
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-11-03 09:38:40 +0530

    Time: 2 ms (7.93%) | Memory: 41.5 MB (27.67%) - LeetSync

---
+++ 812-rotate-string/rotate-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        int l1 = s.length() , l2 = goal.length();
+        if(l1 != l2) return false;
+        int i=-1;
+        char firstCh = goal.charAt(0);
+        do{
+            i = s.indexOf(firstCh,i+1);
+            System.out.println(i);
+            if(i == -1) return false;
+            String s1 = s.substring(0,i);
+            String s2 = s.substring(i);
+            String g1 = goal.substring(0,s2.length());
+            String g2 = goal.substring(s2.length());
+            if(s2.equals(g1) && s1.equals(g2)) return true;
+        }
+        while(i != -1);
+        return false;
+    }
+}
