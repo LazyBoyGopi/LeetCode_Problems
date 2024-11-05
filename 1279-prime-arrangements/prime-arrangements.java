d4b983d5da8603a9dc582f2705268a18b2032de3
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-11-05 16:05:05 +0530

    Time: 1 ms (38.64%) | Memory: 39.9 MB (97.02%) - LeetSync

--- 1279-prime-arrangements/prime-arrangements.java
+++ 1279-prime-arrangements/prime-arrangements.java
@@ -1,22 +1,17 @@
 class Solution {
     int[]ar = new int[101];
-    long[]factorial = new long[101];
     final int MOD = 1_000_000_007;
 
     public int numPrimeArrangements(int n) {
         if(ar[2] != 1) getAllPrimeNumbers();
-        return (int) (factorial[ar[n]] * (factorial[n-ar[n]]) % MOD);
+        return (int) (factorial(ar[n]) * (factorial(n-ar[n])) % MOD);
     }
     private void getAllPrimeNumbers(){
         ar[2] = 1;
-        factorial[0] = 1;
-        factorial[1] = 1;
-        factorial[2] = 2;
+
         long tillNow = 2;
         for(int i=3;i<=100;i++){
             ar[i] = ar[i-1] + checkPrime(i);
-            factorial[i] = (tillNow *i) % MOD;
-            tillNow = factorial[i];
         }
     }
     private int checkPrime(int n){
@@ -26,5 +21,10 @@ class Solution {
             if(n%i == 0) return 0;
         }
         return 1;
+    }
+    private long factorial(int n){
+        long fac = 1;
+        for(int i = 2;i<=n;i++) fac = (fac*i)%MOD;
+        return fac;
     }
 }
