17a9f41dfa107ac1bca051de8f8ae9e33dc866bb
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-11-06 17:07:25 +0530

    Time: 9 ms (44.66%) | Memory: 44.7 MB (32.19%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
@@ -1,16 +1,20 @@
 class Solution {
     public int[] sortByBits(int[] arr) {
-        int[][]ar = new int[arr.length][2];
+        Map<Integer,List<Integer>>map = new HashMap();
         for(int i=0;i<arr.length;i++){
-            ar[i][0] = Integer.bitCount(arr[i]);
-            ar[i][1] = arr[i];
+            int bitsCount = Integer.bitCount(arr[i]);
+            map.putIfAbsent(bitsCount,new ArrayList());
+            map.get(bitsCount).add(arr[i]);
         }
-        Arrays.sort(ar,(a,b) -> {
-            if(a[0] != b[0]) return Integer.compare(a[0],b[0]);
-            return Integer.compare(a[1],b[1]);
-        });
-        for(int i=0;i<arr.length;i++){
-            arr[i] = ar[i][1];
+        int idx = 0;
+        for(int i=0;i<=32;i++){
+            if(map.containsKey(i)){
+                List<Integer>list = map.get(i);
+                Collections.sort(list);
+                for(int ele : list){
+                    arr[idx++] = ele;
+                }
+            }
         }
         return arr;
     }
