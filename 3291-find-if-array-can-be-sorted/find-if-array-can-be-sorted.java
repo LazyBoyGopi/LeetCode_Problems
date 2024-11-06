ec28f1a1c55ea9db73b77ad4d07c0558a43d2107
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-11-06 10:39:54 +0530

    Time: 5 ms (44.29%) | Memory: 44.5 MB (25.16%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.java
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.java
@@ -1,18 +1,18 @@
 class Solution {
-    private int countSetBits(int n){
+    /* private int countSetBits(int n){
         int count = 0;
         while(n > 1){
             if(n % 2 == 1) count++;
             n /= 2;
         }
         return n == 1? count+1 : count;
-    }
-    private boolean isSorted(int[]nums){
+    } */
+    /*private boolean isSorted(int[]nums){
         for(int i=0;i<nums.length-1;i++){
             if(nums[i] > nums[i+1]) return false;
         }
         return true;
-    }
+    } */
     private int maxFrom(int[]nums,int st,int end){
         int max = nums[st];
         for(int i=st+1;i<=end;i++) max = Math.max(max,nums[i]);
@@ -28,10 +28,15 @@ class Solution {
     }
     public boolean canSortArray(int[] nums) {
         int len = nums.length;
-        if(len == 1 || isSorted(nums)) return true;
+        // if(len == 1 || isSorted(nums)) return true;
         int[]ar = new int[len];
 
         for(int i=0;i<len;i++){
+            /*
+            * Integer.bitCount() method return number of '1' bits in the given number, if the given number is positive and return 2's complement if the given number is negative -> ( 2 -> 1 ) , (-2 -> 31) TC -> O(1)
+            * Or call countSetBits() method TC-> O(logN);
+             */
+
             ar[i] = Integer.bitCount(nums[i]);
         }
         int leftLast = 0;
@@ -41,7 +46,7 @@ class Solution {
                 leftLast = i+1;
             }
         }
-
+        System.out.println(Integer.bitCount(2));
         return true;
     }
 }
