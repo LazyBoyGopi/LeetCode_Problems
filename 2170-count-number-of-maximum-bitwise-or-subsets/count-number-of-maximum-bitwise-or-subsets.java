f6b388f23d8bcea63fcf70ffd11a1a5ece967d21
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-11-07 11:47:11 +0530

    Time: 31 ms (33.59%) | Memory: 51.2 MB (12.23%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
@@ -1,23 +1,27 @@
 class Solution {
-    private void getAllSubsets(int []nums,Map<Integer,Integer>map,int idx,int OR){
+    private void getAllSubsets(int []nums,int[]ar,int idx,int OR){
         if(idx == nums.length){
-            map.put(OR,map.getOrDefault(OR,0)+1);
+            ar[OR]++;
             return;
         }
-        getAllSubsets(nums,map,idx+1,OR);
-        getAllSubsets(nums,map,idx+1,OR | nums[idx]);
+        getAllSubsets(nums,ar,idx+1,OR);
+        getAllSubsets(nums,ar,idx+1,OR | nums[idx]);
+    }
+    private int getMax(int[]nums){
+        int max = nums[0];
+        for(int ele : nums) max += ele;
+        return max;
     }
     public int countMaxOrSubsets(int[] nums) {
-        Map<Integer,Integer>map = new HashMap();
-        getAllSubsets(nums,map,0,0);
+        int[]ar = new int[getMax(nums)+1];
+        getAllSubsets(nums,ar,0,0);
         int max = Integer.MIN_VALUE;
         int maxCount = 0;
-        for(Map.Entry<Integer,Integer>entry : map.entrySet()){
-            if(entry.getKey() > max){
-                max = entry.getKey();
-                maxCount = entry.getValue();
+        for(int i=ar.length-1;i>=0;i--){
+            if(ar[i] != 0){
+                return ar[i];
             }
         }
-        return maxCount;
+        return -1;
     }
 }
