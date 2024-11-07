36d136a622f5a47594e69ea9cb0aa122b21621d4
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-11-07 20:44:44 +0530

    Time: 2 ms (67.11%) | Memory: 42.4 MB (20.88%) - LeetSync

--- 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.java
+++ 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.java
@@ -1,16 +1,21 @@
 class Solution {
+    private boolean isBigger(String str,String ans){
+        if(ans.equals("")) return true;
+        int idx = 0, len = str.length();
+        while(idx < len){
+            if(str.charAt(idx) > ans.charAt(idx)) return true;
+            idx++;
+        }
+        return false;
+    }
     public String removeDigit(String number, char digit) {
-        List<Integer>indexes = new LinkedList();
+        String ans = new String("");
         for(int i=0;i<number.length();i++){
             if(number.charAt(i) == digit){
-                indexes.add(i);
+                String str = new StringBuilder(number).deleteCharAt(i).toString();
+                if(ans.equals("") || ans.compareTo(str) < 0) ans = str;
             }
         }
-        List<String>list = new LinkedList();
-        for(int ele : indexes){
-            list.add(new StringBuilder(number).deleteCharAt(ele).toString());
-        }
-        Collections.sort(list);
-        return list.get(list.size()-1);
+        return ans;
     }
 }
