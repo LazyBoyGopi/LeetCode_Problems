43556ec8005d7d53cd0b8ff2fd5e275d6d2b33b8
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-11-07 11:29:30 +0530

    Time: 26 ms (95.24%) | Memory: 56.5 MB (62.15%) - LeetSync

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.java
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.java
@@ -1,22 +1,9 @@
 class Solution {
-    int len;
-    private void getAllCombinations(int[]candidates,int idx,int and,int curLen){
-        if(idx == candidates.length) {
-            if(and > 0)
-            len = Math.max(len,curLen);
-            return;
-        }
-        if(and > 0)
-            len = Math.max(len,curLen);
-        getAllCombinations(candidates,idx+1,and,curLen);
-        getAllCombinations(candidates,idx+1,and & candidates[idx],curLen+1);
-    }
     public int largestCombination(int[] candidates) {
-        // len = 0;
-        // getAllCombinations(candidates,0,0,0);
-        // return len;
+        
         int[]ar = new int[24];
-        for(int ele : candidates){
+        for(int i=0;i<candidates.length;i++){
+            int ele = candidates[i];
             int idx = 23;
             while(ele > 0){
                 if((ele & 1) == 1)
@@ -26,8 +13,8 @@ class Solution {
             }
         }
         int max = 0;
-        for(int ele : ar){
-            max = Math.max(ele,max);
+        for(int i=0;i<24;i++){
+            max = Math.max(ar[i],max);
         }
         return max;
     }
