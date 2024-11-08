ef71f08bdcee678140dbc8e58ea237ed82020036
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-11-08 21:25:59 +0530

    Time: 1 ms (82.87%) | Memory: 41.5 MB (81.12%) - LeetSync

--- 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.java
+++ 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.java
@@ -1,22 +1,11 @@
 class Solution {
-    private int isBigger(String str,String ans){
-        int idx = 0, len = str.length();
-        while(idx < len){
-            char c1 = ans.charAt(idx);
-            char c2 = str.charAt(idx);
-            if(c1 != c2) return c1-c2;
-            idx++;
-        }
-        return ans.length()-str.length();
-    }
     public String removeDigit(String number, char digit) {
-        String ans = new String("");
-        for(int i=0;i<number.length();i++){
-            if(number.charAt(i) == digit){
-                String str = number.substring(0,i)+number.substring(i+1);
-                if(ans.equals("") || isBigger(str,ans) < 0) ans = str;
+        for(int i=0;i<number.length()-1;i++){
+            if(number.charAt(i) == digit && number.charAt(i+1) > digit){
+                return number.substring(0,i)+number.substring(i+1);
             }
         }
-        return ans;
+        int lastIdx = number.lastIndexOf(digit);
+        return number.substring(0,lastIdx)+number.substring(lastIdx+1);
     }
 }
