7d4827e8ab40fe49f6467d2493e5136357a87fe2
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-11-09 13:16:55 +0530

    Time: 1 ms (87.50%) | Memory: 41 MB (45.19%) - LeetSync

--- 3394-minimum-array-end/minimum-array-end.java
+++ 3394-minimum-array-end/minimum-array-end.java
@@ -1,28 +1,32 @@
 class Solution {
-    private int[] getBit(int ele){
-        int[]ar = new int[64];
+    private int[] getBit(int ele) {
+        int[] ar = new int[64];
         int idx = 63;
-        while(ele > 0){
+        while (ele > 0) {
             ar[idx--] += (ele & 1);
             ele >>= 1;
         }
         return ar;
     }
+
     public long minEnd(int n, int x) {
         int[]xBit = getBit(x);
         int[]nBit = getBit(--n);
         int i=63, j= 63;
-        while(i >= 0){
-            if(xBit[i] == 1){
-                i--;
-                continue;
-            }
-            xBit[i--] = nBit[j--];
-        }
         long ans = 0;
         long curValAtTheBit = 1;
-        for(i=63;i>=0;i--){
-            ans += curValAtTheBit*xBit[i];
+        while(i >= 0){
+            if(xBit[i] == 1){
+                ans += curValAtTheBit;
+               
+            }else{
+                if(nBit[j] == 1){
+                    ans += curValAtTheBit;
+                }
+                j--;
+                
+            }
+            i--;
             curValAtTheBit *= 2;
         }
         return ans;
