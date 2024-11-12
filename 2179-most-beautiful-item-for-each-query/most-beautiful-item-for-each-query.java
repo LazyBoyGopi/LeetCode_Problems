ad1f3cd00a82950b0e41d30db437f37f81cf5a88
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-11-12 22:43:32 +0530

    Time: 46 ms (53.62%) | Memory: 81.4 MB (46.15%) - LeetSync

--- 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.java
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.java
@@ -1,29 +1,27 @@
 class Solution {
-    private int getHighestBeautyIndex(int[][]items,int price){
-        int st = 0, end = items.length-1;
+    private int getHighestBeauty(int[][]items,int price){
+        int st = 0, end = items.length-1, max = 0;
         while(st <= end){
             int mid = st+(end-st)/2;
             if(items[mid][0] > price) end = mid-1;
-            else st = mid+1;
+            else {
+                max = Math.max(max,items[mid][1]);
+                st = mid+1;
+            }
         }
-        return end;
+        return max;
     }
     public int[] maximumBeauty(int[][] items, int[] queries) {
         Arrays.sort(items,(a,b)->Integer.compare(a[0],b[0]));
-        Map<Integer,Integer>map = new HashMap();
         int max = items[0][1];
-        for(int i = 0;i<items.length;i++){
-            int []item = items[i];
+        for(int[]item : items){
             max = Math.max(max,item[1]);
-            map.put(item[0],max);
+            item[1] = max;
         }
         int[]ans = new int[queries.length];
         int i=0;
         for(int query : queries){
-            int idx = getHighestBeautyIndex(items,query);
-            if(idx >= 0)
-                ans[i] = map.get(items[idx][0]);
-            i++;
+            ans[i++] = getHighestBeauty(items,query);
         }
         return ans;
     }
