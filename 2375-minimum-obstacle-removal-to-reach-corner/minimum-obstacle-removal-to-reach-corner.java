c6dffc706250ae6acb87734081587d62792aab17
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-11-28 21:58:13 +0530

    Time: 146 ms (37.50%) | Memory: 83.3 MB (10.12%) - LeetSync

--- 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.java
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.java
@@ -1,35 +1,29 @@
 class Solution {
-    int[][] directions = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
-
+    int[][]directions = {{1,0},{-1,0},{0,1},{0,-1}};
     public int minimumObstacles(int[][] grid) {
         int m = grid.length, n = grid[0].length;
-        int[][] minObstacles = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                minObstacles[i][j] = Integer.MAX_VALUE;
-            }
+        int[][]minObstacles = new int[m][n];
+        for(int []obstacle : minObstacles){
+            Arrays.fill(obstacle,Integer.MAX_VALUE);
         }
-        Queue<int[]> pq = new PriorityQueue<int[]>((a, b) -> Integer.compare(a[0], b[0]));
-        pq.add(new int[] { 0, 0, 0 });
+        Queue<int[]>pq = new PriorityQueue<int[]>((a,b)-> Integer.compare(a[0],b[0]));
+        pq.offer(new int[]{0,0,0});
         minObstacles[0][0] = 0;
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
+        while(!pq.isEmpty()){
+            int[]cur = pq.poll();
             int obstacles = cur[0], row = cur[1], col = cur[2];
-            if (row == m - 1 && col == n - 1)
-                return obstacles;
-            for (int[] dir : directions) {
-                int newRow = row + dir[0], newCol = col + dir[1];
-                if (newRow >= 0 && newCol >= 0 && newRow < m && newCol < n) {
-                    int newObstacles = obstacles+grid[newRow][newCol];
-                    if (minObstacles[newRow][newCol] == Integer.MAX_VALUE) {
+            if(row == m-1 && col == n-1) return obstacles;
+            for(int[]dir : directions){
+                int newRow = row+dir[0], newCol = col+dir[1];
+                if(newRow >= 0 && newCol >= 0 && newRow < m && newCol < n){
+                    int newObstacles = obstacles + grid[newRow][newCol];
+                    if(minObstacles[newRow][newCol] == Integer.MAX_VALUE){
                         minObstacles[newRow][newCol] = newObstacles;
-                        pq.add(new int[] { newObstacles, newRow, newCol });
+                        pq.offer(new int[]{newObstacles,newRow,newCol});
                     }
                 }
             }
-
         }
         return -1;
     }
-
 }
