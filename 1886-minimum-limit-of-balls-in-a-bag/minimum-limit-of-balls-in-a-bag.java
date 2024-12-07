5fe5dd2f8822e0d023d9e3bda17351defbc97fba
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-12-07 18:54:03 +0530

    Time: 46 ms (69.52%) | Memory: 60.6 MB (5.26%) - LeetSync

--- 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.java
+++ 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.java
@@ -4,9 +4,20 @@ class Solution {
         for(int ele : nums) max = Math.max(max,ele);
         return max;
     }
+    private int findStart(int[]nums,int target){
+        int st = 0, end = nums.length-1;
+        while(st <= end){
+            int mid = st+(end-st)/2;
+            if(nums[mid] < target) st = mid+1;
+            else end = mid-1;
+        }
+        return st;
+    }
     private boolean isPossible(int maxBalls,int[]nums, int maxOperations){
         int operationsCount = 0;
-        for(int ele : nums){
+        int st = findStart(nums,maxBalls);
+        for(int i=st;i<nums.length;i++){
+            int ele = nums[i];
             int operations = (ele-1)/maxBalls;
             operationsCount += operations;
             if(operationsCount > maxOperations) return false;
@@ -15,6 +26,7 @@ class Solution {
     }
     public int minimumSize(int[] nums, int maxOperations) {
         int st = 1,end = findMax(nums);
+        Arrays.sort(nums);
         while(st <= end){
             int mid = (st+end)/2;
             if(isPossible(mid,nums,maxOperations)){
