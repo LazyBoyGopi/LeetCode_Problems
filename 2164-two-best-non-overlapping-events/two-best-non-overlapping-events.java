1d2d07c2ac6e860aa3e02e2b6ed040656e8af835
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-12-08 19:53:34 +0530

    Time: 36 ms (93.92%) | Memory: 89.1 MB (88.00%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.java
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.java
@@ -1,26 +1,25 @@
 class Solution {
     public int maxTwoEvents(int[][] events) {
         int n = events.length;
+        int []suffix = new int[n];
         int maxSum = Integer.MIN_VALUE;
-        Arrays.sort(events,(a,b)->Integer.compare(a[0],b[0]));
-        int[]suffix = new int[n];
+        Arrays.sort(events,(a,b)-> a[0]-b[0]);
         suffix[n-1] = events[n-1][2];
-        for(int i=n-2;i>=0;i--){
+        for(int i=n-2;i>=0;--i){
             suffix[i] = Math.max(suffix[i+1],events[i][2]);
         }
         for(int i=0;i<n;i++){
             int curEndTime = events[i][1];
-            int st = i, end = n-1;
+            int l = i+1,r = n-1;
             int nextEventIdx = -1;
-            while(st <= end){
-                int mid = st+(end-st)/2;
-                if(events[mid][0] > curEndTime) {
+            while(l <= r){
+                int mid = l+(r-l)/2;
+                if(events[mid][0] > curEndTime){
                     nextEventIdx = mid;
-                    end = mid-1;
+                    r = mid-1;
                 }
-                else st = mid+1;
+                else l = mid+1;
             }
-
             if(nextEventIdx != -1){
                 maxSum = Math.max(maxSum,events[i][2]+suffix[nextEventIdx]);
             }
