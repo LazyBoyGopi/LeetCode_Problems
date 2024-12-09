20a0ebcf6558d531a8cbd2425cd41ab200d7046f
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-12-09 19:21:58 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (40.10%) - LeetSync

--- 3429-special-array-i/special-array-i.java
+++ 3429-special-array-i/special-array-i.java
@@ -3,10 +3,11 @@ class Solution {
         boolean isEven = nums[0] % 2 == 0;
         int len = nums.length;;
         for(int i=1;i<len;i++){
-            if(isEven && nums[i]%2 == 0){
-                return false;
+            if(isEven){
+                if(nums[i]%2 == 0)
+                    return false;
             }
-            else if(!isEven && nums[i]% 2 == 1) return false;
+            else if(nums[i]% 2 == 1) return false;
             isEven = !isEven;
         }
         return true;
