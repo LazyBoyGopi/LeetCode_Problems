e6bfaf49b0e704f412484f3d9bd48ee351604802
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-12-10 15:58:39 +0530

    Time: 11 ms (61.04%) | Memory: 45.2 MB (40.89%) - LeetSync

--- 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.java
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.java
@@ -1,18 +1,15 @@
 class Solution {
-    private boolean isPossible(String str){
-       char ch = str.charAt(0);
-       for(int i=1;i<str.length();i++){
-        if(ch != str.charAt(i)) return false;
-       }
-       return true;
-    }
     public int maximumLength(String s) {
         Map<String,Integer>map = new HashMap();
-        for(int i=0;i<s.length();i++){
-            for(int j=i+1;j<=s.length();j++){
-                String str = s.substring(i,j);
-                if(isPossible(str))
-                    map.put(str,map.getOrDefault(str,0)+1);
+        int len = s.length();
+        outer:for(int i=0;i<len;i++){
+            char ch = s.charAt(i);
+            for(int j=i;j<len;j++){
+                if(ch != s.charAt(j)){
+                    continue outer;
+                }
+                String str = s.substring(i,j+1);
+                map.put(str,map.getOrDefault(str,0)+1);
             }
         }
         int maxLen = -1;
