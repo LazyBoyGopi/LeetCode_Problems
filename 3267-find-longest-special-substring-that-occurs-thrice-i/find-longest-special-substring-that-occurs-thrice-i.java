8ae18ce95b8db4e859dfe94c9c1dcfdcddd48767
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-12-10 19:35:06 +0530

    Time: 37 ms (14.72%) | Memory: 45.5 MB (21.56%) - LeetSync

--- 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.java
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.java
@@ -1,23 +1,68 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int maximumLength(String s) {
-        Map<String,Integer>map = new HashMap();
-        int len = s.length();
-        outer:for(int i=0;i<len;i++){
-            char ch = s.charAt(i);
-            for(int j=i;j<len;j++){
-                if(ch != s.charAt(j)){
-                    continue outer;
+        int ans = -1;
+        Map<Pair, Integer> map = new HashMap<>(); // Map of (character, size) -> count
+        int n = s.length();
+        char[] chars = s.toCharArray();
+
+        // First pass: Populate the map with frequencies of contiguous substrings
+        map.put(new Pair(chars[0], 1), 1);
+        int currFreq = 1;
+        for (int i = 1; i < n; i++) {
+            if (chars[i] == chars[i - 1]) {
+                currFreq++;
+                map.put(new Pair(chars[i], currFreq), map.getOrDefault(new Pair(chars[i], currFreq), 0) + 1);
+            } else {
+                currFreq = 1;
+                map.put(new Pair(chars[i], currFreq), map.getOrDefault(new Pair(chars[i], currFreq), 0) + 1);
+            }
+        }
+
+        // Second pass: Find the maximum length of a substring satisfying the condition
+        for (char c = 'a'; c <= 'z'; c++) {
+            for (int i = n; i > 0; i--) {
+                Pair key = new Pair(c, i);
+                if (map.containsKey(key)) {
+                    if (map.get(key) >= 3) {
+                        ans = Math.max(ans, i);
+                    }
+                    Pair smallerKey = new Pair(c, i - 1);
+                    map.put(smallerKey, map.getOrDefault(smallerKey, 0) + map.get(key));
                 }
-                String str = s.substring(i,j+1);
-                map.put(str,map.getOrDefault(str,0)+1);
             }
         }
-        int maxLen = -1;
-        for(Map.Entry<String,Integer>entry : map.entrySet()){
-            if(entry.getValue() > 2){
-                maxLen = Math.max(maxLen,entry.getKey().length());
-            }
-        }
-        return maxLen;
+
+        return ans;
     }
+
+    // Helper class for the (character, size) pair
+    static class Pair {
+        char c;
+        int size;
+
+        Pair(char c, int size) {
+            this.c = c;
+            this.size = size;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Pair pair = (Pair) o;
+            return c == pair.c && size == pair.size;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = Character.hashCode(c);
+            result = 31 * result + Integer.hashCode(size);
+            return result;
+        }
+    }
+
+    // Main method for testing
 }
