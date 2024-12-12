2227c1ce08a45a266f8923c0e39a5b57aea3d4f5
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-12-12 11:36:24 +0530

    Time: 39 ms (75.41%) | Memory: 60.3 MB (41.79%) - LeetSync

--- 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.java
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.java
@@ -3,11 +3,9 @@ class Solution {
         Arrays.sort(nums);
         int l = 0, r = 0, len = nums.length;
         int max = 0, range = 2*k;
-
-        while(r < len){
-            while(r < len && nums[r]-nums[l] <= range) r++;
-            max = Math.max(max,r-l);
-            while(l < len && r < len && nums[r]-nums[l] > range) l++;
+        for(;r < len ; r++){
+            while(nums[r]-nums[l] > range) l++;
+            max = Math.max(max,r-l+1);
         }   
         return max;
     }
