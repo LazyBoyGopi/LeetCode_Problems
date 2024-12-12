78b02c50263ddba6a61ef00afdeb8c018b4991a7
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-12-12 11:37:17 +0530

    Time: 39 ms (75.41%) | Memory: 59.7 MB (84.58%) - LeetSync

--- 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.java
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.java
@@ -1,9 +1,9 @@
 class Solution {
     public int maximumBeauty(int[] nums, int k) {
         Arrays.sort(nums);
-        int l = 0, r = 0, len = nums.length;
-        int max = 0, range = 2*k;
-        for(;r < len ; r++){
+        int l = 0, len = nums.length,max = 0, range = 2*k;
+         
+        for(int r = 0;r < len ; r++){
             while(nums[r]-nums[l] > range) l++;
             max = Math.max(max,r-l+1);
         }   
