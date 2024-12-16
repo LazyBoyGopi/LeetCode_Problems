6030d4c11e1160fd0485b003f6e73b60a0a8db62
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-12-16 10:02:57 +0530

    Time: 4 ms (16.55%) | Memory: 45.5 MB (7.02%) - LeetSync

---
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
@@ -0,0 +1,30 @@
+class Solution {
+    private class Pair{
+        public int value;
+        public int index;
+        public Pair(int _value,int _index){
+            value = _value;
+            index = _index;
+        }
+    }
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+        int len = nums.length;
+        Queue<Pair>queue = new PriorityQueue<>((a,b)->{
+            if(a.value==b.value) return a.index-b.index;
+            return a.value-b.value;
+        });
+        for(int i=0;i<len;i++){
+            queue.offer(new Pair(nums[i],i));
+        }
+        while(k > 0){
+            Pair curPair = queue.poll();
+            int newValue = curPair.value*multiplier;
+            queue.offer(new Pair(newValue,curPair.index));
+            k--;
+        }
+        for(Pair pair : queue){
+            nums[pair.index] = pair.value;
+        }
+        return nums;
+    }
+}
