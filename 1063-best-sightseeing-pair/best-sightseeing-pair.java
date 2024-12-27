3c5d9ee63cd3525978b6420afaa9900bb7ba148f
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2024-12-27 21:22:39 +0530

    Time: 3 ms (97.03%) | Memory: 53.8 MB (16.76%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.java
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.java
@@ -1,5 +1,7 @@
 class Solution {
 
+
+    //Brute force
     /*
     int maxScore = Integer.MIN_VALUE;
         int len = values.length;
@@ -12,20 +14,9 @@ class Solution {
         return maxScore;
     */
 
+
+    //Better
     /*
-        Queue<Integer>queue = new PriorityQueue<Integer>(Comparator.reverseOrder());
-        int maxScore = 0,len = values.length;
-        queue.offer(values[0]);
-        for(int i=0;i<len;i++){
-            int ele = values[i];
-            int top = queue.peek();
-            if(top + ele-i > maxScore) maxScore = top+ele-i;
-            queue.offer(ele+i);
-        }
-        return maxScore;
-    }
-    */
-    public int maxScoreSightseeingPair(int[] values) {
         Queue<Integer>queue = new PriorityQueue<Integer>(Comparator.reverseOrder());
         int maxScore = 0,len = values.length;
         queue.offer(values[0]);
@@ -34,6 +25,32 @@ class Solution {
             int top = queue.peek();
             if(top + ele-i > maxScore) maxScore = top+ele-i;
             queue.offer(ele+i);
+        }
+        return maxScore;
+    }
+    */
+
+    //Optimal
+    /*
+        int maxTillNow = values[0], len = values.length, maxScore = 0;
+        for(int i=1;i<len;i++){
+            int ele = values[i];
+            if(maxTillNow+ele-i > maxScore) maxScore = maxTillNow+ele-i;
+            if(maxTillNow < ele+i) maxTillNow = ele+i;
+        }
+        return maxScore;
+     */
+
+    public int maxScoreSightseeingPair(int[] values) {
+        int len = values.length;
+        int[]maxEle = new int[len];
+        maxEle[0] = values[0];
+        for(int i=1;i<len;i++){
+            maxEle[i] = Math.max(maxEle[i-1],values[i]+i);
+        }
+        int maxScore = 0;
+        for(int i=1;i<len;i++){
+            maxScore = Math.max(maxScore,values[i]-i + maxEle[i-1]);
         }
         return maxScore;
     }
