458687364858fbc4e009c067788241ed8cb4bad4
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-01-01 14:33:51 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (41.34%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
@@ -1,41 +1,50 @@
 class Solution {
-    HashSet<Integer> isTravelNeeded = new HashSet<>();
 
-    private int solve(int[] dp, int[] days, int[] costs, int currDay) {
-        // If we have iterated over travel days, return 0.
-        if (currDay > days[days.length - 1]) {
+    private int oneDay,sevenDays,thirtyDays;
+
+    private int binarySearch(int[]days,int st,int target){
+        int end = days.length-1;
+        while(st <= end){
+            int mid = st+(end-st)/2;
+            int ele = days[mid];
+            if(ele == target) return mid;
+            if(ele > target) end = mid-1;
+            else st = mid+1;
+        }
+        return end;
+    }
+
+    private int getAllCombinations(int[]days,int idx,int[]dp){
+        int len = days.length;
+
+        if(idx >= len){
             return 0;
         }
 
-        // If we don't need to travel on this day, move on to next day.
-        if (!isTravelNeeded.contains(currDay)) {
-            return solve(dp, days, costs, currDay + 1);
-        }
+        if(dp[idx] != -1 ) return dp[idx];
 
-        // If already calculated, return from here with the stored answer.
-        if (dp[currDay] != -1) {
-            return dp[currDay];
-        }
+        int curDays = days[idx];
+        int sevenAdded = curDays + 6;
+        int thirtyAdded = curDays + 29;
 
-        int oneDay = costs[0] + solve(dp, days, costs, currDay + 1);
-        int sevenDay = costs[1] + solve(dp, days, costs, currDay + 7);
-        int thirtyDay = costs[2] + solve(dp, days, costs, currDay + 30);
+        int sevenIdx = binarySearch(days,idx,sevenAdded);
+        int thirtyIdx = binarySearch(days,idx,thirtyAdded);
 
-        // Store the cost with the minimum of the three options.
-        return dp[currDay] = Math.min(oneDay, Math.min(sevenDay, thirtyDay));
+
+        int one = oneDay+getAllCombinations(days,idx+1,dp);
+        int seven = sevenDays+getAllCombinations(days,sevenIdx+1,dp);
+        int thirty = thirtyDays+getAllCombinations(days,thirtyIdx+1,dp);
+
+        
+        return dp[idx] = Math.min(one,Math.min(seven,thirty));
     }
 
     public int mincostTickets(int[] days, int[] costs) {
-        // The last day on which we need to travel.
-        int lastDay = days[days.length - 1];
-        int dp[] = new int[lastDay + 1];
-        Arrays.fill(dp, -1);
-
-        // Mark the days on which we need to travel.
-        for (int day : days) {
-            isTravelNeeded.add(day);
-        }
-
-        return solve(dp, days, costs, 1);
+        oneDay = costs[0];
+        sevenDays = costs[1];
+        thirtyDays = costs[2];
+        int[]dp = new int[days.length+1];
+        Arrays.fill(dp,-1);
+        return getAllCombinations(days,0,dp);
     }
 }
