22ec46b3eb242b00c0c3d0f548a16957d6aea425
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-01-01 15:01:53 +0530

    Time: 4 ms (21.52%) | Memory: 41.5 MB (64.03%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
@@ -1,14 +1,22 @@
 class Solution {
     public int maxScore(String s) {
-        int len = s.length(), maxScore = 0;
-        int leftZeroes = 0;
-        for(int i=0;i<len-1;i++){
-            if(s.charAt(i) == '0') leftZeroes++;
-            int rightOnes = 0;
-            for(int j = i+1;j<len;j++){
-                if(s.charAt(j) == '1') rightOnes++;
-            }
-            maxScore = Math.max(maxScore,rightOnes+leftZeroes);
+        int len = s.length();
+
+        int[]preSum = new int[len];
+        preSum[0] = s.charAt(0) == '0' ? 1 : 0;
+        for(int i=1;i<len;i++){
+            preSum[i] = preSum[i-1] + (s.charAt(i) == '0' ? 1 : 0);
+        }
+        System.out.println(Arrays.toString(preSum));
+        int maxScore = 0;
+        int totalZeroes = preSum[len-1];
+        for(int i=1;i<len;i++){
+            int leftZero = preSum[i-1];
+            int rightLen = len-i;
+            int rightZeroes = totalZeroes - leftZero;
+            int rightOnes = rightLen - rightZeroes;
+            int curScore = leftZero + rightOnes;
+            maxScore = Math.max(maxScore,curScore);
         }
         return maxScore;
     }
