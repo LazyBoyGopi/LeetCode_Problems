e23c8c8c844f106dc332cb7699b2b32697feab89
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-01-01 15:02:02 +0530

    Time: 1 ms (97.36%) | Memory: 41.3 MB (77.00%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
@@ -7,7 +7,6 @@ class Solution {
         for(int i=1;i<len;i++){
             preSum[i] = preSum[i-1] + (s.charAt(i) == '0' ? 1 : 0);
         }
-        System.out.println(Arrays.toString(preSum));
         int maxScore = 0;
         int totalZeroes = preSum[len-1];
         for(int i=1;i<len;i++){
