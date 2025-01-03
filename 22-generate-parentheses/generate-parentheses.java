53f9fb4f35b731c2110ee1c0fb3c3ad4fab54750
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-01-03 11:54:20 +0530

    Time: 1 ms (82.55%) | Memory: 43.1 MB (70.17%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    List<String>ans;
+    private void getAns(int opened,int closed,int n,StringBuilder str){
+        if(closed == n){
+            ans.add(new StringBuilder(str).toString());
+            return;
+        }
+        if(opened > closed){
+            str.append(')');
+            getAns(opened,closed+1,n,str);
+            str.setLength(str.length()-1);
+        }
+        if(opened < n){
+            str.append('(');
+            getAns(opened+1,closed,n,str);
+            str.setLength(str.length()-1);
+        }
+    }
+    public List<String> generateParenthesis(int n) {
+        ans = new ArrayList<String>();
+        getAns(0,0,n,new StringBuilder());
+        return ans;
+    }
+}
