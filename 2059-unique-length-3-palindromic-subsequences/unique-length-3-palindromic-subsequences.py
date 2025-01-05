13aca62f895bf3c969f9258a49492516f13b90c6
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-01-05 11:26:24 +0530

    Time: 554 ms (55.14%) | Memory: 18.3 MB (25.38%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -3,22 +3,23 @@ class Solution:
         map = {}
         set = {0}
         count = 0
+
         for i in range(len(s)):
             curCh = s[i]
             if not curCh in map:
                 map[curCh] = i
+
         for i in range(len(s)-1,0,-1):
             curCh  = s[i]
             firstIdx = map[curCh]
             
             if firstIdx < i and not curCh in set:
-                print(curCh,firstIdx)
                 uniqueCharacters = {0}
                 uniqueCharacters.remove(0)
                 for j in range(firstIdx+1,i):
                     uniqueCharacters.add(s[j])
-                print(curCh,uniqueCharacters)
                 count += len(uniqueCharacters)
                 set.add(curCh)
+            
         return count
         
