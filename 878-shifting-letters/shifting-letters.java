22ef732206af64a2e0b2919dda31b9d14440010a
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-01-05 15:02:10 +0530

    Time: 19 ms (17.74%) | Memory: 57.2 MB (65.15%) - LeetSync

---
+++ 878-shifting-letters/shifting-letters.java
@@ -0,0 +1,54 @@
+//Brute Force
+
+/*
+class Solution {
+    private char shiftCurCharacter(char curCh,int times){
+        while(times > 0){
+            if(curCh == 'z') curCh = 'a';
+            else curCh = (char)(curCh+1);
+            times--;
+        }
+        return curCh;
+    }
+    private void shiftTillNow(char[]ar,int end,int times){
+        for(int i=0;i<=end;i++){
+            ar[i] = shiftCurCharacter(ar[i],times);
+        }
+    }
+    public String shiftingLetters(String s, int[] shifts) {
+        int sLen = shifts.length;
+        char[]ar = s.toCharArray();
+        for(int i=0;i<sLen;i++){
+            shiftTillNow(ar,i,shifts[i]);
+        }
+        return new StringBuilder().append(ar).toString();
+    }
+}
+ */
+
+
+//Optimised
+
+class Solution {
+    private char shiftCurCharacter(char curCh,long times){
+        times %= 26;
+        while(times > 0){
+            if(curCh == 'z')
+                curCh = 'a';
+            else
+                curCh = (char)(curCh+1);
+            times--;
+        }
+        return curCh;
+    }
+    public String shiftingLetters(String s, int[] shifts) {
+        long shiftsTillNow = 0;
+        int len = s.length();
+        char[]ar = s.toCharArray();
+        for(int i=len-1;i>=0;i--){
+            shiftsTillNow += shifts[i];
+            ar[i] = shiftCurCharacter(ar[i],shiftsTillNow);
+        }
+        return new StringBuilder().append(ar).toString();
+    }
+}
