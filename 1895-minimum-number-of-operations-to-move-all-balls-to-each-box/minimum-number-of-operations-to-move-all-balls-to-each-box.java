e298640f3168ac7a42cf39e73112221c124b4c39
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-01-06 19:55:56 +0530

    Time: 634 ms (5.04%) | Memory: 182.1 MB (5.03%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,15 +1,42 @@
 class Solution {
+    private int sum(List<Integer>list,int curIdx){
+        int sum = 0;
+        for(int idx : list) sum += Math.abs(curIdx-idx);
+        return sum;
+    }
     public int[] minOperations(String boxes) {
         int len = boxes.length();
-        int[]ans = new int[len];
+        List<Integer>[] pre = new LinkedList[len];
+        List<Integer>[] suf = new LinkedList[len];
+
+        List<Integer>list = new LinkedList();
         for(int i=0;i<len;i++){
-            int curOperations = 0;
-            for(int j=0;j<len;j++){
-                if(boxes.charAt(j) == '1'){
-                    curOperations += Math.abs(i-j);
-                }
+            pre[i] = new LinkedList(list);
+            if(boxes.charAt(i) == '1'){
+                list.add(i);
             }
-            ans[i] = curOperations;
+        }
+        list = new LinkedList();
+        for(int i=len-1;i>=0;i--){
+            suf[i] = new LinkedList(list);
+            if(boxes.charAt(i) == '1'){
+                list.add(i);
+            }
+        }
+        int[]pre1 = new int[len];
+        int[]suf1 = new int[len];
+        for(int i=0;i<len;i++){
+            pre1[i] = sum(pre[i],i);
+        }
+        for(int i=len-1;i>=0;i--){
+            suf1[i] = sum(suf[i],i);
+        }
+        System.out.println(Arrays.toString(pre1)+"\n"+Arrays.toString(suf1));
+        int[]ans = new int[len];
+        ans[0] = suf1[0];
+        ans[len-1] = pre1[len-1];
+        for(int i=1;i<len-1;i++){
+            ans[i] = pre1[i] + suf1[i];
         }
         return ans;
     }
