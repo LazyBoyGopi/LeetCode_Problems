c7a6baf81d07316aae9ed94d75da629f4d0f26de
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-01-07 22:17:06 +0530

    Time: 9 ms (6.83%) | Memory: 44.6 MB (6.00%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.java
@@ -1,5 +1,5 @@
 class Solution {
-    private boolean contains(String first,String second){
+    /*private boolean contains(String first,String second){
         int p1 = 0, p2 = 0,l1 = first.length(), l2 = second.length();
         if(l1 > l2) return false;
         boolean isFirst = false;
@@ -7,20 +7,47 @@ class Solution {
         while(p1 < l1 && p2 < l2){
             if(first.charAt(p1) == second.charAt(p2)){
                 p1++;
-                if(!isFirst){
-                    isFirst = true;
-                    lastEqual = p2;
-                }
+               
                 p2++;
             }else{
                 p1 = 0;
-                if(lastEqual != -1) {
-                    p2 = lastEqual+1;
-                    isFirst = false;
-                    lastEqual = -1;
-                }else p2++;
+                //  p2++;
             }
             if(p1 == l1) return true;
+        }
+        return false;
+    } */
+    private int[] getLSP(String pattern){
+        int len = pattern.length();
+        int[]lsp = new int[len];
+        int lspTillNow = 0;
+        for(int i=1;i<len;i++){
+            char curCh = pattern.charAt(i);
+            while(lspTillNow > 0 && pattern.charAt(lspTillNow) != curCh){
+                lspTillNow = lsp[lspTillNow-1];
+            }
+            if(curCh == pattern.charAt(lspTillNow)) lspTillNow++;
+            lsp[i] = lspTillNow;
+        }
+        return lsp;
+    }
+    private boolean contains(String pattern,String txt){
+        int[]lsp = getLSP(pattern);
+        int pLen = pattern.length(), tLen = txt.length();
+        int i=0, j=0;
+        while(i < tLen){
+            if(pattern.charAt(j) == txt.charAt(i)){
+                i++;
+                j++;
+            }else{
+                if(j > 0)
+                    j = lsp[j-1];
+                else 
+                    i++;
+            }
+            if(j == pLen){
+                return true;
+            }
         }
         return false;
     }
