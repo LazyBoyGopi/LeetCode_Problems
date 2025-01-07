f2b8e6b8e1b8d320b673cc250b92d48a334b6237
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-01-07 19:16:16 +0530

    Time: 5 ms (41.61%) | Memory: 42.3 MB (54.32%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.java
@@ -1,4 +1,29 @@
 class Solution {
+    private boolean contains(String first,String second){
+        int p1 = 0, p2 = 0,l1 = first.length(), l2 = second.length();
+        if(l1 > l2) return false;
+        boolean isFirst = false;
+        int lastEqual = -1;
+        while(p1 < l1 && p2 < l2){
+            if(first.charAt(p1) == second.charAt(p2)){
+                p1++;
+                if(!isFirst){
+                    isFirst = true;
+                    lastEqual = p2;
+                }
+                p2++;
+            }else{
+                p1 = 0;
+                if(lastEqual != -1) {
+                    p2 = lastEqual+1;
+                    isFirst = false;
+                    lastEqual = -1;
+                }else p2++;
+            }
+            if(p1 == l1) return true;
+        }
+        return false;
+    }
     public List<String> stringMatching(String[] words) {
         List<String>list = new ArrayList();
         int len = words.length;
@@ -7,7 +32,7 @@ class Solution {
             for(int j=0;j<len;j++){
                 String jStr = words[j];
                 if(i== j) continue;
-                if(jStr.contains(cur)){
+                if(contains(cur,jStr)){
                     list.add(cur);
                     break;
                 }
