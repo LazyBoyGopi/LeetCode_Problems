60b8a7afa027c3722dc102cb93962cfe088cb40a
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-01-08 20:17:24 +0530

    Time: 2 ms (100.00%) | Memory: 42.1 MB (83.52%) - LeetSync

--- 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.java
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.java
@@ -1,12 +1,26 @@
 class Solution {
+    private boolean isPrefixAndSuffix(String s1,String s2){
+        int l1 = s1.length(), l2 = s2.length();
+        if(l1 > l2) return false;
+        for(int i=0;i<l1;i++){
+            if(s1.charAt(i) != s2.charAt(i)) return false;
+        }
+        l2--;
+        for(int i=l1-1;i>=0;i--,l2--){
+            if(s1.charAt(i) != s2.charAt(l2)) return false;
+        }
+        return true;
+    }
     public int countPrefixSuffixPairs(String[] words) {
-        int count = 0;
-        for(int i=0;i<words.length;i++){
-            for(int j=i+1;j<words.length;j++){
-                String str1 = words[i] , str2 = words[j];
-                if(str2.startsWith(str1) && str2.endsWith(str1)) count++;
+        int len = words.length, totalCount = 0;
+        for(int i=0;i<len;i++){
+            String cur = words[i];
+            for(int j=i+1;j<len;j++){
+                if(i == j) continue;
+                if(isPrefixAndSuffix(cur,words[j]))
+                    totalCount++;
             }
         }
-        return count;
+        return totalCount;
     }
 }
