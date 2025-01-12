2c1b64dda1371e4f581460b4f8a1e418d1327e1b
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-01-12 16:25:56 +0530

    Time: 15 ms (83.40%) | Memory: 45.5 MB (87.55%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.java
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.java
@@ -118,25 +118,58 @@ class Solution {
 
 
 class Solution {
+
     public boolean canBeValid(String s, String locked) {
-        int len = s.length();
-        if((len & 1) == 1) return false;
-        Stack<Integer>open = new Stack();
-        Stack<Integer>lock = new Stack();
-        for(int i=0;i<len;i++){
-            char cur = s.charAt(i);
-            if(locked.charAt(i) == '0') lock.push(i);
-            else if(cur == '(') open.push(i);
-            else if(cur == ')'){
-                if(!open.isEmpty()) open.pop();
-                else if(!lock.isEmpty()) lock.pop();
-                else return false;
+        int length = s.length();
+
+        // If length of string is odd, return false.
+        if (length % 2 == 1) {
+            return false;
+        }
+        int openBrackets = 0;
+        int unlocked = 0;
+
+        // Iterate through the string to handle '(' and ')'.
+        for (int i = 0; i < length; i++) {
+            if (locked.charAt(i) == '0') {
+                unlocked++;
+            } else if (s.charAt(i) == '(') {
+                openBrackets++;
+            } else if (s.charAt(i) == ')') {
+                if (openBrackets > 0) {
+                    openBrackets--;
+                } else if (unlocked > 0) {
+                    unlocked--;
+                } else {
+                    return false;
+                }
             }
         }
-        while(!open.isEmpty() && !lock.isEmpty() && open.peek() < lock.peek()){
-        open.pop();
-        lock.pop();
+
+        // Match remaining open brackets with unlocked characters.
+        int balance = 0;
+        for (int i = length - 1; i >= 0; i--) {
+            if (locked.charAt(i) == '0') {
+                balance--;
+                unlocked--;
+            } else if (s.charAt(i) == '(') {
+                balance++;
+                openBrackets--;
+            } else if (s.charAt(i) == ')') {
+                balance--;
+            }
+            if (balance > 0) {
+                return false;
+            }
+            if (unlocked == 0 && openBrackets == 0) {
+                break;
+            }
         }
-        return open.isEmpty();
+
+        if (openBrackets > 0) {
+            return false;
+        }
+
+        return true;
     }
 }
