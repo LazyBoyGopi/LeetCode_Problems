2007b1a26d335116a1c1651931ee27800324c288
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-01-13 10:44:10 +0530

    Time: 9 ms (92.05%) | Memory: 46.8 MB (24.50%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.java
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.java
@@ -1,18 +1,22 @@
 class Solution {
     public int minimumLength(String s) {
         int len = s.length();
-        Map<Character,Integer>map = new HashMap();
+        int[]freq = new int[26];
         for(int i=0;i<len;i++){
-            char ch = s.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
+            int idx = s.charAt(i)-'a';
+            freq[idx]++;
         }
-        int newLen = 0;
-        for(int value : map.values()){
-            if(value > 2){
-                if((value & 1) == 1) newLen += 1;
-                else newLen += 2;
-            }else newLen += value;
+        int newLength = 0;
+        for(int i=0;i<26;i++){
+            int curLen = freq[i];
+            if(curLen > 2){
+                if((curLen & 1) == 1)
+                    newLength += 1;
+                else 
+                    newLength += 2;
+            }
+            else newLength += curLen;
         }
-        return newLen;
+        return newLength;
     }
 }
