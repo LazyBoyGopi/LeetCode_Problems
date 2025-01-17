7ba2c58d0dfb0f1680b1ef599fdb750783959ae8
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-01-17 10:52:36 +0530

    Time: 2 ms (100.00%) | Memory: 62.4 MB (40.59%) - LeetSync

---
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
@@ -0,0 +1,13 @@
+class Solution {
+    private boolean checkWith(int[]derived,int firstBit){
+        int len = derived.length,curBit = derived[0] ^ firstBit;
+        for(int i=1;i<len-1;i++){
+            curBit ^= derived[i];
+        }
+        return (curBit ^ derived[len-1]) == firstBit;
+    }
+    public boolean doesValidArrayExist(int[] derived) {
+        if(derived.length == 1) return derived[0] == 0;
+        return checkWith(derived,0) || checkWith(derived,1);
+    }
+}
