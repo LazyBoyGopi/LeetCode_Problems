f9831fe5a98e4e838384cac169a73f6c1a9d172b
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-01-19 22:19:23 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (92.01%) - LeetSync

--- 289-game-of-life/game-of-life.java
+++ 289-game-of-life/game-of-life.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     private int getNeighborsAliveCells(int row,int col,int[][]board){
         int aliveCells = 0,m = board.length, n = board[0].length;
@@ -18,7 +19,7 @@ class Solution {
         int m = board.length, n = board[0].length;
         int[][]ans = new int[m][n];
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+            for(int j=0;j<n;j++){ 
                 int aliveCells = getNeighborsAliveCells(i,j,board);
                 if(board[i][j] == 1){
                     // if(aliveCells < 2){
@@ -44,6 +45,95 @@ class Solution {
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 board[i][j] = ans[i][j];
+            }
+        }
+    }
+}
+ */
+
+
+
+class Solution {
+    private int getNeighborsAliveCells(int row,int col,int[][]board){
+        int aliveCells = 0,m = board.length, n = board[0].length;
+        if(row-1 >=0 && col-1 >= 0){
+            int curVal = board[row-1][col-1];
+            if(curVal == 1 || curVal == 2 || curVal == 3)
+                aliveCells++;
+        } 
+        if(row-1 >=0) {
+            int curVal = board[row-1][col];
+            if(curVal == 1 || curVal == 2 || curVal == 3)
+                aliveCells++;
+        }
+
+        if(row-1 >=0 && col+1 < n) {
+            int curVal = board[row-1][col+1];
+            if(curVal == 1 || curVal == 2 || curVal == 3)
+                aliveCells++;
+        }
+        if(col-1 >= 0) {
+            int curVal = board[row][col-1];
+            if(curVal == 1 || curVal == 2 || curVal == 3)
+                aliveCells++;
+        }
+
+        if(col+1 < n){
+            int curVal = board[row][col+1];
+            if(curVal == 1 || curVal == 2 || curVal == 3)
+                aliveCells++;
+        }
+        if(row+1 < m && col-1 >=0){
+            int curVal = board[row+1][col-1];
+            if(curVal == 1 || curVal == 2 || curVal == 3)
+                aliveCells++;
+        }
+
+        if(row+1 < m){
+            int curVal = board[row+1][col];
+            if(curVal == 1 || curVal == 2 || curVal == 3)
+                aliveCells++;
+        }
+        if(row+1 < m && col+1 < n){
+            int curVal = board[row+1][col+1];
+            if(curVal == 1 || curVal == 2 || curVal == 3)
+                aliveCells++;
+        }
+        return aliveCells;
+    }
+    public void gameOfLife(int[][] board) {
+        int m = board.length, n = board[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){ 
+                int aliveCells = getNeighborsAliveCells(i,j,board);
+                if(board[i][j] == 1){
+                    if(aliveCells < 2){
+                        board[i][j] = 3;
+                    }
+                    else if(aliveCells == 2 || aliveCells == 3){
+                        board[i][j] = 2;
+                    }
+                    else if(aliveCells > 3){
+                        board[i][j] = 3;
+                    }
+                }else {
+                    if(aliveCells == 3){
+                        board[i][j] = 4;
+                    }else{
+                        board[i][j] = 5;
+                    }
+                }
+            }
+        }
+        copyArray(board);
+    }
+    private void copyArray(int[][]board){
+        int m = board.length, n = board[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int curVal = board[i][j];
+                if(curVal == 2 || curVal == 4) board[i][j] = 1;
+                else board[i][j] = 0;
             }
         }
     }
