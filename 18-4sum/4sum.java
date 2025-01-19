3c1e9271a95374dd3da49ad8bcabdb9ece37862d
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-01-19 17:18:08 +0530

    Time: 17 ms (74.79%) | Memory: 43.8 MB (76.45%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,32 +1,36 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> list = new LinkedList<>();
+        List<List<Integer>> result = new LinkedList();
         Arrays.sort(nums);
-        for(int i=0;i<nums.length;)
-        {
-            for(int j=i+1;j<nums.length;)
-            {
-                int k=j+1 , l = nums.length-1;
-                while(k < l)
-                {
+        int i=0, len = nums.length;
+        while(i < len){
+            int j=i+1;
+            while(j < len){
+                int k = j+1, l = len-1;
+                while(k < l){
                     long sum = (long)nums[i]+nums[j]+nums[k]+nums[l];
-                    if(target == sum)
-                    {
-                        list.add(new LinkedList<>(Arrays.asList(nums[i],nums[j],nums[k],nums[l])));
-                        k++;
-                        l--;
-                        while(k < l && nums[k] == nums[k-1]) k++;
-                        while(k < l && nums[l] == nums[l+1]) l--;
-                    }
-                    else if(sum > target) l--;
+                    if(sum == target){
+
+                        List<Integer> list = new LinkedList();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[k]);
+                        list.add(nums[l]);
+                        result.add(list);
+
+                        int temp = nums[k];
+                        while(k < len && nums[k] == temp) k++;
+                        temp = nums[l];
+                        while(l > j && nums[l] == temp) l--;
+                    }else if(sum > target) l--;
                     else k++;
                 }
-                j++;
-                while(j < nums.length && nums[j] == nums[j-1])j++;
+                int temp = nums[j];
+                while(j < len && temp == nums[j]) j++;
             }
-            i++;
-            while(i < nums.length && nums[i] == nums[i-1]) i++;
+            int temp = nums[i];
+            while(i < len && temp == nums[i]) i++;
         }
-        return list;
+        return result;
     }
 }
