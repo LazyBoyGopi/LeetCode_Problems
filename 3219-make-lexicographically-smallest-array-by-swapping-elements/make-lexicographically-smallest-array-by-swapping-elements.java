d589a72a71a4dfe8a5ea3d81356d5d3dcadfc411
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-01-26 11:43:22 +0530

    Time: 84 ms (69.79%) | Memory: 61.4 MB (100.00%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.java
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.java
@@ -1,23 +1,26 @@
 class Solution {
     public int[] lexicographicallySmallestArray(int[] nums, int limit) {
         int len = nums.length;
-        List<int[]>copySorted = new ArrayList();
+        int[][]copySorted = new int[len][2];
         for(int i=0;i<len;i++){
-            copySorted.add(new int[]{nums[i],i});
+            copySorted[i] = new int[]{nums[i],i};
         }
-        copySorted.sort(Comparator.comparingInt(a->a[0]));
+        Arrays.sort(copySorted,(a,b)->a[0]-b[0]);
         int l = 0, r = 1;
         while(r < len){
             List<Integer>sortedIndexes = new ArrayList();
-            sortedIndexes.add(copySorted.get(l)[1]);
-            while(r < len && Math.abs(copySorted.get(r)[0]-copySorted.get(r-1)[0]) <= limit){
-                sortedIndexes.add(copySorted.get(r++)[1]);
+            sortedIndexes.add(copySorted[l][1]);
+            while(r < len && Math.abs(copySorted[r-1][0]-copySorted[r][0]) <= limit){
+                sortedIndexes.add(copySorted[r++][1]);
             }
             Collections.sort(sortedIndexes);
-            for(int i=0;i<sortedIndexes.size();i++){
-                nums[sortedIndexes.get(i)] = copySorted.get(l++)[0];
+            int sLen = sortedIndexes.size();
+            for(int i=0;i<sLen;i++){
+                nums[sortedIndexes.get(i)] = copySorted[i+l][0];
             }
+            l = r;
             r++;
+
         }
         return nums;
     }
