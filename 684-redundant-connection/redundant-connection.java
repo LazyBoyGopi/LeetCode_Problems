7106f34cf9ed3cc598142f59568329672c5a1c7a
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-01-29 21:58:30 +0530

    Time: 1 ms (75.21%) | Memory: 43.1 MB (86.45%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,8 +1,7 @@
 class Solution {
     int[]parents;
     private int dsuf(int vertice){
-        if(parents[vertice] == -1) return vertice;
-        return dsuf(parents[vertice]);
+        return parents[vertice] == -1 ? vertice : dsuf(parents[vertice]);
     }
     public int[] findRedundantConnection(int[][] edges) {
         int n = edges.length;
@@ -11,12 +10,15 @@ class Solution {
         int[]ans = new int[2];
         for(int i=0;i<n;i++){
             int[]edge = edges[i];
+
             int p1 = dsuf(edge[0]);
             int p2 = dsuf(edge[1]);
+
             if(p1 == p2){
                 ans = edge;
-            }else
+            }else{
                 parents[p1] = p2;
+            }
         }
         return ans;
     }
