99dd15d1abb61bfe669b68a8209f6d2ac0fb52a5
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-02-01 10:15:52 +0530

    Time: 1 ms (37.58%) | Memory: 43.4 MB (43.68%) - LeetSync

--- 3429-special-array-i/special-array-i.java
+++ 3429-special-array-i/special-array-i.java
@@ -1,14 +1,13 @@
 class Solution {
     public boolean isArraySpecial(int[] nums) {
-        boolean isEven = nums[0] % 2 == 0;
-        int len = nums.length;;
-        for(int i=1;i<len;i++){
-            if(isEven){
-                if(nums[i]%2 == 0)
-                    return false;
-            }
-            else if(nums[i]% 2 == 1) return false;
-            isEven = !isEven;
+        int n = nums.length;
+        boolean isLastOdd = (nums[0] & 1) == 1;
+        for(int i=1;i<n;i++){
+            boolean isCurOdd = (nums[i] & 1) == 1;
+            
+            if((isCurOdd && isLastOdd) || (!isCurOdd && !isLastOdd))
+                return false;
+            isLastOdd = isCurOdd;
         }
         return true;
     }
