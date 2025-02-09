f4c1fec6c9c24b175da2074c00c6c3c12fef0450
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-02-09 14:12:39 +0530

    Time: 41 ms (49.41%) | Memory: 59.1 MB (34.71%) - LeetSync

---
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.java
@@ -0,0 +1,35 @@
+/**
+Brute Force
+class Solution {
+    public long countBadPairs(int[] nums) {
+        long badPairs = 0l;
+        int len = nums.length, curIEle = -1;
+        for(int i=0;i<len;i++){
+            curIEle = nums[i];
+            for(int j=i+1;j<len;j++){
+                if(nums[j]-curIEle != j-i)
+                    badPairs++;
+            }
+        }
+        return badPairs;
+    }
+}
+ */
+
+class Solution {
+    public long countBadPairs(int[] nums) {
+        long totalGoodPairs = 0;
+        int len = nums.length;
+        Map<Long,Long>map = new HashMap();
+        for(int i=0;i<len;i++){
+            long ele = i-nums[i];
+            map.put(ele,map.getOrDefault(ele,0l)+1);
+        }
+        for(long value : map.values()){
+            if(value > 1){
+                totalGoodPairs += (value)*(value-1)/2;
+            }
+        }
+        return (((long)len*(len-1))/2)-totalGoodPairs;
+    }
+}
