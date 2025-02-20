0c0cc7ca0a9b8b4ac0ae764fde7fb0b998681df4
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-02-20 10:32:10 +0530

    Time: 1 ms (67.36%) | Memory: 41.4 MB (77.55%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,51 +1,30 @@
 class Solution {
-    public String findDifferentBinaryString(String[] nums) {
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<nums.length;i++)
-        {
-            char curr = nums[i].charAt(i);
-            sb.append(curr == '0'?'1':'0');
+    private String dfs(Set<String>set,int n,StringBuilder cur){
+        if(cur.length() == n){
+            if(set.contains(cur.toString())){
+                return "";
+            }
+            return cur.toString();
         }
-        return sb.toString();
-
-        // StringBuilder sb = new StringBuilder();
-        // for(int i=0;i<nums.length;i++)
-        // {
-        //     sb.append('0');
-        // }
-        // String s = sb.toString();
-        // int idx = 0;
-        // int times = 0;
-        // while(true)
-        // {
-        //     boolean flag = true;
-        //     for(String str : nums)
-        //     {
-        //         if(str.equals(s)){
-        //             flag = false;
-        //             break;
-        //         }
-        //     }
-        //     if(flag) break;
-        //     else{
-        //         char[]ar = s.toCharArray();
-        //         if(ar[idx] == '1')
-        //         {
-        //             ar[idx] = '0';
-        //             times++;
-        //         }
-        //         else{
-        //             ar[idx] ='1';
-        //             times++;
-        //         }
-        //         if(times == 2)
-        //         {
-        //             idx++;
-        //             times=0;
-        //         }
-        //         s = new StringBuilder().append(ar).toString();
-        //     }
-        // }
-        // return s;
+        cur.append('0');
+        String s = dfs(set,n,cur);
+        if(s.length() != 0){
+            return s;
+        }
+        cur.setLength(cur.length()-1);
+        cur.append('1');
+        s = dfs(set,n,cur);
+        if(s.length() != 0){
+            return s;
+        }
+        cur.setLength(cur.length()-1);
+        return "";
+    }
+    public String findDifferentBinaryString(String[] nums) {
+        Set<String>set = new HashSet();
+        for(String s : nums){
+            set.add(s);
+        }
+        return dfs(set,nums.length,new StringBuilder());
     }
 }
