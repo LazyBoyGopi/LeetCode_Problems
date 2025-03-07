7a917820973dbb497a059faa5cfa34dae71c560e
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-03-08 00:39:39 +0530

    Time: 576 ms (14.80%) | Memory: 41.1 MB (90.00%) - LeetSync

---
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.java
@@ -0,0 +1,31 @@
+class Solution {
+    private boolean isPrime(int num){
+        if(num == 1) return false;
+        if(num == 2) return true;
+        if(num % 2 == 0) return false;
+        int sqrt = (int)Math.sqrt(num);
+        for(int i=3;i<=sqrt;i+=2)
+            if(num % i == 0)
+                return false;
+        return true;
+    }
+    public int[] closestPrimes(int left, int right) {
+        int[]ans = {-1,-1};
+        int last = -1;
+        int min = Integer.MAX_VALUE;
+        for(int i=left;i<=right;i++){
+            if(isPrime(i)){
+                if(last != -1){
+                    int curMin = i-last;
+                    if(curMin < min){
+                        min = curMin;
+                        ans[0] = last;
+                        ans[1] = i;
+                    }
+                }
+                last = i;
+            }
+        }
+        return ans;
+    }
+}
