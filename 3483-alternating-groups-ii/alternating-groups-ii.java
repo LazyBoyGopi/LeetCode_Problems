f21003ed881d47414a08698af9edb6b48868ab0d
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-03-09 23:26:45 +0530

    Time: 2 ms (98.04%) | Memory: 63.4 MB (19.27%) - LeetSync

--- 3483-alternating-groups-ii/alternating-groups-ii.java
+++ 3483-alternating-groups-ii/alternating-groups-ii.java
@@ -1,39 +1,3 @@
-/*
-//Brute Force(Time Limit exceeded)
-class Solution {
-    private static boolean isAlternate(int[]col,int st,int k){
-        int end = st+k;
-        int i=st;
-        boolean isZero = col[i++] == 0;
-        while(i < end){
-            if(isZero && col[i] == 0)
-                return false;
-            if(!isZero && col[i] == 1)
-                return false;
-            isZero = !isZero;
-            i++;
-        }
-        return true;
-    }
-    public static int numberOfAlternatingGroups(int[] colors, int k) {
-        int len = colors.length;
-        int[]col = new int[len*2];
-        for(int i=0;i<len;i++){
-            col[i] = col[i+len] = colors[i];
-        }
-        int count = 0;
-        for(int i=0;i<len;i++){
-            if(isAlternate(col,i,k))
-               {
-                 count++;
-                 System.out.println(i);
-                }
-        }
-        return count;
-    }
-}
- */
-//Better code (beats - 86%)
 class Solution {
     public int numberOfAlternatingGroups(int[] colors, int k) {
         int len = colors.length;
