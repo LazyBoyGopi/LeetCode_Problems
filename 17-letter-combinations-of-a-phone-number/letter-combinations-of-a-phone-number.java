543fa6f1880771b344adbb11a91f20e991dd442b
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-03-11 23:08:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (53.19%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,23 +1,22 @@
 class Solution {
-    List<String>ans;
-    String[]keypad = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-    private void checkAllCombinations(String digits,int idx,StringBuilder sb){
-        if(idx >= digits.length()){
-            if(sb.length() > 0)
-                ans.add(sb.toString());
+    String[]keyboard ={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    void possibleCombinations(String digits,StringBuilder ans,List<String>list)
+    {
+        if(digits.length() == 0){
+            list.add(ans.toString());
             return;
         }
-        String curStr = keypad[digits.charAt(idx)-'0'];
-        int len = curStr.length();
-        for(int i=0;i<len;i++){
-            sb.append(curStr.charAt(i));
-            checkAllCombinations(digits,idx+1,sb);
-            sb.setLength(sb.length()-1);
+        String key = keyboard[digits.charAt(0)-48];
+        for(int i=0;i<key.length();i++)
+        {
+            possibleCombinations(digits.substring(1),ans.append(key.charAt(i)),list);
+            ans.setLength(ans.length()-1);
         }
     }
     public List<String> letterCombinations(String digits) {
-        ans = new ArrayList();
-        checkAllCombinations(digits,0,new StringBuilder());
-        return ans;
+        List<String>list = new ArrayList<>();
+        if(digits.length() == 0) return list;
+        possibleCombinations(digits,new StringBuilder(),list);
+        return list;
     }
 }
