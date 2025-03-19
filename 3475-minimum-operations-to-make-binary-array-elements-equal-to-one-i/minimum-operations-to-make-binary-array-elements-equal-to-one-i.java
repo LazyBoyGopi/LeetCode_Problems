13691fcf566e6d93c6ced2bb5ba95df5e432fedb
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-03-19 23:19:38 +0530

    Time: 16 ms (5.75%) | Memory: 58.7 MB (5.13%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -1,19 +1,18 @@
 class Solution {
     public int minOperations(int[] nums) {
-        int len = nums.length , flipCount = 0 , numOfFlips =0;
-        Deque<Boolean> flipped = new LinkedList<>();
-        for(int i=0;i<len;i++){
-            if(i >= 3 && flipped.poll()) numOfFlips--;
-            if(numOfFlips%2 == nums[i]){
-                if(i+3 > len) return -1;
-                numOfFlips++;
-                flipCount++;
-                flipped.offer(true);
+       int minOperations = 0, len = nums.length;
+       for(int i=0;i<len-2;i++){
+        if(nums[i] == 0){
+                minOperations++;
+                nums[i+1] = nums[i+1] == 0 ? 1 : 0;
+                nums[i+2] = nums[i+2] == 0 ? 1 : 0;
             }
-            else{
-                flipped.offer(false);
-            }
-        }
-        return flipCount;
+       }
+       int zCount = 0;
+       for(int i=len-1;i>=len-2;i--){
+        if(nums[i] == 0) zCount++;
+       }
+       System.out.println(zCount+" "+minOperations);
+       return zCount == 0 ? minOperations : -1;
     }
 }
