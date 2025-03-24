4110c72c5c3f9c1821a3ea014d20f1c02dadebd7
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-03-24 15:37:28 +0530

    Time: 9 ms (49.11%) | Memory: 46.6 MB (54.78%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,20 +1,29 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals,(a,b) -> Integer.compare(a[0],b[0]));
-        List<int[]> merged = new LinkedList<>();
-        int[]curInterval = intervals[0];
-        for(int curIdx = 0 ;curIdx<intervals.length;curIdx++)
-        {
-            if(intervals[curIdx][0] <= curInterval[1])
-            {
-                curInterval[1] = Math.max(curInterval[1],intervals[curIdx][1]);
-            }
-            else{
-                merged.add(curInterval);
-                curInterval = intervals[curIdx];
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+        List<int[]>mergedList = new ArrayList();
+        int last = intervals[0][1], len = intervals.length;
+
+        mergedList.add(new int[]{intervals[0][0],intervals[0][1]});
+        for(int i=1;i<len;i++){
+            int[]interval = intervals[i];
+            int st = interval[0];
+            int end = interval[1];
+            if(st <= last){
+                int mSize = mergedList.size();
+                int preLast = mergedList.get(mSize-1)[1];
+                mergedList.get(mSize-1)[1] = last = Math.max(preLast,end);
+            }else{
+                mergedList.add(new int[]{st,end});
+                last = end;
             }
         }
-        merged.add(curInterval);
-        return merged.toArray(new int[merged.size()][]);
+        int[][]ans = new int[mergedList.size()][2];
+        int idx = 0;
+        for(int []ar: mergedList){
+            ans[idx][0] = ar[0];
+            ans[idx++][1] = ar[1];
+        }
+        return ans;
     }
 }
