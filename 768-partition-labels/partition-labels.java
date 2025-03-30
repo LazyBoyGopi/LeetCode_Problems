8b453c8754dc2c47801daa51a8de6f8c0e7b6891
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-03-30 13:58:02 +0530

    Time: 85 ms (5.45%) | Memory: 45 MB (5.10%) - LeetSync

---
+++ 768-partition-labels/partition-labels.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        List<Integer> ans = new ArrayList();
+        int[][]range = new int[26][2];
+        for(int [] r : range){
+            r[0] = -1;
+        }
+        int len = s.length();
+        System.out.println(len);
+        for(int i=0;i<len;i++){
+            int curIdx = s.charAt(i)-'a';
+            if(range[curIdx][0] != -1){
+                range[curIdx][1] = i;
+            }else{
+                range[curIdx][0] = range[curIdx][1] = i; 
+            }
+        }
+        int preIdx = 0;
+        int lastIdxTillNow = range[s.charAt(0)-'a'][1];
+        int i = 0;
+        System.out.println(Arrays.deepToString(range));
+        while(i < len){
+            while(i <= lastIdxTillNow){
+                lastIdxTillNow = Math.max(lastIdxTillNow,range[s.charAt(i)-'a'][1]);
+                System.out.println(i+" "+lastIdxTillNow);
+                i++;
+            }
+            ans.add(lastIdxTillNow-preIdx+1);
+            preIdx = i;
+            if(i < len)
+                lastIdxTillNow = range[s.charAt(i)-'a'][1];
+        }
+        return ans;
+    }
+}
