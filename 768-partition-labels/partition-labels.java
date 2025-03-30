4b5ef11f27032b16ffb7c04230a8f40d324d97cc
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-03-30 14:05:42 +0530

    Time: 8 ms (49.76%) | Memory: 41.9 MB (94.90%) - LeetSync

--- 768-partition-labels/partition-labels.java
+++ 768-partition-labels/partition-labels.java
@@ -1,31 +1,15 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
         List<Integer> ans = new ArrayList();
-        int[][]range = new int[26][2];
-        for(int [] r : range){
-            r[0] = -1;
-        }
-        int len = s.length();
-        for(int i=0;i<len;i++){
-            int curIdx = s.charAt(i)-'a';
-            if(range[curIdx][0] != -1){
-                range[curIdx][1] = i;
-            }else{
-                range[curIdx][0] = range[curIdx][1] = i; 
-            }
-        }
-        int preIdx = 0;
-        int lastIdxTillNow = range[s.charAt(0)-'a'][1];
-        int i = 0;
+        int len = s.length(), preIdx = 0, i = 0, lastIdxTillNow = s.lastIndexOf(s.charAt(0));
         while(i < len){
             while(i <= lastIdxTillNow){
-                lastIdxTillNow = Math.max(lastIdxTillNow,range[s.charAt(i)-'a'][1]);
-                i++;
+                lastIdxTillNow = Math.max(lastIdxTillNow,s.lastIndexOf(s.charAt(i++)));
             }
             ans.add(lastIdxTillNow-preIdx+1);
             preIdx = i;
             if(i < len)
-                lastIdxTillNow = range[s.charAt(i)-'a'][1];
+                lastIdxTillNow = s.lastIndexOf(s.charAt(i));
         }
         return ans;
     }
