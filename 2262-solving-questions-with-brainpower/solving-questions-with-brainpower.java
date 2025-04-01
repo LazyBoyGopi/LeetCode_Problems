2a59efff102fe236d0c8aca89bab57a644b52348
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-04-01 22:17:03 +0530

    Time: 4 ms (99.29%) | Memory: 107.8 MB (89.64%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
@@ -4,7 +4,8 @@ class Solution {
         long[]maxFromHere = new long[len];
         long max = 0l;
         for(int i=len-1;i>=0;i--){
-            long ifTaken = questions[i][0] + ((i+questions[i][1]+1) >= len ? 0 : maxFromHere[i+questions[i][1]+1]);
+            int j = i+questions[i][1]+1;
+            long ifTaken = questions[i][0] + (j >= len ? 0 : maxFromHere[j]);
             max = maxFromHere[i] = Math.max(max,ifTaken);
         }
         return max;
