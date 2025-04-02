efbaafeb890b766645dd0eb6f39e5f7c2f6d7651
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-04-02 20:39:29 +0530

    Time: 1 ms (99.31%) | Memory: 41.8 MB (96.56%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.java
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.java
@@ -1,17 +1,26 @@
 class Solution {
-    public long maximumTripletValue(int[] nums) {
+    int len;
+    private long getLeftMax(int[]nums,int st){
         long max = 0;
-        int len = nums.length;
-        for(int i=0;i<len;i++){
-            for(int j=i+1;j<len;j++){
-                for(int k=j+1;k<len;k++){
-                    long curMax = ((long)nums[i]-nums[j]) * nums[k];
-                    if(curMax > max){
-                        max = curMax;
-                    }
-                }
-            }
+        for(int i=0;i<st;i++){
+            max = Math.max(max,nums[i]);
         }
         return max;
+    }
+    private long getRightMax(int[]nums,int st){
+        long max = 0;
+        for(int i=st+1;i<len;i++){
+            max = Math.max(max,nums[i]);
+        }
+        return max;
+    }
+    public long maximumTripletValue(int[] nums) {
+        long ans = 0;
+        len = nums.length;
+        for(int i=1;i<len-1;i++){
+            long leftMax = getLeftMax(nums,i), rightMax = getRightMax(nums,i);
+            ans = Math.max(ans,(leftMax-nums[i])*rightMax);
+        }
+        return ans;
     }
 }
