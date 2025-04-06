c6e3eec63386c0b10c946b23a7771cb2f492f85a
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-04-06 22:46:41 +0530

    Time: 14 ms (66.53%) | Memory: 44.8 MB (10.36%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.java
+++ 368-largest-divisible-subset/largest-divisible-subset.java
@@ -1,35 +1,32 @@
 class Solution {
     public List<Integer> largestDivisibleSubset(int[] nums) {
         Arrays.sort(nums);
-        int len = nums.length;
+        int len = nums.length, maxIdx = 0, totalMaxEle = 0;
         List<Integer>[] dp = new ArrayList[len];
-        for(int i=len-1;i>=0;i--){
+        for (int i = len - 1; i >= 0; i--) {
             int maxElements = 1;
             int idx = i;
             dp[idx] = new ArrayList();
-            for(int j=i+1;j<len;j++){
-                if(nums[j] % nums[i] == 0){
-                    if(maxElements <= dp[j].size()){
+            for (int j = i + 1; j < len; j++) {
+                if (nums[j] % nums[i] == 0) {
+                    if (maxElements <= dp[j].size()) {
                         idx = j;
                         maxElements = dp[j].size();
                     }
                 }
             }
             dp[i].add(nums[i]);
-            if(i != idx){
-                for(int k : dp[idx]){
-                dp[i].add(k);
+            if (i != idx) {
+                for (int k : dp[idx]) {
+                    dp[i].add(k);
+                }
             }
-            }
-        }
-        int maxEle = 0;
-        int idx = 0;
-        for(int i=0;i<len;i++){
-            if(maxEle < dp[i].size()){
-                maxEle = dp[i].size();
-                idx = i;
+            if(totalMaxEle < dp[i].size()){
+                totalMaxEle = dp[i].size();
+                maxIdx = i;
             }
         }
-        return dp[idx];
+        
+        return dp[maxIdx];
     }
 }
