a60d6e937cbe3040f07b0aac90a1566a855c121d
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-04-07 23:16:08 +0530

    Time: 90 ms (23.78%) | Memory: 56.6 MB (37.24%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -0,0 +1,30 @@
+class Solution {
+    private boolean checkPossibilities(int[] nums, int idx, int target, int[][] dp) {
+        if (target == 0) return true;
+        if (idx == 0) return nums[0] == target;
+
+        if (dp[idx][target] != -1) return dp[idx][target] == 1;
+
+        boolean notPick = checkPossibilities(nums, idx - 1, target, dp);
+        boolean pick = false;
+        if (nums[idx] <= target)
+            pick = checkPossibilities(nums, idx - 1, target - nums[idx], dp);
+
+        dp[idx][target] = (pick || notPick) ? 1 : 0;
+        return dp[idx][target] == 1;
+    }
+
+    public boolean canPartition(int[] nums) {
+        int totalSum = 0;
+        for (int ele : nums) totalSum += ele;
+
+        if ((totalSum & 1) == 1) return false;
+
+        int target = totalSum / 2;
+        int[][] dp = new int[nums.length][target + 1];
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+
+        return checkPossibilities(nums, nums.length - 1, target, dp);
+    }
+}
