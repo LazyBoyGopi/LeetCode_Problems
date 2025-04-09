a48eb5efafa61d62254e5cb1f17d21fb02f07517
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-04-09 10:46:17 +0530

    Time: 2 ms (82.71%) | Memory: 44.3 MB (73.22%) - LeetSync

--- 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.java
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.java
@@ -5,9 +5,9 @@ class Solution {
        for(int ele : nums){
         set.add(ele);
         min = Math.min(min,ele);
+        if(min < k)
+            return -1;
        }
-       if(min < k) return -1;
-       return min == k ? set.size()-1 : set.size();
-       
+       return min < k ? -1 : min == k ? set.size()-1 : set.size();   
     }
 }
