9f1870df40757be1d603c713558d6fc9c3ae4a0f
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-04-11 14:34:06 +0530

    Time: 13 ms (88.51%) | Memory: 44.8 MB (87.93%) - LeetSync

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.java
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.java
@@ -1,25 +1,22 @@
 class Solution {
     public int maximumJumps(int[] nums, int target) {
-        int len = nums.length;
-        int[]dp = new int[len];
-        Arrays.fill(dp,-1);
-        dp[len-1] = 0;
-        int isPos = len-1;
-        for(int i=len-2;i>=0;i--){
-            int max = -1;
-            int idx = -1;
-            for(int j=i+1;j<len;j++){
-                int t = nums[j]-nums[i];
-                if(dp[j] != -1 && -target <= t && t <= target)
-                   {
-                        idx = j;
-                        max = Math.max(max,dp[j]);
-                   }
+        int len = nums.length, isPos = -1;
+        int[] dp = new int[len];
+        Arrays.fill(dp, -1);
+        dp[len - 1] = 0;
+        for (int i = len - 2; i >= 0; i--) {
+            int max = -1, idx = -1;
+            for (int j = i + 1; j < len; j++) {
+                int t = nums[j] - nums[i];
+                if (dp[j] != -1 && -target <= t && t <= target) {
+                    idx = j;
+                    max = Math.max(max, dp[j]);
+                }
             }
-            if(idx != -1){
+            if (idx != -1) {
                 isPos = i;
             }
-            dp[i] = idx == -1 ? -1 : max+1;
+            dp[i] = idx == -1 ? -1 : max + 1;
         }
         return isPos == 0 ? dp[0] : -1;
     }
