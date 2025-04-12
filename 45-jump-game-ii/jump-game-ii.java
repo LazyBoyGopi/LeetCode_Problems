07da2664e0d7cccf41e3bd76c212d1c03fcde3e8
LazyBoyGopi <129869955+LazyBoyGopi@users.noreply.github.com>
2025-04-12 15:40:42 +0530

    Time: 31 ms (18.13%) | Memory: 45.6 MB (18.13%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,17 +1,35 @@
 class Solution {
-    public int jump(int[] nums) {
-        int jump = 0,nextJump = 0, steps = 0,len = nums.length;
-        for(int i=0;i<len;i++){
-            int curJump = i+nums[i];
-            if(i>jump){
-                jump = nextJump;
-                steps++;
-            }
-            if(curJump > nextJump){
-                nextJump = curJump;
-            }
-            if(jump >= len-1) return steps;
+    private int getMinSteps(int[]dp,int i,int j){
+        int minSteps = dp.length;
+        while(i<=j){
+            if(dp[i] != -1)
+                minSteps = Math.min(minSteps,dp[i]);
+            i++;
         }
-        return steps;
+        return minSteps;
+    }
+    public int jump(int[] nums) {
+        int len = nums.length, prev = len-1, steps = 0;
+        int[]dp = new int[len];
+        for(int i=len-2;i>=0;i--){
+            int curMaxIdx = i+nums[i];
+            if(curMaxIdx >= len-1){
+                steps = 1;
+                prev = i;
+            }else if(curMaxIdx == prev){
+                steps += 1;
+                prev = i;
+            }else if(curMaxIdx > prev){
+                steps = getMinSteps(dp,i+1,curMaxIdx)+1;
+                prev = i;
+            }else{
+                dp[i] = -1;
+                continue;
+            }
+            dp[i] = steps;
+        }
+        System.out.println(Arrays.toString(dp));
+        return dp[0];
     }
 }
+
